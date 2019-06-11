66793c22d5b1181ed080dd9815a9e3df58481d45
Bruce He <bruce.he@sickkids.ca>
2019-06-11 17:58:24 -0400

    Updates tto servlet for JSON compatitility.

--- modules/user-viewer/UsersServlet.java
+++ modules/user-viewer/UsersServlet.java
@@ -1,7 +1,27 @@
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
+
 import java.io.PrintWriter;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.jackrabbit.oak.spi.security.principal.PrincipalQueryManager;
-import org.apache.jackrabbit.api.security.principal.PincipalIterator;
+import org.apache.jackrabbit.api.security.principal.PincipalIterator;\
+import java.security.Principal;
 
 // This property type annotation is recommended, and is permissible because we have the maven-bundle-plugin version 4.0.0 or newer
 @Component (service = {Servlet.class})
@@ -33,19 +53,28 @@ public class UsersServlet extends SlingSafeMethodsServlet {
             PrincipalQueryManager users-pqm = new PrincipalQueryManager;
             PrincipalIterator users-pi = users-pqm.findPrincipals(filter, true, 3, offset, limit); 
             // for the int parameter, use: 1 for non-group principals only, 2 for group principals only, 3 for all
-            
+            /*
             out.println("<html>");
             out.println("<head>");
             out.println("<title>User principal list. </title>");
             out.println("<body>");
-            
+            */
             while(user-pi.hasNext()) {
-            	out.println("<p>"+user-pi.next().getPath()+"</p>");
+            	Principal currentPrincipal = users-pi.filter ;
+            	if (currentPrincipal.getClass() == ItemBasedPrincipal.class) {
+            		
+            	}
             }
-            
+            /*
             out.println("</body>");
             out.println("</html>");
+            */
             
+            /*
+             * For JSON: 
+             * 
+             * 
+             */
     	} finally {
     		out.close();
     	}     
