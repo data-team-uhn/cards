7997c1c550be53cac436e45984270914f40903e8
Bruce He <bruce.he@sickkids.ca>
2019-06-11 14:33:42 -0400

    Preliminary draft for user servlet.

--- modules/user-viewer/src/main/frontend/src/userViewer/main.js
+++ modules/user-viewer/src/main/frontend/src/userViewer/main.js
@@ -1,4 +1,23 @@
- import React from 'react';
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+
+import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -447,9 +466,9 @@ class UserBoard extends React.Component {
             <Typography gutterBottom variant="h5" component="h2">
               User Current: {this.state.currentUser}
             </Typography>
-            <Typography>Admin status: {this.state.admin=== true ? "true" : "false"}</Typography>
+            {/*<Typography>Admin status: {this.state.admin=== true ? "true" : "false"}</Typography>*/}
             <Typography>System user status: {this.state.systemUser=== true ? "true" : "false"}</Typography>
-            <Typography>Disabled: {this.state.disabled=== true ? "true" : "false"}</Typography>
+            {/*<Typography>Disabled: {this.state.disabled=== true ? "true" : "false"}</Typography>*/}
             <Typography>Path: {this.state.path}</Typography>
           </CardContent>
           <CardActions>
