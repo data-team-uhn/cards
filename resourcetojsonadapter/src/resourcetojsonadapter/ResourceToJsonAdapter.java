08bb70e296b2cb79a32eecf9969ce8d58b6d1a73
Bruce He <bruce.he@sickkids.ca>
2019-06-19 17:02:27 -0400

    LFS-95: Resource to JSONObject AdapterFactory Experimental setup for AdapterFactory

---
+++ resourcetojsonadapter/src/resourcetojsonadapter/ResourceToJsonAdapter.java
@@ -0,0 +1,15 @@
+package resourcetojsonadapter;
+
+import org.apache.sling.api.adapter.AdapterFactory;
+import org.apache.sling.api.resource.Resource;
+import javax.json.JsonObject;
+
+
+
+public class ResourceToJsonAdapter implements AdapterFactory
+{
+    @Override
+    public <AdapterType> AdapterType getAdapter (Object adaptable, Class<AdapterType> type) {
+        
+    }
+}
