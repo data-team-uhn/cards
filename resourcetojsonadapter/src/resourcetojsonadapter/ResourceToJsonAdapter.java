e3570aa2458dcc407427184ff2223f1821369ac1
Bruce He <bruce.he@sickkids.ca>
2019-06-20 11:46:50 -0400

    LFS-95: Rescource to JSONObject AdapterFactory Completed draft ResourceToJsonAdapter in resourcetojsonadapter/

--- resourcetojsonadapter/src/resourcetojsonadapter/ResourceToJsonAdapter.java
+++ resourcetojsonadapter/src/resourcetojsonadapter/ResourceToJsonAdapter.java
@@ -1,15 +1,40 @@
 package resourcetojsonadapter;
 
+import java.util.function.BiConsumer;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import org.apache.sling.api.adapter.AdapterFactory;
 import org.apache.sling.api.resource.Resource;
-import javax.json.JsonObject;
+import org.apache.sling.api.resource.ValueMap;
+import org.osgi.service.component.annotations.Component;
 
-
+@Component (
+    service = {AdapterFactory.class},
+    property = {
+    AdapterFactory.ADAPTABLE_CLASSES +"=org.apache.sling.api.resource.Resource",
+    AdapterFactory.ADAPTER_CLASSES + "=javax.json.JsonObject"
+    }
+)
 
 public class ResourceToJsonAdapter implements AdapterFactory
 {
     @Override
+    @SuppressWarnings("unchecked")
     public <AdapterType> AdapterType getAdapter (Object adaptable, Class<AdapterType> type) {
-        
+        Resource resource = (Resource) adaptable;
+        if (resource != null) {
+            ValueMap valuemap = resource.getValueMap();
+            JsonObjectBuilder objectbuilder = Json.createObjectBuilder();
+            
+            BiConsumer<String, Object> biconsumer = (key, obj)-> objectbuilder.add(key, obj.toString());
+            
+            valuemap.forEach(biconsumer);
+            
+            JsonObject jsonobject = objectbuilder.build();
+            
+            return (AdapterType) jsonobject;
+        }
+        return null;
     }
 }
