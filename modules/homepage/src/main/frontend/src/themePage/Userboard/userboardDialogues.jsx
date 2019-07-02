9a4eacb6701c62b2ae83f5d09c2c7a393d94f114
Bruce He <bruce.he@sickkids.ca>
2019-07-02 15:30:03 -0400

    LFS-34: User Management Page Remove create user and group dialogues to separate file.

---
+++ modules/homepage/src/main/frontend/src/themePage/Userboard/userboardDialogues.jsx
@@ -0,0 +1,177 @@
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
+
+import React from "react";
+
+import GridItem from "material-dashboard-react/dist/components/Grid/GridItem.js";
+import GridContainer from "material-dashboard-react/dist/components/Grid/GridContainer.js";
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+
+export class CreateUserDialogue extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        newName: "",
+        newPwd: "",
+        newPwdConfirm: ""
+      };
+    }
+  
+    handleCreateUser() {
+      let formData = new FormData();
+      formData.append(':name', this.state.newName);
+      formData.append('pwd', this.state.newPwd);
+      formData.append('pwdConfirm', this.state.newPwdConfirm);
+  
+      console.log(formData);
+  
+      let url = "http://localhost:8080/system/userManager/user.create.html";
+  
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic' + btoa('admin:admin')
+        },
+        body:formData  
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  
+    render(){
+      return(
+        <Dialog
+          open= {true}
+          onClose= {(event) => {event.preventDefault(); this.props.handleClose();}}
+        >
+          <DialogTitle>Create New User</DialogTitle>
+          <DialogContent>
+          <form
+            onSubmit={()=>this.handleCreateUser()}
+          >
+            <GridContainer>
+              <GridItem>
+                <TextField
+                  id="name"
+                  name="name"
+                  label="Name"
+                  onChange={(event) => {this.setState({newName: event.target.value});}}
+                />
+              </GridItem>
+              <GridItem >
+                <TextField
+                  id="password"
+                  name="password"
+                  label="Password"
+                  onChange={(event) => {this.setState({newName: event.target.value});}}
+                />
+              </GridItem>
+              <GridItem >
+                <TextField
+                  id="passwordconfirm"
+                  name="passwordconfirm"
+                  label="Confirm Password"
+                  onChange={(event) => {this.setState({newName: event.target.value});}}
+                />
+              </GridItem>
+            </GridContainer>
+            <Button
+              type = "submit"
+            >
+              Create User
+            </Button>
+          </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.props.handleClose()}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      );
+    }
+  }
+  
+  export class CreateGroupDialogue extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state={
+        newName: ""
+      };
+    }
+  
+    handleCreateGroup() {
+      let formData = new FormData();
+      formData.append(':name', this.state.newName);
+  
+      let url = "http://localhost:8080/system/userManager/group.create.json";
+  
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic' + btoa('admin:admin')
+        },
+        body: formData
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+  
+    render() {
+      return(
+        <Dialog
+          open = {true}
+          onClose = {() => this.props.handleClose()}
+        >
+          <DialogTitle>Create New Group</DialogTitle>
+          <DialogContent>
+            <form
+              onSubmit = {() => this.handleCreateGroup()}
+            >
+              <GridContainer>
+                <GridItem>
+                  <TextField
+                    id = "name"
+                    name = "name"
+                    label = "Name"
+                    onChange = {(event) => {this.setState({newName: event.target.value});}}
+                  />
+                </GridItem>
+              </GridContainer>
+              <Button 
+                type = "submit"
+              >
+                Create Group
+              </Button>
+            </form>  
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.props.handleClose()}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      );
+    }
+  }
