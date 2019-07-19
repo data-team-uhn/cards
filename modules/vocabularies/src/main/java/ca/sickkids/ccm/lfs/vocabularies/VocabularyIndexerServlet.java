81e5d67d439eab20da89414b1f1fde22db1d311b
Bruce He <bruce.he@sickkids.ca>
2019-07-19 14:04:27 -0400

    LFS-101: Add a servlet for indexing a vocabulary Set up indexing servlet.

---
+++ modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/VocabularyIndexerServlet.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package ca.sickkids.ccm.lfs.vocabularies;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.jcr.RepositoryException;
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import javax.servlet.Servlet;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.log.LogService;
+
+@Component (service = {Servlet.class})
+@SlingServletResourceTypes(resourceTypes = {"lfs/VocabulariesHomepage"}, methods = {"POST"})
+public class VocabularyIndexerServlet extends SlingSafeMethodsServlet 
+{
+	@Override
+	public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) 
+	{
+		String identifier = request.getParameter("identifier");
+		String source = request.getParameter("source");
+		
+		
+	}
+}
