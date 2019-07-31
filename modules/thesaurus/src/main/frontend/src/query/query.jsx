af8ffd6a28a194c0007c85fe89ddc7cf98060173
Francis Nguyen <OrdinaryNeumann@gmail.com>
2019-07-31 16:56:41 -0400

    LFS-105: UI to browse NCI Thesaurus terms as a tree Change from a bottom-up coding style to a top-down

--- modules/thesaurus/src/main/frontend/src/query/query.jsx
+++ modules/thesaurus/src/main/frontend/src/query/query.jsx
@@ -61,184 +61,6 @@ class Thesaurus extends React.Component {
     };
   }
 
-  // callback for getInfo to populate info box
-  showInfo = (status, data) => {
-    if (status === null) {
-      // Use an empty array instead of null if this element has no synonyms
-      var synonym = [];
-      if ("synonym" in data)
-      {
-        synonym = data["synonym"];
-      }
-
-      const typeOf = data["parents"].map((parent, index) => {
-        return parent["name"];
-      })
-
-      this.setState({
-        infoID: data["id"],
-        infoName: data["name"],
-        infoDefinition: data["def"],
-        infoAlsoKnownAs: synonym,
-        infoTypeOf: typeOf,
-        infoAnchor: this.state.buttonRefs[data["id"]],
-        termInfoVisible: true,
-      });
-    } else {
-      this.logError("Error: term lookup failed with code " + status);
-    }
-  }
-
-  // Grab information about the given ID and populate the info box
-  getInfo = (id) => {
-    var URL = `${REST_URL}/hpo/${id}`;
-    MakeRequest(URL, this.showInfo);
-  }
-
-  // Register a button reference that the info box can use to align itself to
-  registerInfoButton = (id, node) => {
-    // List items getting deleted will overwrite new browser button refs, so
-    // we must ignore deregistration events
-    if (node != null) {
-      this.state.buttonRefs[id] = node;
-    }
-  }
-
-  // Callback for queryInput to populate the suggestions bar
-  showSuggestions = (status, data) => {
-    if (status === null) {
-        // Populate this.state.suggestions
-        var suggestions = [];
-
-        data["rows"].forEach((element) => {
-          suggestions.push(
-            <MenuItem
-              className={this.props.classes.dropdownItem}
-              key={element["name"]}
-            >
-              {element["name"]}
-              <Button
-                buttonRef={node => {
-                  this.registerInfoButton(element["id"], node);
-                }}
-                color="info"
-                justIcon={true}
-                simple={true}
-                aria-owns={this.state.termInfoVisible ? "menu-list-grow" : null}
-                aria-haspopup={true}
-                onClick={(e) => this.getInfo(element["id"])}
-                className={this.props.classes.buttonLink}
-              >
-                <Info color="primary" />
-              </Button>
-            </MenuItem>
-            );
-        });
-
-        this.setState({
-          suggestions: suggestions,
-          suggestionsVisible: true,
-          suggestionsLoading: false,
-        });
-    } else {
-      this.logError("Error: Thesaurus lookup failed with code " + status);
-    }
-  }
-
-  // Grab suggestions for the given input
-  queryInput = (input) => {
-    // Empty input? Do not query
-    if (input === "") {
-      this.setState({
-        suggestionsLoading: false,
-        termInfoVisible: false,
-        lookupTimer: null,
-      });
-      return;
-    }
-
-    // Grab suggestions
-    input = encodeURIComponent(input);
-    var URL = `${REST_URL}/hpo/suggest?input=${input}`;
-    MakeRequest(URL, this.showSuggestions);
-
-    // Hide the infobox and stop the timer
-    this.setState({
-      suggestionsLoading: true,
-      termInfoVisible: false,
-      lookupTimer: null,
-    });
-  }
-
-  // Lookup the search term after a short interval
-  // This will reset the interval if called before the interval hangs up
-  delayLookup = (status) => {
-    if (this.state.lookupTimer !== null) {
-      clearTimeout(this.state.lookupTimer);
-    }
-
-    this.setState({
-      lookupTimer: setTimeout(this.queryInput, 500, status.target.value),
-      suggestionsVisible: true,
-      suggestions: [],
-    })
-  }
-
-  // Event handler for clicking away from the autocomplete while it is open
-  closeAutocomplete = event => {
-    if (this.anchorEl.contains(event.target)) {
-      return;
-    }
-
-    this.setState({
-      suggestionsVisible: false,
-      termInfoVisible: false,
-    });
-  };
-
-  // Event handler for clicking away from the info window while it is open
-  closeInfo = (event) => {
-    if (this.anchorEl.contains(event.target)) {
-      return;
-    }
-
-    this.setState({ termInfoVisible: false });
-  };
-
-  openDialog = () => {
-    this.setState({
-      browserOpened: true,
-      browseID: this.state.infoID,
-    })
-  }
-
-  closeDialog = () => {
-    this.setState({
-      browserOpened: false,
-      suggestionsVisible: false,
-      termInfoVisible: false,
-    })
-  }
-
-  changeInfoID = (id) => {
-    this.setState({
-      infoID: id
-    });
-  };
-
-  changeBrowseID = (id) => {
-    this.setState({
-      browseID: id,
-    })
-  }
-
-  logError = (message) => {
-    this.setState({
-      snackbarVisible: true,
-      snackbarMessage: message,
-    })
-  }
-
   render() {
     const { classes } = this.props;
 
@@ -422,6 +244,184 @@ class Thesaurus extends React.Component {
           </Snackbar>
       </div>
     );
+  }
+
+  // Lookup the search term after a short interval
+  // This will reset the interval if called before the interval hangs up
+  delayLookup = (status) => {
+    if (this.state.lookupTimer !== null) {
+      clearTimeout(this.state.lookupTimer);
+    }
+
+    this.setState({
+      lookupTimer: setTimeout(this.queryInput, 500, status.target.value),
+      suggestionsVisible: true,
+      suggestions: [],
+    })
+  }
+
+  // Grab suggestions for the given input
+  queryInput = (input) => {
+    // Empty input? Do not query
+    if (input === "") {
+      this.setState({
+        suggestionsLoading: false,
+        termInfoVisible: false,
+        lookupTimer: null,
+      });
+      return;
+    }
+
+    // Grab suggestions
+    input = encodeURIComponent(input);
+    var URL = `${REST_URL}/hpo/suggest?input=${input}`;
+    MakeRequest(URL, this.showSuggestions);
+
+    // Hide the infobox and stop the timer
+    this.setState({
+      suggestionsLoading: true,
+      termInfoVisible: false,
+      lookupTimer: null,
+    });
+  }
+
+  // Callback for queryInput to populate the suggestions bar
+  showSuggestions = (status, data) => {
+    if (status === null) {
+        // Populate this.state.suggestions
+        var suggestions = [];
+
+        data["rows"].forEach((element) => {
+          suggestions.push(
+            <MenuItem
+              className={this.props.classes.dropdownItem}
+              key={element["name"]}
+            >
+              {element["name"]}
+              <Button
+                buttonRef={node => {
+                  this.registerInfoButton(element["id"], node);
+                }}
+                color="info"
+                justIcon={true}
+                simple={true}
+                aria-owns={this.state.termInfoVisible ? "menu-list-grow" : null}
+                aria-haspopup={true}
+                onClick={(e) => this.getInfo(element["id"])}
+                className={this.props.classes.buttonLink}
+              >
+                <Info color="primary" />
+              </Button>
+            </MenuItem>
+            );
+        });
+
+        this.setState({
+          suggestions: suggestions,
+          suggestionsVisible: true,
+          suggestionsLoading: false,
+        });
+    } else {
+      this.logError("Error: Thesaurus lookup failed with code " + status);
+    }
+  }
+
+  // Event handler for clicking away from the autocomplete while it is open
+  closeAutocomplete = event => {
+    if (this.anchorEl.contains(event.target)) {
+      return;
+    }
+
+    this.setState({
+      suggestionsVisible: false,
+      termInfoVisible: false,
+    });
+  };
+
+  // Register a button reference that the info box can use to align itself to
+  registerInfoButton = (id, node) => {
+    // List items getting deleted will overwrite new browser button refs, so
+    // we must ignore deregistration events
+    if (node != null) {
+      this.state.buttonRefs[id] = node;
+    }
+  }
+
+  // Grab information about the given ID and populate the info box
+  getInfo = (id) => {
+    var URL = `${REST_URL}/hpo/${id}`;
+    MakeRequest(URL, this.showInfo);
+  }
+
+  // callback for getInfo to populate info box
+  showInfo = (status, data) => {
+    if (status === null) {
+      // Use an empty array instead of null if this element has no synonyms
+      var synonym = [];
+      if ("synonym" in data)
+      {
+        synonym = data["synonym"];
+      }
+
+      const typeOf = data["parents"].map((parent, index) => {
+        return parent["name"];
+      })
+
+      this.setState({
+        infoID: data["id"],
+        infoName: data["name"],
+        infoDefinition: data["def"],
+        infoAlsoKnownAs: synonym,
+        infoTypeOf: typeOf,
+        infoAnchor: this.state.buttonRefs[data["id"]],
+        termInfoVisible: true,
+      });
+    } else {
+      this.logError("Error: term lookup failed with code " + status);
+    }
+  }
+
+  // Event handler for clicking away from the info window while it is open
+  closeInfo = (event) => {
+    if (this.anchorEl.contains(event.target)) {
+      return;
+    }
+
+    this.setState({ termInfoVisible: false });
+  };
+
+  openDialog = () => {
+    this.setState({
+      browserOpened: true,
+      browseID: this.state.infoID,
+    })
+  }
+
+  closeDialog = () => {
+    this.setState({
+      browserOpened: false,
+      suggestionsVisible: false,
+      termInfoVisible: false,
+    })
+  }
+
+  changeInfoID = (id) => {
+    this.setState({
+      infoID: id
+    });
+  };
+
+  changeBrowseID = (id) => {
+    this.setState({
+      browseID: id,
+    })
+  }
+
+  logError = (message) => {
+    this.setState({
+      snackbarVisible: true,
+      snackbarMessage: message,
+    })
   }
 }
 
