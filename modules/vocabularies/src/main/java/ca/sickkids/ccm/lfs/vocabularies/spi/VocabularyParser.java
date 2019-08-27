add60af82939b6b77098ea0f2fedb839c29a1ac6
Sergiu Dumitriu <sergiu@phenotips.org>
2019-08-27 11:52:46 -0400

    LFS-22: As an admin, I can specify a standardized vocabulary for populating the values of a variable Extract generic vocabulary parsing methods into a common VocabularyParserUtils service

--- modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/spi/VocabularyParser.java
+++ modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/spi/VocabularyParser.java
@@ -20,8 +20,6 @@ package ca.sickkids.ccm.lfs.vocabularies.spi;
 
 import java.io.IOException;
 
-import javax.jcr.Node;
-
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.osgi.service.log.LogService;
@@ -42,29 +40,5 @@ public interface VocabularyParser
      * @throws IOException thrown when response Json cannot be written
      */
     void parseVocabulary(SlingHttpServletRequest request, SlingHttpServletResponse response, LogService logger)
-        throws IOException;
-
-    /**
-     * Remove any previous instances of the vocabulary which is to be parsed and indexed in the JCR repository.
-     *
-     * @param homepage - VocabulariesHomepage node instance serving as root for Vocabulary nodes
-     * @param name - identifier of the vocabulary which will become its node name
-     * @throws VocabularyIndexException thrown when the node cannot be removed
-     */
-    void clearVocabularyNode(Node homepage, String name)
-        throws VocabularyIndexException;
-
-    /**
-     * Writes a json to the http response consisting of two entries, "isSuccessful" which indicates if the parsing
-     * attempt was successful or not, and "error" which is the error message in case of an error.
-     *
-     * @param request - http request for the VocabularyIndexerServlet
-     * @param response - http response for the VocabularyIndexerServlet
-     * @param isSuccessful - boolean variable which when true indicates success and when false indicates failure
-     * @param error - error message to display if error occurs
-     * @throws IOException - thrown when the json cannot be written
-     */
-    void writeStatusJson(SlingHttpServletRequest request, SlingHttpServletResponse response,
-        boolean isSuccessful, String error)
         throws IOException;
 }
