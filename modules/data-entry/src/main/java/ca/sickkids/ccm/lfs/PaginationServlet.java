8aecf904df390a745d3db0c99679e0eac6c96db7
Sergiu Dumitriu <sergiu@phenotips.org>
2019-09-19 14:39:53 -0400

    LFS-139 / LFS-140: Add a servlet that can return a page of resources of a certain type Done

---
+++ modules/data-entry/src/main/java/ca/sickkids/ccm/lfs/PaginationServlet.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package ca.sickkids.ccm.lfs;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+
+import javax.jcr.query.Query;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.stream.JsonGenerator;
+import javax.servlet.Servlet;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * A servlet that lists resources of a specific type, depending on which "homepage" resource the request is targeting.
+ * <p>
+ * This servlet supports the following parameters:
+ * </p>
+ * <ul>
+ * <li><tt>offset</tt>: a 0-based number representing how many resources to skip; 0 by default</li>
+ * <li><tt>limit</tt>: a number representing how many resources to include at most in the result; 10 by default</li>
+ * <li><tt>filter</tt>: a (lucene-like) search term, such as {@code germline}, {@code cancer OR tumor},
+ * {@code (*blastoma OR *noma OR tumor*) recurrent}; no filter set by default</li>
+ * </ul>
+ *
+ * @version $Id$
+ */
+@Component(service = { Servlet.class })
+@SlingServletResourceTypes(
+    resourceTypes = { "lfs/QuestionnairesHomepage", "lfs/FormsHomepage", "lfs/SubjectsHomepage" },
+    selectors = { "paginate" })
+public class PaginationServlet extends SlingSafeMethodsServlet
+{
+    private static final long serialVersionUID = -6068156942302219324L;
+
+    @Override
+    public void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws IOException
+    {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        final long limit = getLongValueOrDefault(request.getParameter("limit"), 10);
+        final long offset = getLongValueOrDefault(request.getParameter("offset"), 0);
+        final StringBuilder query =
+            // We select all child nodes of the homepage, filtering out nodes that aren't ours, such as rep:policy
+            new StringBuilder("select n from [nt:base] as n where ischildnode(n, '" + request.getResource().getPath()
+                + "') and n.'sling:resourceSuperType' = 'lfs/Resource'");
+        final String filter = request.getParameter("filter");
+        if (StringUtils.isNotBlank(filter)) {
+            // Full text search; \ and ' must be escaped
+            query.append(" and contains(*, '" + filter.replaceAll("['\\\\]", "\\\\$0") + "')");
+        }
+        query.append(" order by 'jcr:created'");
+        final Iterator<Resource> results =
+            request.getResourceResolver().findResources(query.toString(), Query.JCR_SQL2);
+
+        try (Writer out = response.getWriter(); JsonGenerator jsonGen = Json.createGenerator(out)) {
+            jsonGen.writeStartObject();
+            long[] limits = writeNodes(jsonGen, results, offset, limit);
+            writeSummary(jsonGen, request, limits);
+            jsonGen.writeEnd().flush();
+        }
+    }
+
+    /**
+     * Write metadata about the request and response. This includes the number of returned and total matching nodes, and
+     * copying some request parameters.
+     *
+     * @param jsonGen the JSON generator where the results should be serialized
+     * @param request the current request
+     * @param offset the requested offset, may be the default value of {0}
+     * @param limit the requested limit, may be the default value of {10}
+     * @param returnedNodes the number of matching nodes included in the response, may be {@code 0} if no nodes were
+     *            returned
+     * @param totalMatchingNodes the total number of accessible nodes matching the request, may be {@code 0} if no nodes
+     *            match the filters, or the current user cannot access the nodes
+     */
+    private void writeSummary(final JsonGenerator jsonGen, final SlingHttpServletRequest request, final long[] limits)
+    {
+        jsonGen.write("req", request.getParameter("req"));
+        jsonGen.write("offset", limits[0]);
+        jsonGen.write("limit", limits[1]);
+        jsonGen.write("returnedrows", limits[2]);
+        jsonGen.write("totalrows", limits[3]);
+    }
+
+    private long[] writeNodes(final JsonGenerator jsonGen, final Iterator<Resource> nodes,
+        final long offset, final long limit)
+    {
+        final long[] counts = new long[4];
+        counts[0] = offset;
+        counts[1] = limit;
+        counts[2] = 0;
+        counts[3] = 0;
+
+        long offsetCounter = offset < 0 ? 0 : offset;
+        long limitCounter = limit < 0 ? 0 : limit;
+
+        jsonGen.writeStartObject("rows");
+
+        while (nodes.hasNext()) {
+            Resource n = nodes.next();
+            if (offsetCounter > 0) {
+                --offsetCounter;
+            } else if (limitCounter > 0) {
+                jsonGen.write(n.getPath(), n.adaptTo(JsonObject.class));
+                --limitCounter;
+                ++counts[2];
+            }
+            ++counts[3];
+        }
+
+        jsonGen.writeEnd();
+
+        return counts;
+    }
+
+    private long getLongValueOrDefault(final String stringValue, final long defaultValue)
+    {
+        long value = defaultValue;
+        try {
+            value = Long.parseLong(stringValue);
+        } catch (NumberFormatException exception) {
+            value = defaultValue;
+        }
+        return value;
+    }
+}
