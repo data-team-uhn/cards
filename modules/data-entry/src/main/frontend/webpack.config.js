e84b2722509fb30e78982f3b3e61e315757c3045
Sergiu Dumitriu <sergiu@phenotips.org>
2019-09-24 19:05:19 -0400

    LFS-139 / LFS-136: Create a homepage for Subjects Done

--- modules/data-entry/src/main/frontend/webpack.config.js
+++ modules/data-entry/src/main/frontend/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     [module_name + 'showQuery']: './src/dataQuery/query.js',
     [module_name + 'LiveTable']: './src/dataHomepage/LiveTable.jsx',
     [module_name + 'Questionnaires']: './src/dataHomepage/Questionnaires.jsx',
+    [module_name + 'Subjects']: './src/dataHomepage/Subjects.jsx',
   },
   plugins: [
     new CleanWebpackPlugin(),
