02579a86efe215e0b766e6f82d8aa00362a25b8a
Sergiu Dumitriu <sergiu@phenotips.org>
2019-11-18 14:22:39 -0500

    LFS-124 / LFS-181: Basic form display and saving functionality Fixed bug with multi-value date fields being incorrectly parsed

--- modules/data-entry/src/main/frontend/src/questionnaire/DateQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/DateQuestion.jsx
@@ -99,7 +99,7 @@ function amendMoment(date, format) {
 function DateQuestion(props) {
   let {existingAnswer, type, displayFormat, lowerLimit, upperLimit, classes, ...rest} = props;
   let {text, dateFormat} = {dateFormat: "yyyy-MM-dd", ...props.questionDefinition, ...props};
-  let currentStartValue = existingAnswer && existingAnswer[1].value || '';
+  let currentStartValue = existingAnswer && existingAnswer[1].value && Array.of(existingAnswer[1].value).flat()[0] || '';
   const [selectedDate, changeDate] = useState(amendMoment(moment(currentStartValue), dateFormat));
   // FIXME There's no way to store the end date currently. Maybe add existingAnswer[1].endValue?
   const [selectedEndDate, changeEndDate] = useState(amendMoment(moment(), dateFormat));
