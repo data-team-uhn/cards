efe59b8b38bee21f94581b832d520385382fc439
Sergiu Dumitriu <sergiu@phenotips.org>
2020-02-03 13:37:39 -0500

    Revert "LFS-248: Make the frontend resources load faster by removing duplicated"
    This reverts commit 1d6b15bf75cd34da81f231d61d45e02d8235d29a.

--- modules/aggregated-frontend/src/main/frontend/webpack.config-template.js
+++ modules/aggregated-frontend/src/main/frontend/webpack.config-template.js
@@ -51,6 +51,8 @@ ENTRY_CONTENT
       chunks: 'all',
       cacheGroups: {
         defaultVendors: {
+          minChunks: 2,
+          minSize: 20000000,
           filename: '[name].[contenthash].bundle.js',
           test: /[\\/]node_modules[\\/]/,
           name: 'vendor',
