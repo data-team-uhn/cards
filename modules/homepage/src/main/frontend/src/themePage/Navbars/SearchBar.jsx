data-team-uhn/cards

commit 87559a8eaad5614d99b3f231bd326347de8e00b1

LFS-282: Removed UI elements dealing with resource types that are no longer supported by the quick search backend (Questionnaires and Subjects)

modules/homepage/src/main/frontend/src/themePage/Navbars/SearchBar.jsx
@@ -22,15 +22,12 @@ import { withRouter } from "react-router-dom";
 
 import { ClickAwayListener, Grow, IconButton, Input, InputAdornment, ListItemText, MenuItem, ListItemAvatar, Avatar}  from "@material-ui/core";
 import { MenuList, Paper, Popper, Typography, withStyles } from "@material-ui/core";
-import AssignmentIcon from "@material-ui/icons/Assignment";
 import DescriptionIcon from "@material-ui/icons/Description";
-import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 import Search from "@material-ui/icons/Search";
 import HeaderStyle from "./headerStyle.jsx";
 
 const QUERY_URL = "/query";
 const MAX_RESULTS = 5;
-const LFS_QUERY_MATCH_KEY = "lfs:queryMatch";
 
 function SearchBar(props) {
   const { classes, className, closeSidebar, invertColors, doNotEscapeQuery } = props;
@@ -76,16 +73,16 @@ function SearchBar(props) {
     doNotEscapeQuery && new_url.searchParams.set("doNotEscapeQuery", "true");
     new_url.searchParams.set("limit", MAX_RESULTS);
     new_url.searchParams.set("req", requestID);
-    // In the closure generated, displayResults will look for req_id, instead of requestID+1
+    // In the closure generated, postprocessResults will look for req_id, instead of requestID+1
     setRequestID(requestID+1);
     fetch(new_url)
       .then(response => response.ok ? response.json() : Promise.reject(response))
-      .then(displayResults)
+      .then(postprocessResults)
       .catch(handleError);
   }
 
   // Callback to store the results of the top results, or to display 'No results'
-  let displayResults = (json) => {
+  let postprocessResults = (json) => {
     // Ignore this result if the request ID does not match our last request
     if (json["req"] != requestID) {
       return;
@@ -108,46 +105,22 @@ function SearchBar(props) {
     setError(response);
   }
 
-  const categoryToAvatar = {
-    ["lfs:Form"]: <Avatar className={classes.searchResultFormIcon + " " + classes.searchResultAvatar}>
-        <DescriptionIcon />
-      </Avatar>,
-    ["lfs:Questionnaire"]: <Avatar className={classes.searchResultQuestionnaireIcon + " " + classes.searchResultAvatar}>
-        <AssignmentIcon />
-      </Avatar>,
-    ["lfs:Subject"]: <Avatar className={classes.searchResultSubjectIcon + " " + classes.searchResultAvatar}>
-        <AssignmentIndIcon />
-      </Avatar>
+  // Generate a human-readable info about the resource (form) matching the query:
+  // * questionnaire title (if available) and result type, followed by
+  // * the form's subject name (if available) or the resource's uuid
+  function QuickSearchResultHeader(props) {
+    const {resultData} = props;
+    return resultData && (
+      <React.Fragment>
+        <Typography variant="body2" color="textSecondary">
+          {(resultData.questionnaire?.title?.concat(' ') || '') + (resultData["jcr:primaryType"]?.replace(/lfs:/,"") || '')}
+        </Typography>
+        {resultData.subject?.identifier || resultData["jcr:uuid"] || ''}
+      </React.Fragment>
+    ) || null
   }
 
-  // Get a user friendly version of the icon
-  let getCategoryIcon = (element) => (
-    categoryToAvatar[element["jcr:primaryType"]] ?
-      <ListItemAvatar>{categoryToAvatar[element["jcr:primaryType"]]}</ListItemAvatar>
-      : " "
-  );
-
-  // Get a user friendly version of the category
-  let getFriendlyCategory = (element) => {
-    const friendlyType = element["jcr:primaryType"] && element["jcr:primaryType"].replace(/lfs:/,"");
-    /* Prepend the questionnaire title, if this has it */
-    return (element["questionnaire"] ? element["questionnaire"]["title"] + " " + friendlyType
-    /* Otherwise just use the user-friendly element name */
-      : friendlyType);
-  }
-
-  // Attempt a few different methods of getting the name of an element from <code>/query?quick</code>
-  let getElementName = (element) => {
-    /* Form data: grab the subject name */
-    return (element["subject"] && element["subject"]["identifier"])
-      /* Questionnaire: grab the title */
-      || element["name"] || element["title"]
-      /* Subject: grab the subject ID */
-      || element["identifier"]
-      /* Could not find any of the above: return the uuid */
-      || element["jcr:uuid"];
-  }
-
+  // Display how the query matched the result
   function QuickSearchMatch(props) {
     const {matchData} = props;
     return matchData && (
@@ -157,6 +130,21 @@ function SearchBar(props) {
         <span className={classes.queryMatchBefore}>{matchData[1]}</span>
         <span className={classes.highlightedText}>{matchData[2]}</span>
         <span className={classes.queryMatchAfter}>{matchData[3]}</span>
+      </React.Fragment>
+    ) || null
+  }
+
+  // Display a quick search result
+  function QuickSearchResult(props) {
+    const {resultData} = props;
+    return resultData["jcr:primaryType"] && (
+      <React.Fragment>
+        <ListItemAvatar><Avatar className={classes.searchResultAvatar}><DescriptionIcon /></Avatar></ListItemAvatar>
+        <ListItemText
+          primary={(<QuickSearchResultHeader resultData={resultData} />)}
+          secondary={(<QuickSearchMatch matchData={resultData["lfs:queryMatch"]} />)}
+          className={classes.dropdownItem}
+        />
       </React.Fragment>
     ) || null
   }
@@ -231,33 +219,22 @@ function SearchBar(props) {
                         primaryTypographyProps={{color: "error"}}
                         />
                     </MenuItem>
-                  : results.map( (element) => (
+                  : results.map( (result) => (
                     /* Results if no errors occurred */
                     <MenuItem
                       className={classes.dropdownItem}
-                      key={element["@path"]}
-                      disabled={element["disabled"]}
+                      key={result["@path"]}
+                      disabled={result["disabled"]}
                       onClick={(e) => {
                         // Redirect using React-router
-                        if (element["@path"]) {
-                          props.history.push("/content.html" + element["@path"]);
+                        if (result["@path"]) {
+                          props.history.push("/content.html" + result["@path"]);
                           closeSidebar && closeSidebar();
                           setPopperOpen(false);
                         }
                         }}
                       >
-                        {getCategoryIcon(element)}
-                        {/* for now, nothing in the secondary until we get the suggest() API working */}
-                        <ListItemText
-                          primary={(<div>
-                            <Typography variant="body2" color="textSecondary">
-                              {getFriendlyCategory(element)}
-                            </Typography>
-                            {getElementName(element)}
-                          </div>)}
-                          secondary={(<QuickSearchMatch matchData={element[LFS_QUERY_MATCH_KEY]} />)}
-                          className={classes.dropdownItem}
-                          />
+                      <QuickSearchResult resultData={result} />
                     </MenuItem>
                   ))}
                 </MenuList>

modules/homepage/src/main/frontend/src/themePage/Navbars/headerStyle.jsx
@@ -112,16 +112,8 @@ const headerStyle = theme => ({
     zIndex: "1301"
   },
   searchResultAvatar: {
-    color: "white",
-  },
-  searchResultFormIcon: {
-    backgroundColor: "cornflowerblue"
-  },
-  searchResultQuestionnaireIcon: {
-    backgroundColor: "limegreen"
-  },
-  searchResultSubjectIcon: {
-    backgroundColor: "orange"
+    color: theme.palette.getContrastText(theme.palette.info.main),
+    backgroundColor: theme.palette.info.main,
   },
   queryMatchKey : {
     fontStyle: "italic"

commit 5cb7b0701f6edf233df53ca5eebf2042c1cc68fe

LFS-282: Fixed regressions introduced by commit 87559a8 - "Searching..." and "No results" messages were no longer displayed, - Resource id not fully visible as id no longer wraps

modules/homepage/src/main/frontend/src/themePage/Navbars/SearchBar.jsx
@@ -111,12 +111,12 @@ function SearchBar(props) {
   function QuickSearchResultHeader(props) {
     const {resultData} = props;
     return resultData && (
-      <React.Fragment>
+      <div>
         <Typography variant="body2" color="textSecondary">
           {(resultData.questionnaire?.title?.concat(' ') || '') + (resultData["jcr:primaryType"]?.replace(/lfs:/,"") || '')}
         </Typography>
         {resultData.subject?.identifier || resultData["jcr:uuid"] || ''}
-      </React.Fragment>
+      </div>
     ) || null
   }
 
@@ -135,6 +135,8 @@ function SearchBar(props) {
   }
 
   // Display a quick search result
+  // If it's a resource, show avatar, category, and title
+  // Otherwise, if it's a generic entry, simply display the name
   function QuickSearchResult(props) {
     const {resultData} = props;
     return resultData["jcr:primaryType"] && (
@@ -146,6 +148,11 @@ function SearchBar(props) {
           className={classes.dropdownItem}
         />
       </React.Fragment>
+    ) || (
+       <ListItemText
+          primary={resultData.name || ''}
+          className={classes.dropdownItem}
+        />
     ) || null
   }
 