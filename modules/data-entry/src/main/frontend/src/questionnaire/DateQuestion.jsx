1b55c363d61b3c25c20b135c6d48de466d30a8f1
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-03-09 16:25:49 -0400

    Replace TextField component displaying timezone info with a Select component displaying timezone info
    Timezone list was taken from https://en.wikipedia.org/wiki/List_of_UTC_time_offsets

--- modules/data-entry/src/main/frontend/src/questionnaire/DateQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/DateQuestion.jsx
@@ -19,7 +19,7 @@
 
 import React, { useState } from "react";
 
-import { TextField, Typography, withStyles } from "@material-ui/core";
+import { TextField, Typography, withStyles, MenuItem, Select } from "@material-ui/core";
 
 import PropTypes from "prop-types";
 import moment from "moment";
@@ -217,9 +217,46 @@ function DateQuestion(props) {
         }
         value={outputDateString}
       />
-      <TextField
-        value={timezoneValue}
-      />
+      <Select value={timezoneValue}>
+        <MenuItem value="GMT-12:00">GMT-12:00</MenuItem>
+        <MenuItem value="GMT-11:00">GMT-11:00</MenuItem>
+        <MenuItem value="GMT-10:00">GMT-10:00</MenuItem>
+        <MenuItem value="GMT-09:30">GMT-09:30</MenuItem>
+        <MenuItem value="GMT-09:00">GMT-09:00</MenuItem>
+        <MenuItem value="GMT-08:00">GMT-08:00</MenuItem>
+        <MenuItem value="GMT-07:00">GMT-07:00</MenuItem>
+        <MenuItem value="GMT-06:00">GMT-06:00</MenuItem>
+        <MenuItem value="GMT-05:00">GMT-05:00</MenuItem>
+        <MenuItem value="GMT-04:00">GMT-04:00</MenuItem>
+        <MenuItem value="GMT-03:30">GMT-03:30</MenuItem>
+        <MenuItem value="GMT-03:00">GMT-03:00</MenuItem>
+        <MenuItem value="GMT-02:00">GMT-02:00</MenuItem>
+        <MenuItem value="GMT-01:00">GMT-01:00</MenuItem>
+        <MenuItem value="GMT">GMT</MenuItem>
+        <MenuItem value="GMT+01:00">GMT+01:00</MenuItem>
+        <MenuItem value="GMT+02:00">GMT+02:00</MenuItem>
+        <MenuItem value="GMT+03:00">GMT+03:00</MenuItem>
+        <MenuItem value="GMT+03:30">GMT+03:30</MenuItem>
+        <MenuItem value="GMT+04:00">GMT+04:00</MenuItem>
+        <MenuItem value="GMT+04:30">GMT+04:30</MenuItem>
+        <MenuItem value="GMT+05:00">GMT+05:00</MenuItem>
+        <MenuItem value="GMT+05:30">GMT+05:30</MenuItem>
+        <MenuItem value="GMT+05:45">GMT+05:45</MenuItem>
+        <MenuItem value="GMT+06:00">GMT+06:00</MenuItem>
+        <MenuItem value="GMT+06:30">GMT+06:30</MenuItem>
+        <MenuItem value="GMT+07:00">GMT+07:00</MenuItem>
+        <MenuItem value="GMT+08:00">GMT+08:00</MenuItem>
+        <MenuItem value="GMT+08:45">GMT+08:45</MenuItem>
+        <MenuItem value="GMT+09:00">GMT+09:00</MenuItem>
+        <MenuItem value="GMT+09:30">GMT+09:30</MenuItem>
+        <MenuItem value="GMT+10:00">GMT+10:00</MenuItem>
+        <MenuItem value="GMT+10:30">GMT+10:30</MenuItem>
+        <MenuItem value="GMT+11:00">GMT+11:00</MenuItem>
+        <MenuItem value="GMT+12:00">GMT+12:00</MenuItem>
+        <MenuItem value="GMT+12:45">GMT+12:45</MenuItem>
+        <MenuItem value="GMT+13:00">GMT+13:00</MenuItem>
+        <MenuItem value="GMT+14:00">GMT+14:00</MenuItem>
+      </Select>
       { /* If this is an interval, allow the user to select a second date */
       type === INTERVAL_TYPE &&
       <React.Fragment>
