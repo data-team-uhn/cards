262ff6a7b30be575d23d6fd735b2b8b7f62edf62
Anusha Dey <aunosua00@gmail.com>
2020-06-01 13:39:45 -0400

    refactored, added new recursive component to handle related subjects, start adding styling

--- modules/data-entry/src/main/frontend/src/questionnaire/Subject.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/Subject.jsx
@@ -17,10 +17,11 @@
 //  under the License.
 //
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
 import { Link } from 'react-router-dom';
+import QuestionnaireStyle from "./QuestionnaireStyle.jsx";
 
 import {
   CircularProgress,
@@ -38,124 +39,48 @@ const QUESTION_TYPES = ["lfs:Question"];
 const SECTION_TYPES = ["lfs:Section"];
 const ENTRY_TYPES = QUESTION_TYPES.concat(SECTION_TYPES);
 
+// TODO: new recursive subject component
+// function SubjectWrapper(props) {
+//   let { id, classes, level } = props;
+//   // hold related subjects
+//   let [ relatedSubjects, setRelatedSubjects ] = useState();
 
-// FIX: not rendering boolean/sections correctly. maybe place in formentry 
-let displayQuestion = (questionDefinition, path, existingAnswer, key, classes) => {
+//   //TODO: fetch related subjects --> setRelatedSubjects. get the id's of each related subject ? need to somehow loop through
 
-  const existingQuestionAnswer = existingAnswer && Object.entries(existingAnswer)
-    .find(([key, value]) => value["sling:resourceSuperType"] == "lfs/Answer"
-      && value["question"]["jcr:uuid"] === questionDefinition["jcr:uuid"]);
+//   const currentLevel = level || 0;
 
-  // question title, to be used when 'previewing' the form
-  const questionTitle = questionDefinition["text"];
+//   //currentLevel will also decide styling (header style, etc)
 
-  let content = null;
-
-  if (existingQuestionAnswer && existingQuestionAnswer[1]["value"]) {
-    content = `${questionTitle}: ${existingQuestionAnswer[1]["value"]}`
-  }
-
-  return (
-    <Typography variant="body2" component="p">{content}</Typography>
-  );
-};
-
-function SubjectEntry(props) {
-  let { classes, entryDefinition, path, depth, existingAnswers, keyProp } = props;
-  if (QUESTION_TYPES.includes(entryDefinition["jcr:primaryType"])) {
-    return displayQuestion(entryDefinition, path, existingAnswers, keyProp, classes);
-  }
-  // TODO: handle filtering to get the questions from the section --> displayQuestion
-  else return null;
-}
-
-  function FormData(props) {
-    let { classes, formID, maxDisplayed } = props; // set maxDisplayed default to 2
-    // This holds the full form JSON, once it is received from the server
-    let [ data, setData ] = useState();
-
-    let getFormData = (formID) => {
-      fetch(`/Forms/${formID}.deep.json`)
-          .then((response) => response.ok ? response.json() : Promise.reject(response))
-          .then((json) => setData(json))
-          .catch(handleFormError)
-    }
-
-    let handleFormError = (response) => {
-      // setError(response); // TODO: display for error handling
-      setData([]);  // Prevent an infinite loop if data was not set
-    };
-
-    if (!data) {
-      getFormData(formID);
-      return (
-        <Grid container justify="center"><Grid item><CircularProgress/></Grid></Grid>
-      );
-    }
-
-    // TODO: rearrange: if data --> object.entries --> return the 'subject entry' - no need for separate component
-
-    // if form is not filled out, should display 'No data yet'
-
-    // console.log(data.questionnaire);
-
-    // if (data && data.questionnaire) {
-    //   return (
-    //     <React.Fragment>
-    //       {
-    //         Object.entries(data.questionnaire)
-    //         .filter(([key, value]) => ENTRY_TYPES.includes(value['jcr:primaryType']))
-    //         .slice(0, maxDisplayed) // should it be sliced here or just display first filled values
-    //         .map(([key, entryDefinition]) => displayData(key, entryDefinition, data))
-    //       }
-    //     </React.Fragment>
-    //   );
- 
-    // }
-
-    // //fix error: displaydata is not a function??
-    // let displayData = (key, entryDefinition, existingAnswer) => {
-    //   if (QUESTION_TYPES.includes(entryDefinition["jcr:primaryType"])) {
-
-    //     const existingQuestionAnswer = existingAnswer && Object.entries(existingAnswer)
-    //     .find(([key, value]) => value["sling:resourceSuperType"] == "lfs/Answer"
-    //       && value["question"]["jcr:uuid"] === entryDefinition["jcr:uuid"]);
-
-    //     // question title, to be used when 'previewing' the form
-    //     const questionTitle = entryDefinition["text"];
-
-    //     let content = null;
-
-    //     if (existingQuestionAnswer && existingQuestionAnswer[1]["value"]) {
-    //       content = `${questionTitle}: ${existingQuestionAnswer[1]["value"]}`
-    //     }
-
-    //     return (
-    //       <Typography variant="body2" component="p" key={key}>{content}</Typography>
-    //     );
-    //   }
-    //   // else if (SECTION_TYPES.includes(entryDefinition["jcr:primaryType"])) {
-    //   //   // get the questions from within the section and call the same displayData function
-    //   // }
-    // }
-
-    // return (
-    //   <Typography>Form not found</Typography> // retry button?
-    // );
-
-    return (
-      <React.Fragment>
-        {data && data.questionnaire ?
-          (Object.entries(data.questionnaire)
-            .filter(([key, value]) => ENTRY_TYPES.includes(value['jcr:primaryType']))
-            .slice(0, maxDisplayed) // should it be sliced here or just display first filled values
-            .map(([key, entryDefinition]) => <SubjectEntry key={key} entryDefinition={entryDefinition} path={"."} depth={0} existingAnswers={data} keyProp={key} classes={classes}></SubjectEntry>)
-        )
-        : <Typography>Form not found</Typography> // retry button?
-        }
-      </React.Fragment>
-    );
-  }
+//   return (
+//     <React.Fragment>
+//       {/* title goes in this component */}
+//       <Grid item>
+//         {
+//           data && data.identifier ?
+//             <Typography variant="h2">SubjectType {data.identifier}</Typography>
+//           : <Typography variant="h2">SubjectType {id}</Typography>
+//         }
+//         {
+//           data && data['jcr:createdBy'] && data['jcr:created'] ?
+//           <Typography variant="overline">Entered by {data['jcr:createdBy']} on {moment(data['jcr:created']).format("dddd, MMMM Do YYYY")}</Typography>
+//           : ""
+//         }
+//       </Grid>
+//       {/* place the following in its own grid ? */}
+//       {/* return the current subject AND it's related subjects. then calls this component again, each previously related subject is returned with THEIR related subjects.  */}
+//       {id.map((subject, i) => {
+//         return (
+//           <Grid item key={`level-${currentLevel}-${i}`}>
+//             <Subject id={id}/> 
+//             {/* set props for above subject (render as usual) */}
+//             {relatedSubjects && <SubjectWrapper id={id.relatedSubjects} level={currentLevel+1}/>}
+//           </Grid>
+//         )
+//       })}
+      
+//     </React.Fragment>
+//   );
+// }
 
 /**
  * Component that displays a Subject.
@@ -171,16 +96,8 @@ function Subject (props) {
   let [ data, setData ] = useState();
   // Error message set when fetching the data from the server fails
   let [ error, setError ] = useState();
-  // table data: related forms
-  const [tableData, setTableData] = useState();
-
-  const [fetchStatus, setFetchStatus] = useState(
-    {
-      "currentRequestNumber": -1,
-      "currentFetch": false,
-      "fetchError": false,
-    }
-  );
+  // table data: related forms to the subject
+  let [tableData, setTableData] = useState();
 
   // Fetch the subject's data as JSON from the server.
   // The data will contain the subject metadata,
@@ -212,6 +129,33 @@ function Subject (props) {
     );
   }
 
+  const customUrl='/Forms.paginate?fieldname=subject&fieldvalue='
+        + encodeURIComponent(data['jcr:uuid']);
+  
+  let fetchTableData = () => {
+    fetch(customUrl)
+    .then((response) => response.ok ? response.json() : Promise.reject(response))
+    .then(handleTableResponse)
+    .catch(handleTableError);
+  };
+
+  let handleTableResponse = (json) => {
+    setTableData(json.rows);
+  };
+
+  let handleTableError = (response) => {
+    setError(response);
+    setTableData([]);
+  };
+
+  // If the data has not yet been fetched, return an in-progress symbol
+  if (!tableData) {
+    fetchTableData();
+    return (
+      <Grid container justify="center"><Grid item><CircularProgress/></Grid></Grid>
+    );
+  }
+
   // If an error was returned, do not display a subject at all, but report the error
   if (error) {
     return (
@@ -223,66 +167,7 @@ function Subject (props) {
         </Grid>
       </Grid>
     );
-  }
-
-  const customUrl='/Forms.paginate?fieldname=subject&fieldvalue='
-        + encodeURIComponent(data['jcr:uuid']);
-  
-  // todo later:  get subjects related to current subject
-  //              if subject has related subjects, rerender a livetable with formPreview=true for each related subject
-  //              make subject recursive somehow?
-  // todo later: add 'subject type' button
-
-  // FETCHING THE FORMS RELATED TO SUBJECTS
-
-  const urlBase = (
-    customUrl ?
-      new URL(customUrl, window.location.origin)
-    :
-      new URL(window.location.pathname.substring(window.location.pathname.lastIndexOf("/")) + ".paginate", window.location.origin)
-  );
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Define the component's behavior
-
-  let fetchSubjectData = () => {
-    if (fetchStatus.currentFetch) {
-      // TODO: abort previous request
-    }
-
-    let url = new URL(urlBase);
-
-    url.searchParams.set("req", ++fetchStatus.currentRequestNumber);
-
-    let currentFetch = fetch(url);
-    setFetchStatus(Object.assign({}, fetchStatus, {
-      "currentFetch": currentFetch,
-      "fetchError": false,
-    }));
-    currentFetch.then((response) => response.ok ? response.json() : Promise.reject(response)).then(handleSubjectResponse).catch(handleSubjectError);
-  };
-
-  let handleSubjectResponse = (json) => {
-    if (+json.req !== fetchStatus.currentRequestNumber) {
-      // This is the response for an older request. Discard it, wait for the right one.
-      return;
-    }
-    console.log(json.rows);
-    setTableData(json.rows);
-  };
-
-  let handleSubjectError = (response) => {
-    setFetchStatus(Object.assign({}, fetchStatus, {
-      "currentFetch": false,
-      "fetchError": (response.statusText ? response.statusText : response.toString()),
-    }));
-    setTableData();
-  }; // TODO: handle all errors the same?
-
-  // initialize fetch
-  if (fetchStatus.currentRequestNumber == -1) {
-    fetchSubjectData();
-  }
+  }  
 
   return (
     <React.Fragment>
@@ -308,7 +193,6 @@ function Subject (props) {
                       title={
                         <Link to={"/content.html" + entry["@path"]}>
                         <Button size="small"> 
-                          {/* classname styling - import from livetable styling? or add new styles to questionnaire */}
                           {entry.questionnaire["@name"]}
                         </Button>
                       </Link>
@@ -317,7 +201,7 @@ function Subject (props) {
                     <CardContent>
                       <FormData formID={entry["@name"]} maxDisplayed={4}/>
                       <Link to={"/content.html" + entry["@path"]}>
-                        <Button size="small">See More...</Button>
+                        <Typography variant="body2" component="p">See More...</Typography>
                       </Link>
                     </CardContent>
                   </Card>
@@ -325,14 +209,91 @@ function Subject (props) {
               )
             })}
           </Grid>
-          ) : <Typography>Loading...</Typography>
+          ) : <Typography variant="body2" component="p">Loading...</Typography>
         }
     </React.Fragment>
   );
 };
 
+let displayQuestion = (questionDefinition, existingAnswer) => {
+
+  // TODO: section (get questions --> displayQuestion)
+
+  const existingQuestionAnswer = existingAnswer && Object.entries(existingAnswer)
+    .find(([key, value]) => value["sling:resourceSuperType"] == "lfs/Answer"
+      && value["question"]["jcr:uuid"] === questionDefinition["jcr:uuid"]);
+
+  // question title, to be used when 'previewing' the form
+  const questionTitle = questionDefinition["text"];
+
+  let content = null;
+
+  if (existingQuestionAnswer && existingQuestionAnswer[1]["value"]) {
+    content = `${questionTitle}: ${existingQuestionAnswer[1]["value"]}`
+  }
+
+  return (
+    <Typography variant="body2" component="p">{content}</Typography>
+  );
+};
+
+// Component that displays a preview of the saved form answers
+function FormData(props) {
+  let { classes, formID, maxDisplayed } = props; // todo: set maxDisplayed default to 2
+  // This holds the full form JSON, once it is received from the server
+  let [ data, setData ] = useState();
+
+  let getFormData = (formID) => {
+    fetch(`/Forms/${formID}.deep.json`)
+        .then((response) => response.ok ? response.json() : Promise.reject(response))
+        .then((json) => setData(json))
+        .catch(handleFormError)
+  }
+
+  let handleFormError = (response) => {
+    setError(response);
+    setData([]);  // Prevent an infinite loop if data was not set
+  };
+
+  if (!data) {
+    getFormData(formID);
+    return (
+      <Grid container justify="center"><Grid item><CircularProgress/></Grid></Grid>
+    );
+  }
+
+  if (error) {
+    return (
+      <Typography variant="h2" color="error">
+        Error obtaining form data: {error.status} {error.statusText}
+      </Typography>
+    );
+  } 
+
+  console.log(data.questionnaire);
+
+  if (data && data.questionnaire) {
+    return (
+      <React.Fragment>
+        {
+          Object.entries(data.questionnaire)
+          .filter(([key, value]) => ENTRY_TYPES.includes(value['jcr:primaryType']))
+          .slice(0, maxDisplayed) // should it be sliced here or just display first filled values
+          .map(([key, entryDefinition]) => displayQuestion(entryDefinition, data))
+        }
+      </React.Fragment>
+    );
+
+  }
+  //TODO fix: return this if nothing was returned (not working)
+  else return (
+    <Typography variant="body2" component="p">No data inputted yet</Typography> 
+  );
+}
+
+
 Subject.propTypes = {
   id: PropTypes.string
 }
 
-export default Subject;
+export default withStyles(QuestionnaireStyle)(Subject);
