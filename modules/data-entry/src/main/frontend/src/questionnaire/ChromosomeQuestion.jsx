4d65aef6561b7040ebed3f0ce3c47cd80a9b3745
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-06-04 13:13:29 -0400

    LFS-444: New question type: chromosome Add simplified implementation of ChromosomeQuestion.jsx which functions simply as text entry but saves as data type lfs:ChromosomeAnswer

---
+++ modules/data-entry/src/main/frontend/src/questionnaire/ChromosomeQuestion.jsx
@@ -0,0 +1,81 @@
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+
+import React, { useState } from "react";
+
+import { Typography, withStyles } from "@material-ui/core";
+
+import PropTypes from "prop-types";
+
+import MultipleChoice from "./MultipleChoice";
+import Question from "./Question";
+import QuestionnaireStyle from "./QuestionnaireStyle";
+
+import AnswerComponentManager from "./AnswerComponentManager";
+
+// Component that renders a multiple choice question, with the choices being
+// the set of chromosomes in an organism.
+// Selected answers are placed in a series of <input type="hidden"> tags for
+// submission.
+//
+// Optional arguments:
+//  max: Integer denoting maximum number of arguments that may be selected
+//  min: Integer denoting minimum number of arguments that may be selected
+//  text: String containing the question to ask
+//
+// sample usage:
+// <ChromosomeQuestion
+//    text="Test text question (lowercase only)"
+//    />
+function ChromosomeQuestion(props) {
+  let { ...rest } = props;
+
+  return (
+    <Question
+      {...rest}
+      >
+      <MultipleChoice
+        answerNodeType="lfs:ChromosomeAnswer"
+        input="input"
+        textbox="textbox"
+        {...rest}
+        />
+    </Question>);
+}
+
+ChromosomeQuestion.propTypes = {
+  classes: PropTypes.object.isRequired,
+  questionDefinition: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    minAnswers: PropTypes.number,
+    maxAnswers: PropTypes.number,
+  }).isRequired,
+  text: PropTypes.string,
+  minAnswers: PropTypes.number,
+  maxAnswers: PropTypes.number
+};
+
+const StyledChromosomeQuestion = withStyles(QuestionnaireStyle)(ChromosomeQuestion)
+export default StyledChromosomeQuestion;
+
+AnswerComponentManager.registerAnswerComponent((questionDefinition) => {
+  if (questionDefinition.dataType === "chromosome") {
+    return [StyledChromosomeQuestion, 50];
+  }
+});
