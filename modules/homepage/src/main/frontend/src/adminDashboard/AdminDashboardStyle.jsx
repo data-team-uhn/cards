0a0a7e22a8feeb5eca6001088e4eec381973ed1b
Anusha Dey <aunosua00@gmail.com>
2020-06-29 13:34:02 -0400

    add styling to dashboard

---
+++ modules/homepage/src/main/frontend/src/adminDashboard/AdminDashboardStyle.jsx
@@ -0,0 +1,26 @@
+/*!
+=========================================================
+* Material Dashboard React - v1.7.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { blackColor } from "../themeStyle.jsx"
+
+const adminStyle = theme => ({
+  listItem: {
+    textDecoration: "none",
+  },
+  listText: {
+    "&:hover,&:focus,&:visited,&": {
+      color: blackColor
+    }
+  }
+})
+
+export default adminStyle;
