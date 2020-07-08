45fb183b8b633743e1c1fd7f0a560aa6f8176f00
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-07-08 15:02:03 -0400

    Fix LFS-487 bug

--- modules/data-entry/src/main/frontend/src/questionnaire/NumberQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/NumberQuestion.jsx
@@ -75,7 +75,8 @@ const DATA_TO_VALUE_TYPE = {
 //    />
 function NumberQuestion(props) {
   const { existingAnswer, errorText, isRange, classes, ...rest} = props;
-  const { text, dataType, displayMode, minValue, maxValue } = {...props.questionDefinition, ...props};
+  const { text, displayMode, minValue, maxValue } = {...props.questionDefinition, ...props};
+  const dataType = props.questionDefinition.dataType;
   const answerNodeType = props.answerNodeType || DATA_TO_NODE_TYPE[dataType];
   const valueType = props.valueType || DATA_TO_VALUE_TYPE[dataType];
   const [error, setError] = useState(false);
