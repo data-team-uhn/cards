d333fa4343bced5809d46463bcb96e81e15ef320
OrdiNeu <OrdinaryNeumann@gmail.com>
2020-08-21 16:36:55 -0400

    LFS-172: As an admin, I can import, visualize and design questionnaires Improve error reporting

--- modules/data-entry/src/main/frontend/src/questionnaireEditor/DeleteQuestionnaireDialog.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaireEditor/DeleteQuestionnaireDialog.jsx
@@ -23,72 +23,76 @@ import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography, 
 import QuestionnaireStyle from "../questionnaire/QuestionnaireStyle.jsx";
 
 let DeleteQuestionnaireDialog = (props) => {
-    let [ forms, setForms ] = useState(0);
-    let [ lastSaveStatus, setLastSaveStatus ] = useState(undefined);
-    let [ saveInProgress, setSaveInProgress ] = useState();
-    
-    let deleteQuestionnaireWarningMessage = () => {
-      let formsExist = forms && forms > 0;
-      if (!formsExist) {
-        // Find all forms with that questionnaire uuid
-        fetch('/query?query=' + encodeURIComponent(`select * from [lfs:Form] as n WHERE n.'questionnaire'='${props.data['jcr:uuid']}'`))
-          .then((response) => response.ok ? response.json() : Promise.reject(response))
-          .then((json) => { parseResult(json); });
-      }
-      // Count the number of returned forms
-      let parseResult = (forms) => {
-        let filteredForms = Object.values(forms['rows']).length;
-        setForms(filteredForms);
-        formsExist = forms && forms > 0;
-     }
-      return formsExist
-        ? `There are ${forms} forms filled out for this questionnaire. Are you sure you wish to proceed?`
-        : "Are you sure you wish to proceed?"
+  let { data, id, onClose, onError, open } = props;
+  let [ forms, setForms ] = useState(0);
+  let [ lastSaveStatus, setLastSaveStatus ] = useState(undefined);
+  let [ saveInProgress, setSaveInProgress ] = useState();
+  
+  let deleteQuestionnaireWarningMessage = () => {
+    let formsExist = forms && forms > 0;
+    if (!formsExist) {
+      // Find all forms with that questionnaire uuid
+      fetch('/query?query=' + encodeURIComponent(`select * from [lfs:Form] as n WHERE n.'questionnaire'='${data['jcr:uuid']}'`))
+        .then((response) => response.ok ? response.json() : Promise.reject(response))
+        .then((json) => { parseResult(json); });
     }
-  
-    let deleteQuestionnaire = () => {
-      event.preventDefault();
-  
-      // If the previous save attempt failed, instead of trying to save again, open a login popup
-      if (lastSaveStatus === false) {
-        loginToSave();
-        return;
-      }
-  
-      setSaveInProgress(true);
-  
-      fetch(props.data["@path"], {
-        method: "DELETE",
-      }).then((response) => response.ok ? true : Promise.reject(response))
-        .then(() => setLastSaveStatus(true))
-        // FIXME Use setError?
-        .catch(() => {
-          // If the user is not logged in, offer to log in
-          const sessionInfo = window.Sling.getSessionInfo();
-          if (sessionInfo === null || sessionInfo.userID === 'anonymous') {
-            // On first attempt to save while logged out, set status to false to make button text inform user
-            setLastSaveStatus(false);
-          }
-        })
-      .finally(() => setSaveInProgress(false));
-      props.onClose();
+    // Count the number of returned forms
+    let parseResult = (forms) => {
+      let filteredForms = Object.values(forms['rows']).length;
+      setForms(filteredForms);
+      formsExist = forms && forms > 0;
     }
-  
-    let loginToSave = () => {
-      const width = 600;
-      const height = 800;
-      const top = window.top.outerHeight / 2 + window.top.screenY - (height / 2);
-      const left = window.top.outerWidth / 2 + window.top.screenX - (width / 2);
-      // After a successful log in, the login dialog code will "open" the specified resource, which results in executing the specified javascript code
-      window.open("/login.html?resource=javascript%3Awindow.close()", "loginPopup", `width=${width}, height=${height}, top=${top}, left=${left}`);
-      // Display 'save' on button
-      setLastSaveStatus(undefined);
+    return formsExist
+      ? `There are ${forms} forms filled out for this questionnaire. Are you sure you wish to proceed?`
+      : "Are you sure you wish to proceed?"
+  }
+
+  let deleteQuestionnaire = (event) => {
+    event.preventDefault();
+
+    // If the previous save attempt failed, instead of trying to save again, open a login popup
+    if (lastSaveStatus === false) {
+      loginToSave();
+      return;
     }
+
+    setSaveInProgress(true);
+
+    fetch(data["@path"], {
+      method: "DELETE",
+    }).then((response) => response.ok ? true : Promise.reject(response))
+      .then(() => setLastSaveStatus(true))
+      .catch((error) => {
+        // If the user is not logged in, offer to log in
+        const sessionInfo = window.Sling.getSessionInfo();
+        if (sessionInfo === null || sessionInfo.userID === 'anonymous') {
+          // On first attempt to save while logged out, set status to false to make button text inform user
+          setLastSaveStatus(false);
+        } else if (error.status == 409) {
+          onError("Cannot delete an questionnaire with active entries");
+        } else {
+          onError("Fetch error: " + error.status);
+        }
+      })
+    .finally(() => setSaveInProgress(false));
+    onClose();
+  }
+
+  let loginToSave = () => {
+    const width = 600;
+    const height = 800;
+    const top = window.top.outerHeight / 2 + window.top.screenY - (height / 2);
+    const left = window.top.outerWidth / 2 + window.top.screenX - (width / 2);
+    // After a successful log in, the login dialog code will "open" the specified resource, which results in executing the specified javascript code
+    window.open("/login.html?resource=javascript%3Awindow.close()", "loginPopup", `width=${width}, height=${height}, top=${top}, left=${left}`);
+    // Display 'save' on button
+    setLastSaveStatus(undefined);
+  }
   
   return (
     <React.Fragment>
-      <Dialog id="deleteDialog" open={props.open} onClose={props.onClose}>
-        <form action={props.data["@path"]} onSubmit={deleteQuestionnaire} method="DELETE" key={props.id}>
+      <Dialog id="deleteDialog" open={open} onClose={onClose}>
+        <form action={data["@path"]} onSubmit={deleteQuestionnaire} method="DELETE" key={id}>
           <DialogTitle>
             Confirm questionnaire deletion
           </DialogTitle>
@@ -97,18 +101,22 @@ let DeleteQuestionnaireDialog = (props) => {
           </DialogContent>
           <DialogActions>
             <Button
+              color="secondary"
+              disabled={saveInProgress}
               type="submit"
               variant="contained"
-              color="secondary"
               >
-              {'Yes, delete'}
+              {saveInProgress ? 'Deleting' :
+              lastSaveStatus === false ? 'Deletion failed, log in and try again?' :
+              'Yes, delete'}
             </Button>
             <Button
-              variant="contained"
               color="default"
-              onClick={props.onClose}
+              disabled={saveInProgress}
+              onClick={onClose}
+              variant="contained"
               >
-              {'Cancel'}
+              Cancel
             </Button>
           </DialogActions>
         </form>
