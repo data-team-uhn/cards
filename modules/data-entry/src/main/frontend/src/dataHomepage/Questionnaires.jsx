69e24ba94e0f5c5a427adea24957672b86221a3d
OrdiNeu <OrdinaryNeumann@gmail.com>
2020-09-24 13:40:06 -0400

    Revert "LFS-172: Add Actions to LiveTable"
    This reverts commit 7219dbb487e6168fd10d1ca33f08a0e12e03b327.

--- modules/data-entry/src/main/frontend/src/dataHomepage/Questionnaires.jsx
+++ modules/data-entry/src/main/frontend/src/dataHomepage/Questionnaires.jsx
@@ -21,15 +21,44 @@ import LiveTable from "./LiveTable.jsx";
 import Questionnaire from "../questionnaire/Questionnaire.jsx";
 import QuestionnaireStyle from "../questionnaire/QuestionnaireStyle";
 import NewQuestionnaireDialog from "./NewQuestionnaireDialog.jsx";
-import { Button, Card, CardHeader, CardContent, withStyles } from "@material-ui/core";
+import { Card, CardHeader, Button, CardContent, withStyles } from "@material-ui/core";
+
 
 function Questionnaires(props) {
   let [ forms, setForms ] = useState({});
+  let [ uuid, setUuid] = useState("")
   const { match, classes } = props;
   const entry = /Questionnaires\/(.+)/.exec(location.pathname);
+  
+  let getFormCount = (id) => {
+    // Get uuid of questionnaire
+    fetch(`/Questionnaires/${id}.deep.json`)
+    .then((response) => response.ok ? response.json() : Promise.reject(response))
+    .then((response) => { setUuid(response['jcr:uuid']) });
+
+    // Find all forms with that questionnaire uuid
+    fetch('/query?query=' + encodeURIComponent(`select * from [lfs:Form] as n WHERE n.'questionnaire'='${uuid}'`))
+      .then((response) => response.ok ? response.json() : Promise.reject(response))
+      .then((json) => { parseResult(json, id); });
+   }
+
+   // Count the number of returned forms
+   let parseResult = (forms, id) => {
+    let filteredForms = Object.values(forms['rows']).length;
+    setForms(forms.push({ id, filteredForms }));
+   }
 
   if (entry) {
     return <Questionnaire id={entry[1]} key={location.pathname}/>;
+  }
+
+  if (!forms) {
+    getFormCount(columns.title);
+    columns.push({
+      "key": `${forms[columns.title]}`,
+      "label": "Forms",
+      "format": "string",
+    });
   }
   
   let columns = [
@@ -50,12 +79,6 @@ function Questionnaires(props) {
       "label": "Created on",
       "format": "date:YYYY-MM-DD HH:mm",
     },
-    {
-      "key":"jcr:uuid",
-      "label":"Actions",
-      "edit":"example",
-      "delete":"example"
-    }
   ]
   return (
     <Card>
