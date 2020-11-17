823ae0f65d1f01e896fb5d3476fde496ed622b14
OrdiNeu <OrdinaryNeumann@gmail.com>
2020-11-17 02:16:36 -0500

    LFS-670 / LFS-675: [wip] Add the file upload to a static directory

--- modules/data-entry/src/main/frontend/src/questionnaire/FileResourceQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/FileResourceQuestion.jsx
@@ -42,36 +42,61 @@ import AnswerComponentManager from "./AnswerComponentManager";
 function FileResourceQuestion(props) {
   const { classes, existingAnswer, ...rest } = props;
   const { maxAnswers, namePattern } = { ...props.questionDefinition, ...props }
-  let [ answers, setAnswers ] = useState([]);
-  let [ selectedFiles, setSelectedFiles ] = useState([]);
+  let [ uploadedFiles, setUploadedFiles ] = useState({});
   let [ error, setError ] = useState();
   let [ uploadInProgress, setUploadInProgress ] = useState(false);
   let [ uploadProgress, setUploadProgress ] = useState({});
+  let [ answerPath, setAnswerPath ] = useState("");
+
+  let answers = Object.values(uploadedFiles).map((filepath) => [filepath, filepath]);
+  console.log(answers);
 
   // Add files to the pending state
   let addFiles = (files) => {
-    console.log(files);
-    setSelectedFiles(files);
+    upload(files);
+    //setUploadedFiles(fileNames);
   }
 
+  /*
+  // Uploaded files need to be children of our answer node
+  // If the answer node doesn't yet exist, we need to create it
+  let checkOrCreateAnswer = () => {
+    return fetch(answerPath)
+      .then(response => response.ok ? response.json() : Promise.reject({error: response.status}))
+      // If there is no errors, the answer node exists. If there is an error, we need to create it
+      // before attempting to upload anything else
+      .catch(error => {
+        // Create the answer node
+        let formData = new FormData();
+        formData.append('jcr:primaryType', 'lfs:FileResourceAnswer');
+        formData.append('question', props.questionDefinition["jcr:uuid"]);
+
+        console.log("Answer created");
+        return fetch(answerPath, {
+          method: 'POST',
+          body: formData
+        });
+      });
+  }
+
+  if (answerPath != "") {
+    console.log("Here");
+    checkOrCreateAnswer();
+  }*/
+
   // Event handler for selecting files
-  let upload = (event) => {
-    // Begin 
-
+  let upload = (files) => {
     // TODO - handle possible logged out situation here - open a login popup
-
     setUploadInProgress(true);
     setUploadProgress({});
     setError("");
-
-    uploadAllFiles(selectedFiles)
+    uploadAllFiles(files)
       .then(() => {
-
         setUploadInProgress(false);
       })
       .catch( (error) => {
-
-        handleError(error);
+        console.log(error);
+        //handleError(error);
         setUploadInProgress(false);
     });
   };
@@ -79,25 +104,52 @@ function FileResourceQuestion(props) {
   // Find the icon and load them
   let uploadAllFiles = (selectedFiles) => {
     const promises = [];
-    selectedFiles.forEach(file => {
+    for (let i = 0; i < selectedFiles.length; i++) {
+      promises.push(uploadSingleFile(selectedFiles[i]));
+    }
+    /*selectedFiles.forEach(file => {
+      console.log("V");
       promises.push(uploadSingleFile(file));
-    });
+    });*/
 
     return Promise.all(promises);
   };
 
+  console.log(uploadedFiles);
   let uploadSingleFile = (file) => {
-    return new Promise((resolve, reject) => {
+    console.log(file);
+    console.log("E");
+    // TODO: Prevent duplicate filenames
+    let data = new FormData();
+    data.append(file['name'], file);
+    data.append('jcr:primaryType', 'nt:folder');
+    return fetch("/Uploads/", {
+      method: "POST",
+      body: data
+    }).then((response) =>
+      response.ok ? response.text() : Promise.reject(response)
+    ).then((response) => {
+      // Determine what the output filename is, and save it
+      let uploadFinder = /Content created (.+)<\/title>/;
+      let match = response.match(uploadFinder);
+      uploadedFiles[file["name"]] = match[1];
+      setUploadedFiles(uploadedFiles);
+    }).catch((errorObj) => {
+      // Is the user logged out? Or did something else happen?
+      setError(String(errorObj));
+    });
+
+    /*return new Promise((resolve, reject) => {
 
       var reader = new FileReader();
       reader.readAsText(file);
 
       //When the file finishes loading
+      console.log("C");
       reader.onload = function(event) {
 
         // get the file data
         var csv = event.target.result;
-        let json = assembleJson(file, csv);
 
         let data = new FormData();
         data.append(':contentType', 'json');
@@ -105,10 +157,11 @@ function FileResourceQuestion(props) {
         data.append(':content', JSON.stringify(json));
 
         var xhr = new XMLHttpRequest();
+        // TODO: Figure out where to store this file
         xhr.open('POST', '/');
 
         xhr.onload = function() {
-
+          console.log("Upload complete");
           if (xhr.status != 201) {
             uploadProgress[file.name] = { state: "error", percentage: 0 };
             console.log("Error", xhr.statusText)
@@ -117,8 +170,8 @@ function FileResourceQuestion(props) {
             uploadProgress[file.name] = { state: "done", percentage: 100 };
 
             file.formPath = "/" + Object.keys(json).find(str => str.startsWith("Forms/"));
-            selectedFiles[selectedFiles.findIndex(el => el.name === file.name)] = file;
-            setSelectedFiles(selectedFiles);
+            uploadedFiles[uploadedFiles.findIndex(el => el.name === file.name)] = file;
+            setUploadedFiles(uploadedFiles);
           }
 
           setUploadProgress(uploadProgress);
@@ -144,7 +197,7 @@ function FileResourceQuestion(props) {
         }
         xhr.send(data);
       }
-    });
+    });*/
   };
 
   return (
@@ -165,7 +218,7 @@ function FileResourceQuestion(props) {
 
       <input type="hidden" name="*@TypeHint" value="nt:file" />
       <label htmlFor="contained-button-file">
-        <Button onClick={upload} variant="contained" color="primary" disabled={uploadInProgress || !!error && selectedFiles.length == 0} className={classes.uploadButton}>
+        <Button onClick={upload} variant="contained" color="primary" disabled={uploadInProgress || !!error && uploadedFiles.length == 0} className={classes.uploadButton}>
           <span><BackupIcon className={classes.buttonIcon}/>
             {uploadInProgress ? 'Uploading' :
                 // TODO - judge upload status button message over all upload statuses of all files ??
@@ -175,10 +228,10 @@ function FileResourceQuestion(props) {
           </span>
         </Button>
       </label>
-      { selectedFiles && selectedFiles.length > 0 && <span>
+      { /*uploadedFiles && uploadedFiles.length > 0 && <span>
       <Typography variant="h6" className={classes.fileInfo}>Selected files info</Typography>
 
-      { selectedFiles.map( (file, i) => {
+      { uploadedFiles.map( (file, i) => {
 
           const upprogress = uploadProgress ? uploadProgress[file.name] : null;
 
@@ -233,13 +286,14 @@ function FileResourceQuestion(props) {
               </Typography>
             </div>
         ) } ) }
-      </span> }
+                  </span> */}
       <Answer
         answers={answers}
         questionDefinition={props.questionDefinition}
         existingAnswer={existingAnswer}
         answerNodeType="lfs:FileResourceAnswer"
         valueType="path"
+        onDecidedOutputPath={setAnswerPath}
         {...rest}
         />
     </Question>);
