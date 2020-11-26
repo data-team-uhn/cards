d679e1c4dc42363d02508f91f0e38ac51b7ec026
Marta Girdea <marta@phenotips.org>
2020-11-25 20:02:41 -0500

    LFS-272: Forms are periodically saved automatically as the user fills out information Reorganize effects

--- modules/data-entry/src/main/frontend/src/questionnaire/Form.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/Form.jsx
@@ -99,19 +99,21 @@ function Form (props) {
 
   let formNode = React.useRef();
 
-  useEffect(() => {
-    let backgroundSave = () => {
-      if (!saveInProgress && lastSaveStatus === undefined) {
-        saveData();
-      }
+  let backgroundSave = () => {
+    if (!saveInProgress && lastSaveStatus === undefined) {
+      saveData();
     }
+  }
+
+  useEffect(backgroundSave, [lastSaveStatus]);
+
+  useEffect(() => {
     window.addEventListener("beforeunload", backgroundSave);
-    backgroundSave();
     // When component unmounts:
     return (() => {
       window.removeEventListener("beforeunload", backgroundSave);
     });
-  }, [lastSaveStatus]);
+  }, []);
 
   // Fetch the form's data as JSON from the server.
   // The data will contain the form metadata,
