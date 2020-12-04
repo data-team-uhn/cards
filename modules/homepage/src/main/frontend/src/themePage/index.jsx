0d723db9a7b3748769af6de8a2e978d0be439076
OrdiNeu <OrdinaryNeumann@gmail.com>
2020-12-03 23:53:47 -0500

    LFS-728: Make logo, app name, and side bar background image configurable
    Move the background to a place where it can be easily overwritten as
    well

--- modules/homepage/src/main/frontend/src/themePage/index.jsx
+++ modules/homepage/src/main/frontend/src/themePage/index.jsx
@@ -35,7 +35,7 @@ class Main extends React.Component {
     super(props);
 
     this.state = {
-      image: "/libs/lfs/resources/cancer-cells.jpg",
+      image: "/libs/lfs/resources/background.jpg",
       color: "blue",
       hasImage: true,
       fixedClasses: "dropdown show",
