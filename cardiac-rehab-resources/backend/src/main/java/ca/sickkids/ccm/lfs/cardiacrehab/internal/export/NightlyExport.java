3d4500cd8ee3c6bf0886cbd54cca5043765f6bc8
Andrew Crowther <acrowthe3388@gmail.com>
2020-12-15 14:57:44 -0500

    Testing only - LFS-752: Set export to occur every minute

--- cardiac-rehab-resources/backend/src/main/java/ca/sickkids/ccm/lfs/cardiacrehab/internal/export/NightlyExport.java
+++ cardiac-rehab-resources/backend/src/main/java/ca/sickkids/ccm/lfs/cardiacrehab/internal/export/NightlyExport.java
@@ -47,7 +47,7 @@ public class NightlyExport
     protected void activate(ComponentContext componentContext) throws Exception
     {
         LOGGER.info("NightlyExport activating");
-        ScheduleOptions options = this.scheduler.EXPR("30 23 * * * ?");
+        ScheduleOptions options = this.scheduler.EXPR("0 * * * * ?");
         options.name("NightlyExport");
         options.canRunConcurrently(true);
 
