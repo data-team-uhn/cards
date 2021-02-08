b1c9547e4391d6918bc54134d6ed4b0107d8059a
Michael Lescisin <michael.lescisin@sickkids.ca>
2021-02-08 15:24:46 -0500

    LFS-724 / LFS-926: Update jcr:lastModified and jcr:lastModifiedBy when updating a mix:lastModified node Rename last-modified-property module to versioning

--- modules/versioning/src/main/java/ca/sickkids/ccm/lfs/versioning/LastModifiedEditor.java
+++ modules/versioning/src/main/java/ca/sickkids/ccm/lfs/versioning/LastModifiedEditor.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ca.sickkids.ccm.lfs.lastmodifiedproperty;
+package ca.sickkids.ccm.lfs.versioning;
 
 import java.util.Arrays;
 import java.util.Calendar;
