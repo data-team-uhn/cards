e752ee625c7cc303b529d52f60548b22d3db98e4
veronikaslc <veronika.koltunova@sickkids.ca>
2021-02-09 06:14:27 -0500

    LFS-632: As an admin, I can update the Bioportal API key from the Admin screen Fixed backend key fetching

--- modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/VocabularyBioPortalApiKeyServlet.java
+++ modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/VocabularyBioPortalApiKeyServlet.java
@@ -22,9 +22,6 @@ import java.io.IOException;
 import java.io.Writer;
 
 import javax.jcr.Node;
-import javax.jcr.PathNotFoundException;
-import javax.jcr.RepositoryException;
-import javax.jcr.ValueFormatException;
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
 import javax.servlet.Servlet;
@@ -54,54 +51,32 @@ import org.slf4j.LoggerFactory;
 public class VocabularyBioPortalApiKeyServlet extends SlingSafeMethodsServlet
 {
     /** OS environment variable which has the api key for bioontology portal. */
-    private static final String APIKEY_ENVIRONMENT_VARIABLE =
-        "BIOPORTAL_APIKEY";
+    private static final String APIKEY_ENVIRONMENT_VARIABLE = "BIOPORTAL_APIKEY";
 
     /** The response from this service is a JSON object with this key holding the BioPortal API key as its value. */
-    private static final String RESPONSE_JSON_KEY =
-        "apikey";
+    private static final String RESPONSE_JSON_KEY = "apikey";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VocabularyIndexerServlet.class);
-
-    private ThreadLocal<Boolean> keyNodeExists = new ThreadLocal<Boolean>()
-    {
-        @Override
-        protected Boolean initialValue()
-        {
-            return Boolean.FALSE;
-        }
-    };
 
     @Override
     public void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws IOException
     {
-        // get node /libs/lfs/conf/BioportalApiKey
-        String resourcePath = "/libs/lfs/conf/BioportalApiKey";
         // req is the SlingHttpServletRequest
         ResourceResolver resourceResolver = request.getResourceResolver();
-        Resource res = resourceResolver.getResource(resourcePath);
-
-        if (res != null) {
-            this.keyNodeExists.set(Boolean.TRUE);
-        }
 
         response.setContentType("application/json");
         final Writer out = response.getWriter();
-        try (JsonGenerator jsonGen = Json.createGenerator(out)) {
-            jsonGen.writeStartObject();
-            if (this.keyNodeExists.get()) {
-                // if node exists
-                jsonGen.write(RESPONSE_JSON_KEY, this.getAPIKeyFromNode(res));
-                jsonGen.write("isNode", true);
-            } else {
-                // if node does not exist, get api key from env variable
-                jsonGen.write(RESPONSE_JSON_KEY, this.getAPIKeyFromEnvironment());
-                jsonGen.write("isNode", false);
-            }
-            jsonGen.writeEnd().flush();
-        } catch (RepositoryException e) {
-            LOGGER.error("Failed to write key: {}", e.getMessage(), e);
+        JsonGenerator jsonGen = Json.createGenerator(out);
+        jsonGen.writeStartObject();
+        String key = this.getAPIKeyFromNode(resourceResolver);
+        if (!"".equals(key)) {
+            // if node exists
+            jsonGen.write(RESPONSE_JSON_KEY, key);
+        } else {
+            // if node does not exist, get api key from env variable
+            jsonGen.write(RESPONSE_JSON_KEY, this.getAPIKeyFromEnvironment());
         }
+        jsonGen.writeEnd().flush();
     }
 
     /**
@@ -110,7 +85,7 @@ public class VocabularyBioPortalApiKeyServlet extends SlingSafeMethodsServlet
      *
      * @return BioPortal API key
      */
-    private String getAPIKeyFromEnvironment()
+    public String getAPIKeyFromEnvironment()
     {
         String apiKey = System.getenv(APIKEY_ENVIRONMENT_VARIABLE);
         apiKey = StringUtils.isBlank(apiKey) ? "" : apiKey;
@@ -118,18 +93,24 @@ public class VocabularyBioPortalApiKeyServlet extends SlingSafeMethodsServlet
         return apiKey;
     }
 
-    private String getAPIKeyFromNode(Resource res) throws RepositoryException
+    /**
+     * Retrieves BioPortal API key from the node. If the key is not specified returns an empty string.
+     *
+     * @param resourceResolver resource resolver
+     * @return BioPortal API key
+     */
+    public String getAPIKeyFromNode(ResourceResolver resourceResolver)
     {
-        String apiKey = "init";
+        String resourcePath = "/libs/lfs/conf/BioportalApiKey";
+        String apiKey = "";
 
         try {
+            Resource res = resourceResolver.getResource(resourcePath);
             Node keyNode = res.adaptTo(Node.class);
             apiKey = keyNode.getProperty("key").getString();
             LOGGER.info("BioPortal API key as set in the BioportalApiKey node: [{}]", apiKey);
-        } catch (PathNotFoundException e) {
-            LOGGER.error("Value does not exist for question: {}", e.getMessage(), e);
-        } catch (ValueFormatException e) {
-            LOGGER.error("Value does not exist for question: {}", e.getMessage(), e);
+        } catch (Exception e) {
+            LOGGER.error("Failed to load BioPortal API key from node: {}", e.getMessage(), e);
         }
         return apiKey;
     }
