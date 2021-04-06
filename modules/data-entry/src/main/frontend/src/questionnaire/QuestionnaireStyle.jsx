c5c79349d9606ca1b89ff865d7c6e085463ff159
Marta Girdea <marta@phenotips.org>
2021-04-06 18:24:08 -0400

    LFS-974: Add the ability so specify if an answer option has a None or None of the above behavior in the question editor UI adjustments

--- modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
@@ -470,22 +470,36 @@ const questionnaireStyle = theme => ({
     warningStatus: {
       color: theme.palette.warning.main
     },
+    answerOption: {
+      backgroundColor: grey[200],
+      borderRadius: theme.spacing(.5, 3, 3, .5),
+      margin: theme.spacing(1, 0),
+      "& .MuiFormControl-root" : {
+        paddingTop: theme.spacing(1),
+      },
+      "& .MuiInput-underline:before" : {
+        borderBottom: "0 none !important",
+      },
+      "& .MuiInput-underline:after" : {
+        borderBottom: "0 none !important",
+      }
+    },
     answerOptionInput: {
-      width: "78%",
-      paddingRight: theme.spacing(1),
-      paddingLeft: theme.spacing(1),
-      backgroundColor: grey[200]
+      width: "100%",
+      backgroundColor: grey[200],
+      "& .MuiInputBase-input" : {
+        paddingRight: theme.spacing(1),
+        paddingLeft: theme.spacing(1),
+      },
     },
     answerOptionDeleteButton: {
-      right: theme.spacing(-2),
+      float: "right",
     },
-    specialOption: {
+    newOptionInput: {
       marginBottom: theme.spacing(2),
-      width: "78%",
     },
     specialOptionSwitch: {
-      marginLeft: "0",
-      marginRight: "0",
+      margin: "0",
     },
 });
 
--- modules/data-entry/src/main/frontend/src/questionnaireEditor/AnswerOptions.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaireEditor/AnswerOptions.jsx
@@ -159,18 +159,26 @@ let AnswerOptions = (props) => {
   let generateSpecialOptions = (index) => {
     let option = specialOptionsInfo[index];
     return (
-    <div onClick={(event) => option.setter({ ...option.data, [option.label]: true})} >
+    <Grid container
+       direction="row"
+       justify="space-between"
+       alignItems="stretch"
+       onClick={(event) => option.setter({ ...option.data, [option.label]: true})}
+       >
+      <Grid item xs={10}>
       <Tooltip title={option.tooltip}>
         <TextField
           disabled={!option.data[option.label]}
           label={option.tootltip}
           error={option.data[option.label] && option.isDuplicate}
           helperText={option.isDuplicate ? 'duplicated value or label' : ''}
-          className={classes.specialOption + " " + classes.answerOptionInput}
+          className={classes.answerOptionInput}
           defaultValue={option.data.label? option.data.value + " = " + option.data.label : option.data.value}
           onChange={(event) => { handleSpecialInputOption(option, event.target.value); }}
         />
       </Tooltip>
+      </Grid>
+      <Grid item xs={2}>
       <Tooltip title={option.switchTooltip} className={classes.specialOptionSwitch}>
         <FormControlLabel
           control={
@@ -193,7 +201,8 @@ let AnswerOptions = (props) => {
         :
         <input type='hidden' name={`${option.data['@path']}@Delete`} value="0" />
       }
-    </div>
+      </Grid>
+    </Grid>
     )
   }
 
@@ -204,7 +213,14 @@ let AnswerOptions = (props) => {
       )}
       { generateSpecialOptions(0) }
       { options.map((value, index) =>
-        <React.Fragment key={value.value}>
+        <Grid container
+          direction="row"
+          justify="space-between"
+          alignItems="stretch"
+          className={classes.answerOption}
+          key={value.value}
+          >
+          <Grid item xs={10}>
           <input type='hidden' name={`${value['@path']}/jcr:primaryType`} value={'lfs:AnswerOption'} />
           <input type='hidden' name={`${value['@path']}/label`} value={value.label} />
           <input type='hidden' name={`${value['@path']}/value`} value={value.value} />
@@ -216,13 +232,17 @@ let AnswerOptions = (props) => {
             defaultValue={value.label? value.value + " = " + value.label : value.value}
             multiline
             />
+          </Grid>
+          <Grid item xs={2}>
           <IconButton onClick={() => { deleteOption(index); }} className={classes.answerOptionDeleteButton}>
             <CloseIcon/>
           </IconButton>
-        </React.Fragment>
+          </Grid>
+        </Grid>
       )}
       <TextField
-        className={classes.specialOption}
+        fullWidth
+        className={classes.newOptionInput}
         value={tempValue}
         error={isDuplicate}
         label="value OR value=label (e.g. F=Female)"
