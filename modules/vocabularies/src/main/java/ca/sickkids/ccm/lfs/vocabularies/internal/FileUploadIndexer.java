6d4c5ece4b101eec45d342cea652252a013834f2
Michael Lescisin <michael.lescisin@sickkids.ca>
2021-04-28 10:48:18 -0400

    WIP

--- modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/internal/FileUploadIndexer.java
+++ modules/vocabularies/src/main/java/ca/sickkids/ccm/lfs/vocabularies/internal/FileUploadIndexer.java
@@ -95,7 +95,6 @@ public class FileUploadIndexer implements VocabularyIndexer
         String vocabName = request.getParameter("vocabName");
         String overwrite = request.getParameter("overwrite");
         RequestParameter uploadedOntology = request.getRequestParameter("filename");
-        System.exit(1);
 
         // Obtain the resource of the request and adapt it to a JCR node. This must be the /Vocabularies homepage node.
         Node homepage = request.getResource().adaptTo(Node.class);
