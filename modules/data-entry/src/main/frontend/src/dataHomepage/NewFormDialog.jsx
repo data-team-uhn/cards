df4bc160619a010f83f5941a73dfe30c0b2ac642
Michael Lescisin <michael.lescisin@sickkids.ca>
2021-06-24 12:26:01 -0400

    LFS-1206: Questionnaire/Subject selector does not reset internal state when closed before completion
    Reset the dialog state, regardless of if it is a New Subject Dialog or Questionnaire / Subject selector dialog that is being closed
    
    Additionally, set the disableProgress state variable to false upon dialog state reset

--- modules/data-entry/src/main/frontend/src/dataHomepage/NewFormDialog.jsx
+++ modules/data-entry/src/main/frontend/src/dataHomepage/NewFormDialog.jsx
@@ -64,6 +64,12 @@ function NewFormDialog(props) {
 
   const globalLoginDisplay = useContext(GlobalLoginContext);
 
+  let resetDialogState = () => {
+    setSelectedQuestionnaire(null);
+    setSelectedSubject(null);
+    setDisableProgress(false);
+  }
+
   let createForm = (subject) => {
     setError("");
 
@@ -252,6 +258,7 @@ function NewFormDialog(props) {
         title={progress === PROGRESS_SELECT_QUESTIONNAIRE ? "Select a questionnaire" : "Select a subject"}
         open={mode === MODE_ACTION ? dialogOpen : open}
         onClose={() => {
+          resetDialogState();
           setDialogOpen(false);
           if (onClose) {
             onClose();
@@ -409,8 +416,7 @@ function NewFormDialog(props) {
         allowedTypes={filteredAllowedSubjectTypes}
         disabled={isFetching}
         onClose={() => {
-          setSelectedSubject(null);
-          setSelectedQuestionnaire(null);
+          resetDialogState();
           setNewSubjectPopperOpen(false);
           setError();
           if (onClose) {
