3014c188758d9ddc2b4877ee9491eb529e84bc4e
Marta Girdea <marta@phenotips.org>
2021-07-09 18:35:06 -0400

    LFS-360: Clarify the source of filter variables Minor styling improvement

--- modules/data-entry/src/main/frontend/src/dataHomepage/Filters.jsx
+++ modules/data-entry/src/main/frontend/src/dataHomepage/Filters.jsx
@@ -381,7 +381,7 @@ function Filters(props) {
       } else if (Array.isArray(name)) {
         // Arrays represent Questionnaires of Sections
         // which we'll need to turn into opt groups
-        return [<MenuItem className={classes.categoryHeader} disabled>{name[0]}</MenuItem>,
+        return [<ListSubheader className={classes.categoryHeader} color="primary">{name[0]}</ListSubheader>,
           GetReactComponentFromFields(name.slice(1), true)];
       }
     })
