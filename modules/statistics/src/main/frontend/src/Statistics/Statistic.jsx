034b5ae6613261b305aa1a4d59317c69511c7c76
Marta Girdea <marta@phenotips.org>
2021-09-04 19:20:32 -0400

    CARDS-500: As a user, I can access statistics on a dashboard * Improved the card header of each stats card: smaller title font, added an avatar dependent on chart type * Added a message when no data is available to generate a statisticm, to replace the empty chart * Re-positioned the legend * Quick fix to make the y axis label clearer (used plural)

--- modules/statistics/src/main/frontend/src/Statistics/Statistic.jsx
+++ modules/statistics/src/main/frontend/src/Statistics/Statistic.jsx
@@ -18,12 +18,18 @@
 //
 import React, { useState } from "react";
 import {
+  Avatar,
   Card,
   CardContent,
   CardHeader,
   Grid,
+  Typography,
   withStyles
 } from "@material-ui/core";
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LineChartIcon from '@material-ui/icons/ShowChart';
+import { deepPurple, indigo } from '@material-ui/core/colors';
+
 import palette from "google-palette";
 import {
    BarChart, Bar, CartesianGrid, Line, LineChart, XAxis, YAxis, Tooltip, Label, Legend,
@@ -36,7 +42,6 @@ function Statistic(props) {
   // Colours to be used before relying on the google palette
   const DEFAULT_PALETTE = [
     theme.palette.primary.main,
-    theme.palette.secondary.main
   ];
 
   // Transform our input data from the statistics servlet into something recharts can understand
@@ -65,6 +70,10 @@ function Statistic(props) {
   }
 
   let isBar = definition["type"] == "bar";
+
+  let icon = isBar ? <BarChartIcon /> : <LineChartIcon />;
+  let iconColor = isBar ? deepPurple[400] : indigo[400];
+
   let ChartType = isBar ? BarChart : LineChart;
   // Generate a palette via google-palette, and append the necessary # for a hex value
   let chartColours = DEFAULT_PALETTE.slice();
@@ -73,20 +82,32 @@ function Statistic(props) {
     chartColours = chartColours.concat(palette("rainbow", numKeys-DEFAULT_PALETTE.length).map((col) => "#" + col));
   }
 
+  let yAxisLabel = `${definition["y-label"]}s`;
+  let widgetHeight = 250; // TODO: make mobile friendly
+
   return <Grid item md={12} lg={6}>
     <Card>
       <CardHeader
-        title={definition["name"]}
+        disableTypography
+        avatar={<Avatar style={{background: iconColor }}>{icon}</Avatar>}
+        title={<Typography variant="h6">{definition["name"]}</Typography>}
         />
       <CardContent>
-        <ChartType width={730} height={250} data={rechartsData}>
+      { Object.keys(allFields).length == 0 ?
+        <Grid container direction="row" justify="center" alignItems="center" style={{height: widgetHeight}}>
+          <Grid item>
+            <Typography color="textSecondary" variant="caption">No data available for this statistic</Typography>
+          </Grid>
+        </Grid>
+        :
+        <ChartType width={730} height={widgetHeight} data={rechartsData}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="x">
             <Label value={definition["x-label"]} offset={0} position="insideBottom" />
           </XAxis>
-          <YAxis allowDecimals={false}  label={{ value: definition["y-label"], angle: -90, position: 'insideLeft' }} />
+          <YAxis allowDecimals={false} label={{ value: yAxisLabel, angle: -90, position: 'insideLeft' }} />
           <Tooltip />
-          <Legend />
+          <Legend align="right" verticalAlign="top" />
           {Object.keys(allFields).map((field, idx) =>
             isBar ?
               <Bar dataKey={field} fill={chartColours[idx]} key={idx}/>
@@ -94,6 +115,7 @@ function Statistic(props) {
               <Line dataKey={field} type="monotone" stroke={chartColours[idx]} key={idx} />
           )}
           </ChartType>
+      }
       </CardContent>
     </Card>
   </Grid>
