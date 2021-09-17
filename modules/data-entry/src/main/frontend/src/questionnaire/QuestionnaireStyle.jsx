edbb884331773c8f515b6a5a0ee2f33b139adae9
OrdiNeu <OrdinaryNeumann@gmail.com>
2021-09-17 13:57:12 -0400

    CARDS-500: As a user, I can access statistics on a dashboard
    Remove unused style

--- modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
@@ -602,9 +602,6 @@ const questionnaireStyle = theme => ({
       flexBasis: "100%",
       paddingLeft: theme.spacing(4)
     },
-    filterPlaceholder: {
-        opacity: 0.3
-    },
 });
 
 export default questionnaireStyle;
