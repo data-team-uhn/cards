data-team-uhn/cards

commit a0a4e2d0d9a01668b62aa23104f597690637712b

CARDS-500: As a user, I can access statistics on a dashboard Bugfix: markdown fields are displayed unformatted in the questionnaire editor

modules/data-entry/src/main/frontend/src/questionnaireEditor/Fields.jsx
@@ -32,8 +32,6 @@ import ObjectInput from "./ObjectInput";
 import TextInput from "./TextInput";
 import MarkdownTextField from "./MarkdownTextField";
 import FormattedText from "../components/FormattedText.jsx";
-import MDEditor from '@uiw/react-md-editor';
-import ReferenceInput from "./ReferenceInput";
 import { FieldsProvider } from "./FieldsContext.jsx";
 
 let Fields = (props) => {
@@ -98,9 +96,9 @@ let Fields = (props) => {
   return <FieldsProvider>
       {
         edit ?
-          Object.entries(JSON).filter(([key, _]) => key != "//REQUIRED").map(([key, value]) => displayEditField(key, value))
+          Object.entries(JSON).filter(([key, _]) => !key.startsWith("//")).map(([key, value]) => displayEditField(key, value))
         :
-          Object.keys(getAllKeys(JSON)).filter(([key, _]) => key != "//REQUIRED").map(key => (data[key] ? displayStaticField(key) : ''))
+          Object.entries(JSON).filter(([key, _]) => !key.startsWith("//")).map(([key, value]) => (data[key] ? displayStaticField(key) : ''))
       }
     </FieldsProvider>;
 }

commit f5fd0ae6d485009714f8349a0af25a4f6b64e187

CARDS-500: As a user, I can access statistics on a dashboard Bugfix: `ReferenceInput` no longer registered as a field displayer (bug introduced by commit a0a4e2d)

modules/data-entry/src/main/frontend/src/questionnaireEditor/Fields.jsx
@@ -32,6 +32,7 @@ import ObjectInput from "./ObjectInput";
 import TextInput from "./TextInput";
 import MarkdownTextField from "./MarkdownTextField";
 import FormattedText from "../components/FormattedText.jsx";
+import ReferenceInput from "./ReferenceInput";
 import { FieldsProvider } from "./FieldsContext.jsx";
 
 let Fields = (props) => {