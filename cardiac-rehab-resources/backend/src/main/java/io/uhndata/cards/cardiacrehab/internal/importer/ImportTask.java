b1121dccc5bae949f14a2631f132b32a4ce13b1f
OrdiNeu <OrdinaryNeumann@gmail.com>
2021-11-16 11:44:14 -0500

    [wip] CARDS-1389 work

--- cardiac-rehab-resources/backend/src/main/java/io/uhndata/cards/cardiacrehab/internal/importer/ImportTask.java
+++ cardiac-rehab-resources/backend/src/main/java/io/uhndata/cards/cardiacrehab/internal/importer/ImportTask.java
@@ -20,6 +20,7 @@
 package io.uhndata.cards.cardiacrehab.internal.importer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.StringReader;
@@ -29,10 +30,17 @@ import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
+import javax.jcr.RepositoryException;
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.json.JsonReader;
 
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -62,15 +70,14 @@ public class ImportTask implements Runnable
     {
 
         // Since we can't query more than one date at a time, query three dates
-        String postRequestTemplate = "{ patientsByDateAndClinic: (clinic: \"PMH 8 Palliative Care Oncology Clinic\","
-            + " date: \"%s\") {} }"
+        String postRequestTemplate = "{\"query\": \"query{"
+            + "patientsByDateAndClinic(clinic: \"PMH 8 Palliative Care Oncology Clinic\", date: \"%s\") {"
             + "name {given family}\nsex\nmrn\nohip\ndob\nemailOk\ncom {email {home work}}\n"
-            + "nextAppointment {id time status} }";
+            + "nextAppointment {id time status} }}\"}";
         Calendar dateToQuery = Calendar.getInstance();
         Date today = new Date();
         dateToQuery.setTime(today);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        StringBuilder response = new StringBuilder();
         for (int i = 0; i < 3; i++)
         {
             String postRequest = String.format(postRequestTemplate, formatter.format(dateToQuery));
@@ -82,8 +89,9 @@ public class ImportTask implements Runnable
 
                 http.setRequestMethod("POST");
                 http.setRequestProperty("Content-Type", "application/json");
-                http.setRequestProperty("Authorization", "");
+                http.setRequestProperty("Authorization", System.getenv("PROM_AUTH_TOKEN"));
                 http.setDoOutput(true);
+
                 // Get the next date
                 try (OutputStream os = con.getOutputStream()) {
                     byte[] input = postRequest.getBytes("utf-8");
@@ -93,16 +101,45 @@ public class ImportTask implements Runnable
                 // Get the response from the server
                 BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
                 String responseLine = null;
+                StringBuilder rawResponse = new StringBuilder();
                 while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
+                    rawResponse.append(responseLine.trim());
                 }
 
                 // Read the response into a JsonObject
-                JsonReader jsonReader = Json.createReader(new StringReader(response.toString()));
+                JsonReader jsonReader = Json.createReader(new StringReader(rawResponse.toString()));
+                JsonObject response = jsonReader.readObject();
+
+                JsonArray data = response.getJsonObject("data").getJsonArray("patientsByDateAndClinic");
+                for (int j = 0; j < data.size(); j++) {
+                    StorePatient(data.getJsonObject(j));
+                }
             } catch (Exception e) {
                 LOGGER.warn("Failed to query server: {}", e.getMessage(), e);
             }
             dateToQuery.add(Calendar.DATE, 1);
         }
+    }
+
+    /**
+     * Store information about a patient from a JsonObject
+     * @param patient
+     */
+    void StorePatient(JsonObject patient) throws LoginException, IOException, RepositoryException {
+        ResourceResolver resolver = this.resolverFactory.getResourceResolver(null);
+        Resource parentResource = resolver.getResource("/Patients/");
+        Resource patientFormType = resolver.getResource("/Questionnaires/Patient information/");
+        // Determine aspects of the patient that we want
+        resolver.create(parentResource, patient.getString("mrn"), new HashMap<String, Object>() {
+            
+        });
+    }
+
+    /**
+     * Store information about an appointment from a JsonObject
+     * @param appointment
+     */
+    void StoreAppointment(JsonObject appointment) {
+
     }
 }
