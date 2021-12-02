bd60178907f45dc33c4ba502aad421fb3934ab07
Michael Lescisin <michael.lescisin@sickkids.ca>
2021-12-02 17:52:03 -0500

    CARDS-1361: Specify installation details
    If the CARDS Docker container is started with the SAML_AUTH_ENABLED environment variable set to "true", default to the "trusted" permissions mode instead of "open"

--- distribution/docker_entry.sh
+++ distribution/docker_entry.sh
@@ -31,7 +31,13 @@ STORAGE=tar
 #If (inside a docker-compose environment), we are supposed to wait for http://cardsinitial:8080/ to start
 [ -z $WAIT_FOR_CARDSINIT ] || (while true; do (wget -S --spider http://cardsinitial:8080/ 2>&1 | grep 'HTTP/1.1 200 OK') && break; sleep 10; done)
 
-PERMISSIONS="${PERMISSIONS:-open}"
+#If SAML is enabled, default to "trusted" permissions. Otherwise, default to "open" permissions.
+if [[ "$SAML_AUTH_ENABLED" == "true" ]]
+then
+  PERMISSIONS="${PERMISSIONS:-trusted}"
+else
+  PERMISSIONS="${PERMISSIONS:-open}"
+fi
 
 PROJECT_ARTIFACTID=$1
 PROJECT_VERSION=$2
