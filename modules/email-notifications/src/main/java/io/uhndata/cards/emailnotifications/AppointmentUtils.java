efe60014529dd3b79afcf56cea6febafedd1f6f9
Michael Lescisin <michael.lescisin@sickkids.ca>
2022-01-12 17:40:47 -0500

    CARDS-1424: Send email to patients 72 hours prior to appointment, prompting them to complete the surveys

---
+++ modules/email-notifications/src/main/java/io/uhndata/cards/emailnotifications/AppointmentUtils.java
@@ -0,0 +1,189 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.uhndata.cards.emailnotifications;
+
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.Calendar;
+import java.util.Iterator;
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class AppointmentUtils
+{
+    /** Default log. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppointmentUtils.class);
+
+    // Hide the utility class constructor
+    private AppointmentUtils()
+    {
+    }
+
+    public static Resource getSubjectNode(ResourceResolver resolver, String jcrUuid)
+    {
+        Iterator<Resource> results;
+        results = resolver.findResources(
+            "SELECT * FROM [cards:Subject] as s WHERE s.'jcr:uuid'='" + jcrUuid + "'", "JCR-SQL2");
+        if (results.hasNext()) {
+            return results.next();
+        } else {
+            return null;
+        }
+    }
+
+    public static Resource getRelatedSubjectOfType(ResourceResolver resolver, Resource formResource,
+        String subjectTypePath)
+    {
+        final String patientTypeUuid = resolver.getResource(subjectTypePath).getValueMap().get("jcr:uuid", "");
+        final String[] relatedSubjects = formResource.getValueMap().get("relatedSubjects", String[].class);
+        for (int i = 0; i < relatedSubjects.length; i++) {
+            Resource tmpSubject = getSubjectNode(resolver, relatedSubjects[i]);
+            if (tmpSubject == null) {
+                continue;
+            }
+            if (patientTypeUuid.equals(tmpSubject.getValueMap().get("type", ""))) {
+                return tmpSubject;
+            }
+        }
+        return null;
+    }
+
+    public static <T> T getQuestionAnswerForPatientSubject(
+        ResourceResolver resolver, Resource patientSubject, String questionPath, String cardsDataType, T defaultValue)
+    {
+        final String subjectUUID = patientSubject.getValueMap().get("jcr:uuid", "");
+        final String questionUUID = resolver.getResource(questionPath).getValueMap().get("jcr:uuid", "");
+        if ("".equals(questionUUID)) {
+            return defaultValue;
+        }
+        Iterator<Resource> results;
+        results = resolver.findResources(
+            "SELECT t.* FROM [" + cardsDataType + "] as t INNER JOIN [cards:Form] AS f ON isdescendantnode(t, f)"
+            + " WHERE t.'question'='" + questionUUID + "'"
+            + " AND f.'subject'='" + subjectUUID + "'",
+            "JCR-SQL2");
+        if (results.hasNext()) {
+            T answerVal = results.next().getValueMap().get("value", defaultValue);
+            return answerVal;
+        }
+        return defaultValue;
+    }
+
+    public static String getPatientConsentedEmail(ResourceResolver resolver, Resource patientSubject)
+    {
+        long patientEmailOk = getQuestionAnswerForPatientSubject(
+            resolver,
+            patientSubject,
+            "/Questionnaires/Patient information/email_ok",
+            "cards:BooleanAnswer",
+            0
+        );
+        String patientEmailAddress = getQuestionAnswerForPatientSubject(
+            resolver,
+            patientSubject,
+            "/Questionnaires/Patient information/email",
+            "cards:TextAnswer",
+            ""
+        );
+        if (patientEmailOk == 1) {
+            if (EmailValidator.getInstance().isValid(patientEmailAddress)) {
+                return patientEmailAddress;
+            }
+        }
+        return null;
+    }
+    public static String getPatientFullName(ResourceResolver resolver, Resource patientSubject)
+    {
+        String firstName = getQuestionAnswerForPatientSubject(
+            resolver,
+            patientSubject,
+            "/Questionnaires/Patient information/first_name",
+            "cards:TextAnswer",
+            ""
+        );
+        String lastName = getQuestionAnswerForPatientSubject(
+            resolver,
+            patientSubject,
+            "/Questionnaires/Patient information/last_name",
+            "cards:TextAnswer",
+            ""
+        );
+        return firstName + " " + lastName;
+    }
+
+    public static Resource getFormForAnswer(ResourceResolver resolver, Resource answerResource)
+    {
+        String answerPath = answerResource.getPath();
+        String[] answerPathArray = answerPath.split("/");
+        if (answerPathArray.length < 3) {
+            return null;
+        }
+        if (!"".equals(answerPathArray[0])) {
+            return null;
+        }
+        if (!"Forms".equals(answerPathArray[1])) {
+            return null;
+        }
+        return resolver.getResource("/Forms/" + answerPathArray[2]);
+    }
+
+    public static Calendar parseDate(final String str)
+    {
+        try {
+            final ZonedDateTime zdt = ZonedDateTime.parse(str);
+            final Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(zdt.toInstant().toEpochMilli());
+            return calendar;
+        } catch (final DateTimeParseException e) {
+            LOGGER.warn("PARSING DATE FAILED: Invalid date {}", str);
+            return null;
+        }
+    }
+
+    public static Iterator<Resource> getAppointmentsForDay(ResourceResolver resolver, Calendar dateToQuery)
+    {
+        final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        final Resource visitTimeResult = resolver.getResource("/Questionnaires/Visit information/time");
+        final String visitTimeUUID = visitTimeResult.getValueMap().get("jcr:uuid", "");
+        final Calendar lowerBoundDate = (Calendar) dateToQuery.clone();
+        lowerBoundDate.set(Calendar.HOUR_OF_DAY, 0);
+        lowerBoundDate.set(Calendar.MINUTE, 0);
+        lowerBoundDate.set(Calendar.SECOND, 0);
+        lowerBoundDate.set(Calendar.MILLISECOND, 0);
+        final Calendar upperBoundDate = (Calendar) lowerBoundDate.clone();
+        upperBoundDate.add(Calendar.HOUR, 24);
+        LOGGER.warn("Querying for {}", formatter.format(dateToQuery.getTime()));
+        LOGGER.warn("Lower bound: {}", formatter.format(lowerBoundDate.getTime()));
+        LOGGER.warn("Upper bound: {}", formatter.format(upperBoundDate.getTime()));
+        final Iterator<Resource> results = resolver.findResources(
+            "SELECT d.* FROM [cards:DateAnswer] AS d INNER JOIN [cards:Form] AS f ON"
+            + " isdescendantnode(d, f) WHERE d.'question'='" + visitTimeUUID + "'"
+            + " AND d.'value' >= cast('" + formatter.format(lowerBoundDate.getTime()) + "' AS date)"
+            + " AND d.'value' < cast('" + formatter.format(upperBoundDate.getTime()) + "' AS date)",
+            "JCR-SQL2"
+        );
+        return results;
+    }
+}
---
+++ modules/email-notifications/src/main/java/io/uhndata/cards/emailnotifications/InitialNotificationsTask.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.uhndata.cards.emailnotifications;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.commons.messaging.mail.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.uhndata.cards.auth.token.TokenManager;
+import jakarta.mail.MessagingException;
+
+
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
+public class InitialNotificationsTask implements Runnable
+{
+    /** Default log. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(InitialNotificationsTask.class);
+
+    /** Provides access to resources. */
+    private final ResourceResolverFactory resolverFactory;
+
+    private final TokenManager tokenManager;
+
+    private final MailService mailService;
+
+    InitialNotificationsTask(final ResourceResolverFactory resolverFactory,
+        final TokenManager tokenManager, final MailService mailService)
+    {
+        this.resolverFactory = resolverFactory;
+        this.tokenManager = tokenManager;
+        this.mailService = mailService;
+    }
+
+    @SuppressWarnings("checkstyle:ExecutableStatementCount")
+    @Override
+    public void run()
+    {
+        LOGGER.warn("Executing InitialNotificationsTask");
+        /*
+         * Query the `Visit information` Forms for appointments happening
+         * on day NOW+3. For example, if today is January 1st 2022, find
+         * all appointments on January 4th 2022.
+         *
+         * Suppose that the nightly ImportTask runs every night at 00:00:00.
+         * Therefore, when it runs at 2022-01-01T00:00:00 with a daysToQuery
+         * parameter of 3, it will import appointments for 2022-01-01,
+         * 2022-01-02, and 2022-01-03.
+         *
+         * Now suppose that this nightly InitialNotificationsTask runs every night
+         * at 00:30:00. When it runs at 2022-01-01T00:30:00 it will obtain
+         * all the appointments happening on 2022-01-04 and immediately send
+         * out the notification emails requesting completion of the
+         * pre-appointment surveys. If an appointment is scheduled for
+         * 2022-01-04T00:00:00 (although highly unlikely at this hour)
+         * the email notification will thus be sent out 71.5 hours before
+         * the appointment. If an appointment is scheduled for
+         * 2022-01-04T23:59:59 (although highly unlikely at this hour) the
+         * email notification will thus be sent out 95.5 hours before the
+         * appointment.
+         *
+         */
+        final Date today = new Date();
+        final Calendar dateToQuery = Calendar.getInstance();
+        dateToQuery.setTime(today);
+        dateToQuery.add(Calendar.DATE, 3);
+        try (ResourceResolver resolver = this.resolverFactory.getServiceResourceResolver(null)) {
+            Iterator<Resource> appointmentResults = AppointmentUtils.getAppointmentsForDay(resolver, dateToQuery);
+            while (appointmentResults.hasNext()) {
+                Resource appointmentResult = appointmentResults.next();
+                Resource appointmentForm = AppointmentUtils.getFormForAnswer(resolver, appointmentResult);
+                if (appointmentForm == null) {
+                    continue;
+                }
+                // Get the Patient Subject associated with this appointment Form
+                Resource patientSubject = AppointmentUtils.getRelatedSubjectOfType(
+                    resolver, appointmentForm, "/SubjectTypes/Patient");
+                LOGGER.warn("Patient {} has a scheduled visit on {}",
+                    patientSubject.getPath(), appointmentResult.getValueMap().get("value", ""));
+                Resource visitSubject = AppointmentUtils.getRelatedSubjectOfType(
+                    resolver, appointmentForm, "/SubjectTypes/Patient/Visit");
+                LOGGER.warn("The associated visit subject is {}", visitSubject.getPath());
+                String patientEmailAddress = AppointmentUtils.getPatientConsentedEmail(resolver, patientSubject);
+                if (patientEmailAddress == null) {
+                    continue;
+                }
+                String patientFullName = AppointmentUtils.getPatientFullName(resolver, patientSubject);
+                LOGGER.warn("Surveys email to {}", patientEmailAddress);
+                Calendar tokenExpiryDate = AppointmentUtils.parseDate(appointmentResult.getValueMap().get("value", ""));
+                tokenExpiryDate.add(Calendar.HOUR, 2);
+                LOGGER.warn("The following token will expire on: {}", tokenExpiryDate);
+                String surveysLink = "http://localhost:8080/Proms.html/Cardio?auth_token="
+                    + this.tokenManager.create(
+                        "patient",
+                        tokenExpiryDate,
+                        Collections.singletonMap(
+                            "cards:sessionSubject",
+                            visitSubject.getPath()
+                        )
+                    ).getToken();
+                LOGGER.warn("{}", surveysLink);
+                // Send the Initial Notification Email
+                String emailBody = "You have an appointment in 3 days from now.";
+                emailBody += " Please complete your surveys beforehand at";
+                emailBody += " ";
+                emailBody += surveysLink;
+                try {
+                    EmailUtils.sendNotificationEmail(this.mailService, patientEmailAddress, patientFullName, emailBody);
+                } catch (MessagingException e) {
+                    LOGGER.warn("Failed to send Initial Notification Email");
+                }
+            }
+        } catch (LoginException e) {
+            LOGGER.warn("Failed to results.next().getPath()");
+        }
+    }
+}
