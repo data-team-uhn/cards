bf9c0bff098ab1ba6e04ef8418a8d68351b5e6b3
Sergiu Dumitriu <sergiu@phenotips.org>
2022-01-28 14:11:13 -0500

    CARDS-1475: PROMs dashboard Forbid clinician access to unsubmitted forms

---
+++ proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/UnsubmittedFormsRestrictionFactory.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.uhndata.cards.proms.internal;
+
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.spi.security.authorization.restriction.RestrictionPattern;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.FieldOption;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+import io.uhndata.cards.dataentry.api.FormUtils;
+import io.uhndata.cards.dataentry.api.QuestionnaireUtils;
+import io.uhndata.cards.dataentry.api.SubjectTypeUtils;
+import io.uhndata.cards.dataentry.api.SubjectUtils;
+import io.uhndata.cards.permissions.spi.RestrictionFactory;
+
+/**
+ * Factory for {@link UnsubmittedFormsRestrictionPattern}.
+ *
+ * @version $Id$
+ */
+@Component(immediate = true)
+public class UnsubmittedFormsRestrictionFactory implements RestrictionFactory
+{
+    /** @see #getName */
+    public static final String NAME = "cards:unsubmittedForms";
+
+    /**
+     * This is needed to get access to the current session, which knows if there is a subject bound to it.
+     */
+    @Reference(fieldOption = FieldOption.REPLACE,
+        cardinality = ReferenceCardinality.OPTIONAL,
+        policyOption = ReferencePolicyOption.GREEDY)
+    private ResourceResolverFactory rrf;
+
+    @Reference
+    private FormUtils formUtils;
+
+    @Reference
+    private SubjectUtils subjectUtils;
+
+    @Reference
+    private SubjectTypeUtils subjectTypeUtils;
+
+    @Reference
+    private QuestionnaireUtils questionnaireUtils;
+
+    @Override
+    public RestrictionPattern forValue(PropertyState value)
+    {
+        Session session = null;
+        if (this.rrf != null && this.rrf.getThreadResourceResolver() != null) {
+            session = this.rrf.getThreadResourceResolver().adaptTo(Session.class);
+        }
+        return new UnsubmittedFormsRestrictionPattern(session, this.formUtils, this.subjectUtils,
+            this.subjectTypeUtils, this.questionnaireUtils);
+    }
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public Type<?> getType()
+    {
+        // This doesn't actually support any type, since it is only a marker restriction. However, specifying a type is
+        // mandatory, and a single-value type enforces that a single value is provided, while a multi-value type happily
+        // accepts no value at all.
+        return Type.STRINGS;
+    }
+}
---
+++ proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/UnsubmittedFormsRestrictionPattern.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.uhndata.cards.proms.internal;
+
+import javax.jcr.Node;
+import javax.jcr.PropertyIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.api.Tree;
+import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.spi.security.authorization.restriction.RestrictionPattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.uhndata.cards.dataentry.api.FormUtils;
+import io.uhndata.cards.dataentry.api.QuestionnaireUtils;
+import io.uhndata.cards.dataentry.api.SubjectTypeUtils;
+import io.uhndata.cards.dataentry.api.SubjectUtils;
+
+/**
+ * A restriction that makes a permissions entry only be valid on a form if the form belongs to a Visit subject that has
+ * not been submitted yet.
+ *
+ * @version $Id$
+ */
+public class UnsubmittedFormsRestrictionPattern implements RestrictionPattern
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnsubmittedFormsRestrictionPattern.class);
+
+    /** The current session, which may contain a subject identifier. */
+    private final Session session;
+
+    private final FormUtils formUtils;
+
+    private final SubjectUtils subjectUtils;
+
+    private final SubjectTypeUtils subjectTypeUtils;
+
+    private final QuestionnaireUtils questionnaireUtils;
+
+    /**
+     * Constructor passing all the needed information.
+     *
+     * @param session the current session
+     * @param formUtils for working with forms
+     * @param subjectUtils for working with subjects
+     * @param subjectTypeUtils for working with subject types
+     * @param questionnaireUtils for working with questionnaires
+     */
+    public UnsubmittedFormsRestrictionPattern(final Session session, final FormUtils formUtils,
+        final SubjectUtils subjectUtils, final SubjectTypeUtils subjectTypeUtils,
+        final QuestionnaireUtils questionnaireUtils)
+    {
+        this.session = session;
+        this.formUtils = formUtils;
+        this.subjectUtils = subjectUtils;
+        this.subjectTypeUtils = subjectTypeUtils;
+        this.questionnaireUtils = questionnaireUtils;
+    }
+
+    @Override
+    public boolean matches()
+    {
+        // This is not a repository-wide restriction, it only applies to specific nodes
+        return false;
+    }
+
+    @Override
+    public boolean matches(String path)
+    {
+        // This is called when a new node is being created
+        return false;
+    }
+
+    @Override
+    public boolean matches(final Tree tree, final PropertyState property)
+    {
+        try {
+            if (this.session == null) {
+                // No session, cannot proceed
+                return false;
+            }
+
+            if (!isForm(tree)) {
+                // Not a form
+                return false;
+            }
+
+            // This restriction does not apply to the Visit Information form itself
+            if ("/Questionnaires/Visit information".equals(this.questionnaireUtils
+                .getQuestionnaire(tree.getProperty("questionnaire").getValue(Type.STRING)).getPath())) {
+                return false;
+            }
+
+            return isUnsubmitted(tree);
+        } catch (RepositoryException e) {
+            LOGGER.warn("Failed to determine if form belongs to an unsubmitted visit: {}", e.getMessage(), e);
+        }
+        return false;
+    }
+
+    private boolean isUnsubmitted(final Tree form) throws RepositoryException
+    {
+        final Node subject = this.session.getNodeByIdentifier(form.getProperty("subject").getValue(Type.STRING));
+
+        if (!"Visit".equals(this.subjectTypeUtils.getLabel(this.subjectUtils.getType(subject)))) {
+            // Not a form for a Visit
+            return false;
+        }
+
+        final Node visitInformation = findVisitInformationForm(subject);
+        final Node visitInformationQuestionnaire = this.formUtils.getQuestionnaire(visitInformation);
+        final Node submittedQuestion =
+            this.questionnaireUtils.getQuestion(visitInformationQuestionnaire, "surveys_submitted");
+        final Object submitted = this.formUtils.getValue(this.formUtils.getAnswer(visitInformation, submittedQuestion));
+        return submitted == null || !Long.valueOf(1).equals(submitted);
+    }
+
+    private boolean isForm(final Tree node)
+    {
+        return node.getProperty("jcr:primaryType") != null
+            && StringUtils.equals(node.getProperty("jcr:primaryType").getValue(Type.STRING), "cards:Form");
+    }
+
+    private Node findVisitInformationForm(final Node subject) throws RepositoryException
+    {
+        final PropertyIterator references = subject.getReferences("subject");
+        while (references.hasNext()) {
+            final Node form = references.nextProperty().getParent();
+            final Node questionnaire = this.formUtils.getQuestionnaire(form);
+            if ("/Questionnaires/Visit information".equals(questionnaire.getPath())) {
+                return form;
+            }
+        }
+        return null;
+    }
+}
