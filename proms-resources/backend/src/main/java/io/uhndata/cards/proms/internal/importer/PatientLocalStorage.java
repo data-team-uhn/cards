d618e13ff799bf339f68aa409a916d338e2479a0
Sergiu Dumitriu <sergiu@phenotips.org>
2022-02-17 17:52:00 -0500

    CARDS-1653: PROMs: email not imported Also consider other email addresses, not just the "home" one

--- proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/importer/PatientLocalStorage.java
+++ proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/importer/PatientLocalStorage.java
@@ -447,7 +447,10 @@ public class PatientLocalStorage
             "sex", obj -> obj.getString("sex"),
             "first_name", obj -> obj.getJsonObject("name").getJsonArray("given").getString(0),
             "last_name", obj -> obj.getJsonObject("name").getString("family"),
-            "email", obj -> obj.getJsonObject("com").getJsonObject("email").getString("home"),
+            "email", obj -> obj.getJsonObject("com").getJsonObject("email").values().stream()
+                .filter(e -> e != null && e != JsonValue.NULL)
+                .map(Object::toString)
+                .findFirst().orElse(""),
             "email_ok@cards:BooleanAnswer", obj -> BooleanUtils.toInteger(obj.getBoolean("emailOk", false), 1, 0, -1),
             "fhir_id", obj -> obj.getString(PatientLocalStorage.FHIR_FIELD));
 
