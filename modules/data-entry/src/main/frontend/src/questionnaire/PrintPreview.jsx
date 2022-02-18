39353dc6e599a62a7ffcf8d9b02a69d48a3b0a4b
Marta Girdea <marta@phenotips.org>
2022-02-18 17:53:29 -0500

    CARDS-1656: Print mode header doesn't stretch on the whole width of the page

--- modules/data-entry/src/main/frontend/src/questionnaire/PrintPreview.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/PrintPreview.jsx
@@ -97,6 +97,9 @@ const useStyles = makeStyles(theme => ({
     "@media print" : {
       display: "block",
       padding: theme.spacing(2),
+      "& table" : {
+        width: "100%",
+      },
       "& h1:not(:first-of-type)" : {
         breakBefore: "page",
       }
