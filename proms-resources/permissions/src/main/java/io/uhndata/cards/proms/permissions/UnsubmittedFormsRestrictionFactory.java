1e128b3a95f54664bd0f016b23acb94b576dfa2c
Sergiu Dumitriu <sergiu@phenotips.org>
2022-03-10 13:26:43 -0500

    CARDS-1693: Sometimes starting CARDS fails with "setting ACL" errors Start permissions modules earlier, before repoinit statements are executed

--- proms-resources/permissions/src/main/java/io/uhndata/cards/proms/permissions/UnsubmittedFormsRestrictionFactory.java
+++ proms-resources/permissions/src/main/java/io/uhndata/cards/proms/permissions/UnsubmittedFormsRestrictionFactory.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package io.uhndata.cards.proms.internal;
+package io.uhndata.cards.proms.permissions;
 
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Type;
