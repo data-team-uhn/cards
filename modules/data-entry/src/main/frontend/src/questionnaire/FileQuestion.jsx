c6ca215716d9236fc34002aa0ca7aee3a1c25e94
Michael Lescisin <michael.lescisin@sickkids.ca>
2022-04-01 12:13:24 -0400

    Undo changes in FileQuestion.jsx

--- modules/data-entry/src/main/frontend/src/questionnaire/FileQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/FileQuestion.jsx
@@ -214,7 +214,7 @@ function FileQuestion(props) {
     // Also NB: Since we save before this, we're guaranteed to have the parent created
     let data = new FormData();
     data.append(file['name'], file);
-    data.append('jcr:primaryType', 'cards:DicomAnswer');
+    data.append('jcr:primaryType', 'cards:FileAnswer');
     data.append('question', props.questionDefinition['jcr:uuid']);
     data.append('question@TypeHint', "Reference");
     return fetchWithReLogin(globalLoginDisplay, outURL, {
@@ -343,7 +343,7 @@ function FileQuestion(props) {
         answers={answers}
         questionDefinition={props.questionDefinition}
         existingAnswer={existingAnswer}
-        answerNodeType="cards:DicomAnswer"
+        answerNodeType="cards:FileAnswer"
         onDecidedOutputPath={setAnswerPath}
         valueType="path"
         isMultivalued={maxAnswers != 1}
