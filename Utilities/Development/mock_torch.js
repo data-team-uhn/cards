9b3ce89a4e375bd5e46bd211febb60ca4f11c549
Michael Lescisin <michael.lescisin@sickkids.ca>
2022-04-29 16:22:44 -0400

    Misc: Add the ability for mock_torch.js to generate an arbitrary number of random patients each with one visit
    Refactor code by introducing the readCliParam function

--- Utilities/Development/mock_torch.js
+++ Utilities/Development/mock_torch.js
@@ -23,12 +23,24 @@
 const LISTEN_HOST = '0.0.0.0';
 const LISTEN_PORT = 8011;
 
+const fs = require('fs');
+const uuidv4 = require('uuid').v4;
 const process = require('process');
 const bodyParser = require('body-parser');
 const express = require('express');
 const webApp = express();
 webApp.use(bodyParser.text({type: '*/*'}));
 const webServer = require('http').createServer(webApp);
+
+const RANDOM_NAME_LIST = JSON.parse(fs.readFileSync("com.github.dominictarr.random-name.names.json", "utf-8"));
+
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const getRandomName = () => {
+  return RANDOM_NAME_LIST[randomInt(0, RANDOM_NAME_LIST.length)];
+}
 
 const dateToTorchString = (date) => {
   let dateStr = "";
@@ -46,26 +58,107 @@ const dateToTorchString = (date) => {
   return dateStr;
 };
 
-const getMockAppointmentTime = () => {
+const readCliParam = (paramName) => {
   for (let i = 0; i < process.argv.length; i++) {
-    if (process.argv[i].startsWith("--appointment-time-hours-from-now=")) {
-      let timeOffset = parseInt(process.argv[i].split("=")[1]);
-      let nowTime = new Date();
-      let apptTime = new Date(nowTime.getTime() + (1000*60*60*timeOffset));
-      return dateToTorchString(apptTime);
-    }
-  }
-  return "2022-02-20T10:00:00";
-};
-
-const getMockAppointmentStatus = () => {
-  for (let i = 0; i < process.argv.length; i++) {
-    if (process.argv[i].startsWith("--appointment-status=")) {
+    if (process.argv[i].startsWith("--" + paramName + "=")) {
       return process.argv[i].split("=")[1];
     }
   }
-  return "planned";
+  return undefined;
 };
+
+const getMockAppointmentTime = () => {
+  let appointmentTimeHoursFromNow = readCliParam("appointment-time-hours-from-now");
+  if (appointmentTimeHoursFromNow === undefined) {
+    return "2022-02-20T10:00:00";
+  } else {
+    let timeOffset = parseInt(appointmentTimeHoursFromNow);
+    let nowTime = new Date();
+    let apptTime = new Date(nowTime.getTime() + (1000*60*60*timeOffset));
+    return dateToTorchString(apptTime);
+  }
+};
+
+const getMockAppointmentStatus = () => {
+  let appointmentStatus = readCliParam("appointment-status");
+  if (appointmentStatus === undefined) {
+    return "planned";
+  } else {
+    return appointmentStatus;
+  }
+};
+
+const generateRandomPatient = () => {
+  let givenName = getRandomName();
+  let familyName = getRandomName();
+  return {
+    mrn: 'MRN-' + uuidv4(),
+    ohip: 'OHIP-' + uuidv4(),
+    fhirID: 'FHIR-' + uuidv4(),
+    dob: randomInt(1940, 2000) + '-' + randomInt(1, 12).toString().padStart(2, 0) + '-' + randomInt(1, 28).toString().padStart(2, 0),
+    sex: (Math.random() < 0.5) ? 'F' : 'M',
+    name: {
+      given: [
+        givenName
+      ],
+      family: familyName
+    },
+    emailOk: true,
+    com: {
+      email: {
+        personal: givenName + '.' + familyName + '@mail.com'
+      }
+    }
+  };
+}
+
+const generateRandomAppointment = () => {
+   let physicianGivenName = getRandomName();
+   let physicianFamilyName = getRandomName();
+   return {
+    location: ['6012-HC-Congenital Cardiac'],
+    status: {toJSON: getMockAppointmentStatus},
+    fhirID: 'FHIR-' + uuidv4(),
+    participants: [
+      {
+        role: 'ATND',
+        physician: {
+          eID: 'eID-' + uuidv4(),
+          fhirID: 'FHIR-' + uuidv4(),
+          name: {
+            prefix: [
+              'Dr.'
+            ],
+            given: [
+              physicianGivenName
+            ],
+            family: physicianFamilyName,
+            suffix: [
+              'M.D.'
+            ]
+          }
+        }
+      }
+    ],
+    time: {toJSON: getMockAppointmentTime}
+  };
+}
+
+const generateRandomDataPatientsByDateAndClinic = (nDataPoints) => {
+  let patientsByDateAndClinic = [];
+  for (let i = 0; i < nDataPoints; i++) {
+    let thisPatient = generateRandomPatient();
+    let thisAppointment = generateRandomAppointment();
+    thisPatient.appointments = [thisAppointment];
+    patientsByDateAndClinic.push(thisPatient);
+  }
+
+  return {
+    data: {
+      patientsByDateAndClinic: patientsByDateAndClinic
+    }
+  };
+}
 
 const graphQlResponse = {
   data: {
@@ -175,13 +268,22 @@ const graphQlResponse = {
   }
 };
 
+const selectGraphQlResponse = () => {
+  let nRandomPatients = readCliParam("nRandomPatients");
+  if (nRandomPatients !== undefined) {
+    return generateRandomDataPatientsByDateAndClinic(parseInt(nRandomPatients));
+  } else {
+    return graphQlResponse;
+  }
+};
+
 webApp.post(/.*/, (req, res) => {
   console.log("GraphQL QUERY => " + req.body);
   console.log("");
-  console.log("RESPONSE <= " + JSON.stringify(graphQlResponse));
+  console.log("RESPONSE <= " + JSON.stringify(selectGraphQlResponse()));
   console.log("");
   console.log("");
-  res.json(graphQlResponse);
+  res.json(selectGraphQlResponse());
 });
 
 webServer.listen(LISTEN_PORT, LISTEN_HOST, (err) => {
