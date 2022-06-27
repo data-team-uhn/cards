57e49cc82c03006f87741fc19d7cad36ebd74706
sofi2002sofi <sofia.didula@gmail.com>
2022-06-27 10:56:20 -0400

    CARDS-1806: Add support for including descendant subjects' forms in a subject's JSON

--- modules/data-entry/src/main/java/io/uhndata/cards/dataentry/internal/serialize/DataProcessor.java
+++ modules/data-entry/src/main/java/io/uhndata/cards/dataentry/internal/serialize/DataProcessor.java
@@ -23,6 +23,8 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -36,9 +38,12 @@ import javax.json.JsonObjectBuilder;
 import javax.json.JsonValue;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.uhndata.cards.dataentry.api.QuestionnaireUtils;
 import io.uhndata.cards.serialize.spi.ResourceJsonProcessor;
@@ -51,6 +56,8 @@ import io.uhndata.cards.serialize.spi.ResourceJsonProcessor;
 @Component(immediate = true)
 public class DataProcessor implements ResourceJsonProcessor
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataProcessor.class);
+
     private ThreadLocal<ResourceResolver> resolver = new ThreadLocal<>();
 
     private ThreadLocal<String> selectors = new ThreadLocal<>();
@@ -58,6 +65,14 @@ public class DataProcessor implements ResourceJsonProcessor
     private ThreadLocal<String> rootNode = new ThreadLocal<>();
 
     private ThreadLocal<Map<String, String>> filters = new ThreadLocal<>();
+
+    private ThreadLocal<Map<String, String>> options = new ThreadLocal<>();
+
+    // Node uuid and its depth level relative to the root node
+    private ThreadLocal<Map<String, Integer>> depthLevels = ThreadLocal.withInitial(HashMap::new);
+
+    // Max depth level of the root node
+    private ThreadLocal<Integer> depth = ThreadLocal.withInitial(() -> 0);
 
     @Override
     public String getName()
@@ -96,6 +111,14 @@ public class DataProcessor implements ResourceJsonProcessor
                 StringUtils.substringAfter(s, "=").replaceAll("\\\\\\.", ".")));
         this.filters.set(filtersMap);
 
+        final Map<String, String> optionsMap = new HashMap<>();
+        Arrays.asList(this.selectors.get().split("(?<!\\\\)(?:\\\\\\\\)*\\.")).stream()
+            .filter(s -> StringUtils.startsWith(s, "dataOptions:"))
+            .map(s -> StringUtils.substringAfter(s, "dataOptions:"))
+            .forEach(s -> optionsMap.put(StringUtils.substringBefore(s, "="),
+                StringUtils.substringAfter(s, "=").replaceAll("\\\\\\.", ".")));
+        this.options.set(optionsMap);
+
     }
 
     @Override
@@ -110,11 +133,27 @@ public class DataProcessor implements ResourceJsonProcessor
         try {
             // Only the original subject or questionnaire node will have its data appended
             if (!node.getPath().equals(this.rootNode.get())) {
+                int currentDepth = depthLevelCounter(node);
+                if (currentDepth > this.depth.get()) {
+                    this.depth.set(currentDepth);
+                }
                 return;
             }
 
             boolean isQuestionnaire = node.isNodeType(QuestionnaireUtils.QUESTIONNAIRE_NODETYPE);
-            final String query = generateDataQuery(node, isQuestionnaire);
+
+            // Stores list of Subjects' uuids per each depth level
+            Map<Integer, LinkedList<String>> uuidsPerLevel = new HashMap<>();
+            for (Map.Entry<String, Integer> entry : this.depthLevels.get().entrySet()) {
+                if (uuidsPerLevel.containsKey(entry.getValue())) {
+                    uuidsPerLevel.get(entry.getValue()).add(entry.getKey());
+                } else {
+                    uuidsPerLevel.put(entry.getValue(), new LinkedList<>(List.of(entry.getKey())));
+                }
+            }
+            String additionalSubjectsQuery = generateAdditionalSubjectsQuery(this.options.get().get("descendantData"),
+                    uuidsPerLevel);
+            final String query = generateDataQuery(node, isQuestionnaire, additionalSubjectsQuery);
             Iterator<Resource> forms = this.resolver.get().findResources(query, Query.JCR_SQL2);
 
             final Map<String, JsonArrayBuilder> formsJsons = new HashMap<>();
@@ -125,13 +164,53 @@ public class DataProcessor implements ResourceJsonProcessor
             // The data JSONs have been collected, add them to the subject's JSON
             formsJsons.forEach(json::add);
             final JsonObjectBuilder filtersJson = Json.createObjectBuilder();
+            final JsonObjectBuilder optionsJson = Json.createObjectBuilder();
             this.filters.get().forEach(filtersJson::add);
+            this.options.get().forEach(optionsJson::add);
             json.add("dataFilters", filtersJson);
+            json.add("option", optionsJson);
             json.add("exportDate",
                 new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(Calendar.getInstance().getTime()));
         } catch (RepositoryException e) {
             // Really shouldn't happen
         }
+    }
+
+    private int depthLevelCounter(Node currentNode) throws RepositoryException
+    {
+        final String currentNodeIdentifier = currentNode.getIdentifier();
+        if (this.depthLevels.get().containsKey(currentNodeIdentifier)) {
+            return this.depthLevels.get().get(currentNodeIdentifier);
+        }
+
+        int depthLevel = 1;
+        while (!currentNode.getParent().getPath().equals(this.rootNode.get())) {
+            depthLevel += depthLevelCounter(currentNode.getParent());
+        }
+        this.depthLevels.get().put(currentNodeIdentifier, depthLevel);
+        return depthLevel;
+    }
+
+    private String generateAdditionalSubjectsQuery(String descendantDataValue,
+        Map<Integer, LinkedList<String>> uuidsPerLevel)
+    {
+        int displayLevel = 0;
+
+        if (NumberUtils.isParsable(descendantDataValue)) {
+            displayLevel = Integer.parseInt(descendantDataValue);
+        } else if ("true".equals(descendantDataValue)) {
+            displayLevel = this.depth.get();
+        }
+
+        StringBuilder allUuids = new StringBuilder();
+        for (int i = 1; i < displayLevel + 1; i++) {
+            if (uuidsPerLevel.containsKey(i)) {
+                for (String uuid : uuidsPerLevel.get(i)) {
+                    allUuids.append("or n.subject = '").append(uuid).append("'");
+                }
+            }
+        }
+        return allUuids.toString();
     }
 
     private void storeForm(final Resource form, final Map<String, JsonArrayBuilder> formsJsons,
@@ -151,11 +230,12 @@ public class DataProcessor implements ResourceJsonProcessor
         }
     }
 
-    private String generateDataQuery(final Node entity, final boolean isQuestionnaire) throws RepositoryException
+    private String generateDataQuery(final Node entity, final boolean isQuestionnaire, String additionalSubjectsQuery)
+            throws RepositoryException
     {
         final String entityFilter = isQuestionnaire ? "questionnaire" : "subject";
         final StringBuilder result = new StringBuilder("select * from [cards:Form] as n where n." + entityFilter
-            + " = '" + entity.getIdentifier() + "'");
+            + " = '" + entity.getIdentifier() + "'" + additionalSubjectsQuery);
         this.filters.get().forEach((key, value) -> {
             switch (key) {
                 case "createdAfter":
