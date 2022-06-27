fcffa61fba18adf7098e9ec0217d19b486cd71f8
Sergiu Dumitriu <sergiu@phenotips.org>
2022-06-26 20:17:41 -0400

    CARDS-1401: Creating forms via API ignores the maxPerSubject setting Use DefaultValidator as a base class to avoid reimplementing many empty methods

--- modules/data-entry/src/main/java/io/uhndata/cards/internal/MaxFormsOfTypePerSubjectValidator.java
+++ modules/data-entry/src/main/java/io/uhndata/cards/internal/MaxFormsOfTypePerSubjectValidator.java
@@ -21,8 +21,8 @@ import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.jackrabbit.oak.api.CommitFailedException;
-import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.spi.commit.DefaultValidator;
 import org.apache.jackrabbit.oak.spi.commit.Validator;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.sling.api.resource.LoginException;
@@ -31,13 +31,12 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 
 /**
- * A {@link Validator} that ensures that the number of created Forms of
- * a specifc type does not exceed the maximum value allowed for a
- * Subject.
+ * A {@link Validator} that ensures that the number of created Forms of a specifc type does not exceed the maximum value
+ * allowed for a Subject.
  *
  * @version $Id$
  */
-public class MaxFormsOfTypePerSubjectValidator implements Validator
+public class MaxFormsOfTypePerSubjectValidator extends DefaultValidator
 {
     private final ResourceResolverFactory rrf;
 
@@ -47,42 +46,17 @@ public class MaxFormsOfTypePerSubjectValidator implements Validator
     }
 
     @Override
-    public void enter(NodeState before, NodeState after) throws CommitFailedException
-    {
-    }
-
-    @Override
-    public void leave(NodeState before, NodeState after) throws CommitFailedException
-    {
-    }
-
-    @Override
-    public void propertyAdded(PropertyState after) throws CommitFailedException
-    {
-    }
-
-    @Override
-    public void propertyChanged(PropertyState before, PropertyState after) throws CommitFailedException
-    {
-    }
-
-    @Override
-    public void propertyDeleted(PropertyState before) throws CommitFailedException
-    {
-    }
-
-    @Override
-    public Validator childNodeAdded(String name, NodeState after) throws CommitFailedException
+    public Validator childNodeAdded(final String name, final NodeState after) throws CommitFailedException
     {
         // Get the type of this node. Return immediately if it's not a cards:Form node
-        String childNodeType = after.getName("jcr:primaryType");
+        final String childNodeType = after.getName("jcr:primaryType");
         if (!("cards:Form".equals(childNodeType))) {
             return this;
         }
 
         // Get the jcr:uuid values for the Form's associated Questionnaire and Subject
-        String questionnaireUUID = after.getProperty("questionnaire").getValue(Type.REFERENCE).toString();
-        String subjectUUID = after.getProperty("subject").getValue(Type.REFERENCE).toString();
+        final String questionnaireUUID = after.getProperty("questionnaire").getValue(Type.REFERENCE).toString();
+        final String subjectUUID = after.getProperty("subject").getValue(Type.REFERENCE).toString();
 
         // Obtain a ResourceResolver for querying the JCR
         final Map<String, Object> parameters =
@@ -90,25 +64,32 @@ public class MaxFormsOfTypePerSubjectValidator implements Validator
         try (ResourceResolver serviceResolver = this.rrf.getServiceResourceResolver(parameters)) {
 
             // Get the Questionnaire Resource associated with this Form
-            Resource questionnaire = getQuestionnaireResourceByUuid(serviceResolver, questionnaireUUID);
+            final Resource questionnaire = getQuestionnaireResourceByUuid(serviceResolver, questionnaireUUID);
             if (questionnaire == null) {
                 return this;
             }
 
             // Get the maxPerSubject value for the Questionnaire
-            long maxPerSubject = questionnaire.getValueMap().get("maxPerSubject", -1);
+            final long maxPerSubject = questionnaire.getValueMap().get("maxPerSubject", -1);
 
             // Should this commit be allowed or does it exceed the maxPerSubject constraint?
             if (maxPerSubject > 0) {
-                long formNumber = countFormsPerSubject(subjectUUID, questionnaireUUID, serviceResolver) + 1;
+                final long formNumber = countFormsPerSubject(subjectUUID, questionnaireUUID, serviceResolver) + 1;
                 if (formNumber > maxPerSubject) {
                     throw new CommitFailedException(CommitFailedException.STATE, 400,
-                            "The number of created forms is bigger then is allowed");
+                        "The number of created forms is bigger then is allowed");
                 }
             }
-        } catch (LoginException e) {
+        } catch (final LoginException e) {
             // Should not happen
         }
+        return this;
+    }
+
+    @Override
+    public Validator childNodeChanged(final String name, final NodeState before, final NodeState after)
+        throws CommitFailedException
+    {
         return this;
     }
 
@@ -120,15 +101,14 @@ public class MaxFormsOfTypePerSubjectValidator implements Validator
      * @param ResourceResolver a ResourceResolver that can be used for querying the JCR
      * @return a long-typed number of the number of Forms with the specified questionnaire and subject
      */
-    private long countFormsPerSubject(String subjectUUID, String questionnaireUUID,
-                                      ResourceResolver serviceResolver)
+    private long countFormsPerSubject(final String subjectUUID, final String questionnaireUUID,
+        final ResourceResolver serviceResolver)
     {
         long count = 0;
-        Iterator<Resource> results = serviceResolver.findResources(
-                "SELECT f.* FROM [cards:Form] AS f WHERE f.'subject'='" + subjectUUID + "'"
-                        + " AND f.'questionnaire'='" + questionnaireUUID + "'",
-                "JCR-SQL2"
-        );
+        final Iterator<Resource> results = serviceResolver.findResources(
+            "SELECT f.* FROM [cards:Form] AS f WHERE f.'subject'='" + subjectUUID + "'"
+                + " AND f.'questionnaire'='" + questionnaireUUID + "'",
+            "JCR-SQL2");
 
         while (results.hasNext()) {
             count += 1;
@@ -144,25 +124,13 @@ public class MaxFormsOfTypePerSubjectValidator implements Validator
      * @param uuid the jcr:uuid of the Questionnaire Resource which we wish to obtain
      * @return the matching Questionnaire Resource or null if none can be found
      */
-    private Resource getQuestionnaireResourceByUuid(ResourceResolver serviceResolver, String uuid)
+    private Resource getQuestionnaireResourceByUuid(final ResourceResolver serviceResolver, final String uuid)
     {
-        Iterator<Resource> resourceIterator = serviceResolver.findResources(
-                "SELECT * FROM [cards:Questionnaire] as q WHERE q.'jcr:uuid'='" + uuid + "'", "JCR-SQL2");
+        final Iterator<Resource> resourceIterator = serviceResolver.findResources(
+            "SELECT * FROM [cards:Questionnaire] as q WHERE q.'jcr:uuid'='" + uuid + "'", "JCR-SQL2");
         if (!resourceIterator.hasNext()) {
             return null;
         }
         return resourceIterator.next();
-    }
-
-    @Override
-    public Validator childNodeChanged(String name, NodeState before, NodeState after) throws CommitFailedException
-    {
-        return this;
-    }
-
-    @Override
-    public Validator childNodeDeleted(String name, NodeState before) throws CommitFailedException
-    {
-        return this;
     }
 }
