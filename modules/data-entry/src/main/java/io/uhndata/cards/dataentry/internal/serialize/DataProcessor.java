21f7bfe58dc75dc14613fb57f0a1d4577665a386
sofi2002sofi <sofia.didula@gmail.com>
2022-07-07 12:08:53 -0400

    CARDS-1807: Add support for specifying different selectors to use for serializing a subject's forms

--- modules/data-entry/src/main/java/io/uhndata/cards/dataentry/internal/serialize/DataProcessor.java
+++ modules/data-entry/src/main/java/io/uhndata/cards/dataentry/internal/serialize/DataProcessor.java
@@ -158,9 +158,11 @@ public class DataProcessor implements ResourceJsonProcessor
 
             final Map<String, JsonArrayBuilder> formsJsons = new HashMap<>();
             final ResourceResolver currentResolver = this.resolver.get();
-            final String currentSelectors = this.selectors.get();
-            forms.forEachRemaining(f ->
-                storeForm(currentResolver.resolve(f.getPath() + currentSelectors), formsJsons, isQuestionnaire));
+
+            final String currentSelectors = getCurrentSelectors();
+            forms.forEachRemaining(f -> {
+                    storeForm(currentResolver.resolve(f.getPath() + currentSelectors), formsJsons, isQuestionnaire);
+            });
             // The data JSONs have been collected, add them to the subject's JSON
             formsJsons.forEach(json::add);
             final JsonObjectBuilder filtersJson = Json.createObjectBuilder();
@@ -174,6 +176,14 @@ public class DataProcessor implements ResourceJsonProcessor
         } catch (RepositoryException e) {
             // Really shouldn't happen
         }
+    }
+
+    private String getCurrentSelectors()
+    {
+        if (this.options.get().containsKey("formSelectors")) {
+            return "." + this.options.get().get("formSelectors") + ".json";
+        }
+        return this.selectors.get();
     }
 
     private int depthLevelCounter(Node currentNode) throws RepositoryException
