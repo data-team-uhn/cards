6345ffbc4a5421b695b7eb75d14fdee2c1555d84
OrdiNeu <OrdinaryNeumann@gmail.com>
2022-07-19 13:29:48 -0400

    CARDS-1736: Refactor custom permissions code into the PermissionsManager service

--- modules/permissions/src/main/java/io/uhndata/cards/permissions/internal/PermissionsManagerService.java
+++ modules/permissions/src/main/java/io/uhndata/cards/permissions/internal/PermissionsManagerService.java
@@ -22,6 +22,7 @@ import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
@@ -104,6 +105,35 @@ public class PermissionsManagerService implements PermissionsManager
         }
     }
 
+    @Override
+    public void removeAccessControlEntry(String target, boolean isAllow, Principal principal, Set<String> restrictions,
+        Session session) throws RepositoryException
+    {
+        // Find the necessary ACL to remove
+        AccessControlManager acm = session.getAccessControlManager();
+        JackrabbitAccessControlList acl = AccessControlUtils.getAccessControlList(acm, target);
+        if (acl != null) {
+            // Find the necessary AccessControlEntry to remove
+            JackrabbitAccessControlEntry[] entries = (JackrabbitAccessControlEntry[]) acl.getAccessControlEntries();
+            JackrabbitAccessControlEntry toRemove = null;
+            for (JackrabbitAccessControlEntry entry : entries) {
+                if (entryHasDetails(entry, isAllow, principal, restrictions)) {
+                    // We've found the correct entry, make a note of it
+                    toRemove = entry;
+                    break;
+                }
+            }
+
+            // Remove it if it was found
+            if (toRemove != null) {
+                acl.removeAccessControlEntry(toRemove);
+                acm.setPolicy(target, acl);
+            } else {
+                throw new RepositoryException("Target ACL does not exist");
+            }
+        }
+    }
+
     /**
      * Determine if the given JackrabbitAccessControlEntry fits the given criteria.
      *
@@ -123,6 +153,27 @@ public class PermissionsManagerService implements PermissionsManager
         return (entry.isAllow() == isAllow
             && entry.getPrincipal().equals(principal)
             && entryHasPrivileges(entry, privileges)
+            && entryHasRestrictions(entry, restrictions));
+    }
+
+    /**
+     * Determine if the given JackrabbitAccessControlEntry fits the given criteria.
+     *
+     * @param entry entry to determine sameness of
+     * @param isAllow whether the request is to allow (true) or deny (false) access
+     * @param principal the Principal for the rule (i.e. target users to affect)
+     * @param privileges a comma-delimited list of privileges
+     * @param restrictions the restriction names to check for
+     * @return true if the JackrabbitAccessControlEntry fits the given criteria.
+     * @throws RepositoryException if an error occurs while obtaining restrictions from the entry
+     */
+    private boolean entryHasDetails(JackrabbitAccessControlEntry entry, boolean isAllow, Principal principal,
+        Set<String> restrictions) throws RepositoryException
+    {
+        // Ensure the principal and rules match
+        // Note that using getPrivileges().equals() fails to detect matches
+        return (entry.isAllow() == isAllow
+            && entry.getPrincipal().equals(principal)
             && entryHasRestrictions(entry, restrictions));
     }
 
@@ -173,6 +224,29 @@ public class PermissionsManagerService implements PermissionsManager
         // Key & entry check
         for (String key : entryRestrictions) {
             if (!restrictions.containsKey(key) || !entry.getRestriction(key).equals(restrictions.get(key))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check that the set of given restriction names is present in the given entry.
+     *
+     * @param entry entry to check
+     * @param restrictions set of restrictions names to check
+     * @return true if the entry's restrictions are all contained in the given set
+     * @throws RepositoryException if the restrictions could not be obtained from the entry
+     */
+    private boolean entryHasRestrictions(JackrabbitAccessControlEntry entry, Set<String> restrictions)
+        throws RepositoryException
+    {
+        // Note that we cannot easily obtain the restriction map from the entry
+        String[] entryRestrictions = entry.getRestrictionNames();
+
+        // Key & entry check
+        for (String key : entryRestrictions) {
+            if (!restrictions.contains(key)) {
                 return false;
             }
         }
