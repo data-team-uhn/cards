988816abaa246d30ff226cf92214876033e71bda
Michael Lescisin <michael.lescisin@uhn.ca>
2022-08-09 17:19:26 -0400

    CARDS-1783: Make email scheduling more flexible
    Rename the daysBeforeVisit parameter to daysToVisit and add javadoc

--- proms-resources/backend/src/main/java/io/uhndata/cards/proms/emailnotifications/GeneralNotificationsTask.java
+++ proms-resources/backend/src/main/java/io/uhndata/cards/proms/emailnotifications/GeneralNotificationsTask.java
@@ -32,13 +32,35 @@ public class GeneralNotificationsTask extends AbstractEmailNotification implemen
     private String emailSubject;
     private String plainTemplatePath;
     private String htmlTemplatePath;
-    private int daysBeforeVisit;
+    private int daysToVisit;
 
+    /*
+     * Task that sends email notifications as per the configuration parameters.
+     *
+     * @param resolverFactory a ResourceResolverFactory object that can be used for accessing the JCR
+     * @param tokenManager a TokenManager object that can be used for generating user authentication tokens,
+     * to provide authorization links in emails
+     *
+     * @param mailService a MailService object that can be used for sending emails
+     * @param taskName the name associated with the performance metrics gathered from this task
+     * @param clinicId the clinic ID that identifies the clinic for which notifications should be sent about
+     * (or null for all clinics)
+     *
+     * @param emailSubject the subject line of the email notifications
+     * @param plainTemplatePath the name of the plain text email template associated with the Visit's clinic,
+     * if clinicId is null, otherwise the absolute JCR path to the template.
+     *
+     * @param htmlTemplatePath the name of the HTML email template associated with the Visit's clinic,
+     * if clinicId is null, otherwise the absolute JCR path to the template.
+     *
+     * @param daysToVisit the difference in days between now and the day of the appointment.
+     * Positive values if the appointment is in the future, negative values if the appointment is in the past.
+     */
     @SuppressWarnings({ "checkstyle:ParameterNumber" })
     GeneralNotificationsTask(final ResourceResolverFactory resolverFactory,
         final TokenManager tokenManager, final MailService mailService, final String taskName,
         final String clinicId, final String emailSubject, final String plainTemplatePath,
-        final String htmlTemplatePath, final int daysBeforeVisit)
+        final String htmlTemplatePath, final int daysToVisit)
     {
         super(resolverFactory, tokenManager, mailService);
         this.taskName = taskName;
@@ -46,13 +68,13 @@ public class GeneralNotificationsTask extends AbstractEmailNotification implemen
         this.emailSubject = emailSubject;
         this.plainTemplatePath = plainTemplatePath;
         this.htmlTemplatePath = htmlTemplatePath;
-        this.daysBeforeVisit = daysBeforeVisit;
+        this.daysToVisit = daysToVisit;
     }
 
     @Override
     public void run()
     {
-        long emailsSent = sendNotification(this.daysBeforeVisit, this.plainTemplatePath, this.htmlTemplatePath,
+        long emailsSent = sendNotification(this.daysToVisit, this.plainTemplatePath, this.htmlTemplatePath,
             this.emailSubject, this.clinicId);
         Metrics.increment(this.resolverFactory, this.taskName, emailsSent);
     }
