125a0fc6ffaf946140563069ae53037854f05e70
Michael Lescisin <michael.lescisin@uhn.ca>
2022-08-19 12:25:29 -0400

    CARDS-1892: Publish the Debian/Ubuntu APT package status files for the VM's OS, for each VM that we are using for a CARDS deployment
    Add the Utilities/Administration/github_publish_os_package_list.py script

---
+++ Utilities/Administration/github_publish_os_package_list.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import jwt
+import sys
+import time
+import base64
+import distro
+import argparse
+import requests
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--private_key', help='File path the to private key used for JWT signing')
+argparser.add_argument('--app_id', help='GitHub App ID')
+argparser.add_argument('--installation_id', help='GitHub App installation ID')
+argparser.add_argument('--repository', help='OWNER/NAME of the GitHub repository (eg. data-team-uhn/cards)')
+argparser.add_argument('--deployment_hostname', help='Directory name of the host (eg. cardsdemo.uhndata.io)')
+args = argparser.parse_args()
+
+THIS_DISTRO = distro.id()
+
+with open(args.private_key, 'r') as f:
+	PRIVATE_KEY = f.read()
+
+def updateFile(path, new_content_file, installation_token):
+	installation_headers = {}
+	installation_headers['Accept'] = 'application/vnd.github+json'
+	installation_headers['Authorization'] = 'token ' + installation_token
+
+	prev_version_resp = requests.get("https://api.github.com/repos/" + args.repository + "/contents/" + path, headers=installation_headers)
+	prev_version_sha = prev_version_resp.json()['sha']
+
+	commit = {}
+	commit['message'] = "Updating " + path
+	commit['committer'] = {'name': 'update_os_package_list.py', 'email': 'user@localhost'}
+	with open(new_content_file, 'rb') as f:
+		commit['content'] = base64.b64encode(f.read()).decode()
+	commit['sha'] = prev_version_sha
+
+	r = requests.put("https://api.github.com/repos/" + args.repository + "/contents/" + path, headers=installation_headers, json=commit)
+	if r.status_code not in range(200, 300):
+		raise Exception("Failed to update " + path)
+
+payload = {}
+payload['iat'] = int(time.time())
+payload['exp'] = payload['iat'] + (60 * 10)
+payload['iss'] = args.app_id
+
+github_jwt = jwt.encode(payload, PRIVATE_KEY, algorithm='RS256')
+
+application_headers = {}
+application_headers['Accept'] = 'application/vnd.github+json'
+application_headers['Authorization'] = 'Bearer ' + github_jwt.decode()
+
+installation_token_resp = requests.post("https://api.github.com/app/installations/" + args.installation_id + "/access_tokens", headers=application_headers)
+installation_token = installation_token_resp.json()['token']
+
+if THIS_DISTRO == 'debian':
+	updateFile(args.deployment_hostname + "/vm/os-release", "/usr/lib/os-release", installation_token)
+	updateFile(args.deployment_hostname + "/vm/debian_version", "/etc/debian_version", installation_token)
+	updateFile(args.deployment_hostname + "/vm/status", "/var/lib/dpkg/status", installation_token)
+elif THIS_DISTRO == 'ubuntu':
+	updateFile(args.deployment_hostname + "/vm/os-release", "/usr/lib/os-release", installation_token)
+	updateFile(args.deployment_hostname + "/vm/lsb-release", "/etc/lsb-release", installation_token)
+	updateFile(args.deployment_hostname + "/vm/status", "/var/lib/dpkg/status", installation_token)
+else:
+	print("Error: Unable to handle this Linux distribution.")
+	sys.exit(-1)
