ebd56afb4fa2a2a7cfec0b770993c474543c44b9
sofi2002sofi <sofia.didula@gmail.com>
2022-08-23 02:13:42 -0400

    CARDS-1881: Periodical export task that serializes new forms as CSVs

---
+++ proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/export/ExportTask.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.uhndata.cards.proms.internal.export;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.uhndata.cards.serialize.CSVString;
+
+public class ExportTask implements Runnable
+{
+
+    private static final long DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+    /** Default log. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExportTask.class);
+
+    /** Provides access to resources. */
+    private final ResourceResolverFactory resolverFactory;
+    private final int frequencyInDays;
+    private final List<String> questionnairesToBeExported;
+    private final String savePath;
+
+    ExportTask(final ResourceResolverFactory resolverFactory, int frequencyInDays, String[] questionnairesToBeExported,
+               String savePath)
+    {
+        this.resolverFactory = resolverFactory;
+        this.frequencyInDays = frequencyInDays;
+        this.questionnairesToBeExported = new ArrayList<>(Arrays.asList(questionnairesToBeExported));
+        this.savePath = savePath;
+    }
+
+    @Override
+    public void run()
+    {
+        Set<Resource> questionnaires = this.getQuestionnaires(generateRequestedIdsQueryEnding());
+        final Date lastExportDate = new Date(System.currentTimeMillis() - (this.frequencyInDays * DAY_IN_MS));
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        final String modifiedAfterDate = simpleDateFormat.format(lastExportDate);
+        final File csvFile = new File(this.savePath + "/ExportedForms_" + modifiedAfterDate + ".csv");
+        try (FileWriter writer = new FileWriter(csvFile)) {
+            for (Resource questionnaire : questionnaires) {
+                final String csvPath = String.format(questionnaire.getPath() + ".data.dataFilter:modifiedAfter=%s.csv",
+                        modifiedAfterDate);
+                try (ResourceResolver resolver = this.resolverFactory.getServiceResourceResolver(null)) {
+                    final CSVString csv = resolver.resolve(csvPath).adaptTo(CSVString.class);
+                    writer.write(csv.toString());
+                } catch (LoginException | IOException e) {
+                    LOGGER.warn("Failed to get service session: {}", e.getMessage(), e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private String generateRequestedIdsQueryEnding()
+    {
+        String requestIdsQueryEnding = "";
+        if (this.questionnairesToBeExported != null && !this.questionnairesToBeExported.isEmpty()) {
+            ListIterator<String> export = this.questionnairesToBeExported.listIterator();
+            StringBuilder conditionQuery = new StringBuilder(" WHERE q.[jcr:uuid] = '" + export.next() + "'");
+            while (export.hasNext()) {
+                conditionQuery.append(" or q.[jcr:uuid] = '").append(export.next()).append("'");
+            }
+            requestIdsQueryEnding = conditionQuery.toString();
+        }
+        return requestIdsQueryEnding;
+    }
+
+    private Set<Resource> getQuestionnaires(String requestedIdsQuery)
+    {
+        try (ResourceResolver resolver = this.resolverFactory.getServiceResourceResolver(null)) {
+            String query = "SELECT * FROM [cards:Questionnaire] AS q" + requestedIdsQuery;
+            Iterator<Resource> results = resolver.findResources(query, "JCR-SQL2");
+            Set<Resource> questionnaires = new HashSet<>();
+            while (results.hasNext()) {
+                Resource obj = results.next();
+                questionnaires.add(obj);
+            }
+            return questionnaires;
+        } catch (LoginException e) {
+            LOGGER.warn("Failed to get service session: {}", e.getMessage(), e);
+        }
+        return Collections.emptySet();
+    }
+}
---
+++ proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/export/ScheduledExport.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.uhndata.cards.proms.internal.export;
+
+import java.util.List;
+
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.commons.scheduler.ScheduleOptions;
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true)
+public class ScheduledExport
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledExport.class);
+    private static final String SCHEDULER_JOB_PREFIX = "ScheduledExport-";
+
+    @Reference
+    private ResourceResolverFactory resolverFactory;
+
+    @Reference
+    private Scheduler scheduler;
+
+    @Reference(policyOption = ReferencePolicyOption.GREEDY, bind = "configAdded", unbind = "configRemoved")
+    private volatile List<ExportConfig> configs;
+
+    public void configAdded(final ExportConfig newConfig)
+    {
+        if (this.scheduler == null) {
+            // This method can be called both before the component is activated, while the scheduler dependency may
+            // still be unsatisfied, and while the component is active and the configuration is changed.
+            // If the scheduler is not bound, it's OK to do nothing now, the method will be called during activation.
+            return;
+        }
+
+        final String schedule = "0 0 */" + 24 * newConfig.getConfig().frequency_in_days() + " ? * *";
+        final ScheduleOptions options = this.scheduler.EXPR(schedule);
+        options.name(SCHEDULER_JOB_PREFIX + newConfig.getConfig());
+        options.canRunConcurrently(true);
+
+        final Runnable importJob;
+        importJob =
+                new ExportTask(this.resolverFactory, newConfig.getConfig().frequency_in_days(),
+                        newConfig.getConfig().questionnaires_to_be_exported(),
+                        newConfig.getConfig().save_path());
+        try {
+            this.scheduler.schedule(importJob, options);
+        } catch (final Exception e) {
+            LOGGER.error("ScheduledExport Failed to schedule: {}", e.getMessage(), e);
+        }
+    }
+
+    public void configRemoved(final ExportConfig removedConfig)
+    {
+        LOGGER.debug("Removed torch importer config {}", removedConfig.getConfig());
+        this.scheduler.unschedule(SCHEDULER_JOB_PREFIX + removedConfig.getConfig());
+    }
+
+    @Activate
+    protected void activate(ComponentContext componentContext) throws Exception
+    {
+        this.configs.forEach(this::configAdded);
+    }
+}
