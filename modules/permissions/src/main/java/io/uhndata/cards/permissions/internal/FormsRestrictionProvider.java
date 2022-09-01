049bec27c027c50bb34ac6d59fb89f29d58d5bf8
fnguyen <OrdinaryNeumann@gmail.com>
2022-09-01 13:34:13 -0400

    CARDS-1911: Code cleanup

--- modules/permissions/src/main/java/io/uhndata/cards/permissions/internal/FormsRestrictionProvider.java
+++ modules/permissions/src/main/java/io/uhndata/cards/permissions/internal/FormsRestrictionProvider.java
@@ -128,6 +128,6 @@ public class FormsRestrictionProvider extends AbstractRestrictionProvider
         } else {
             LOGGER.debug("Ignoring unsupported restriction {}", name);
         }
-        return RestrictionPattern.EMPTY;
+        return null;
     }
 }
