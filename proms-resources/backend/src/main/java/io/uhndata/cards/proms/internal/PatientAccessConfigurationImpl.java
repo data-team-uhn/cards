data-team-uhn/cards

commit cc157e2a81f0eb69874b06d8186b7245d90ec46e

CARDS-1867: Patient authentication is always required if tokenless auth is disabled

proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/PatientAccessConfigurationImpl.java
@@ -102,7 +102,8 @@ public class PatientAccessConfigurationImpl extends AbstractNodeUtils implements
         try
         {
             Property required = getConfig(PATIENT_IDENTIFICATION_REQUIRED_PROP);
-            return required == null ? PATIENT_IDENTIFICATION_REQUIRED_DEFAULT : required.getBoolean();
+            return (!isTokenlessAuthEnabled())
+                || (required == null ? PATIENT_IDENTIFICATION_REQUIRED_DEFAULT : required.getBoolean());
         } catch (RepositoryException e) {
             return PATIENT_IDENTIFICATION_REQUIRED_DEFAULT;
         }

commit c2bf84d55177192799e8773bd6767aae37a79a2d

CARDS-1867: Patient authentication is always required if tokenless auth is disabled - fix logic bug introduced in cc157e2

proms-resources/backend/src/main/java/io/uhndata/cards/proms/internal/PatientAccessConfigurationImpl.java
@@ -102,7 +102,7 @@ public class PatientAccessConfigurationImpl extends AbstractNodeUtils implements
         try
         {
             Property required = getConfig(PATIENT_IDENTIFICATION_REQUIRED_PROP);
-            return (!isTokenlessAuthEnabled())
+            return isTokenlessAuthEnabled()
                 || (required == null ? PATIENT_IDENTIFICATION_REQUIRED_DEFAULT : required.getBoolean());
         } catch (RepositoryException e) {
             return PATIENT_IDENTIFICATION_REQUIRED_DEFAULT;