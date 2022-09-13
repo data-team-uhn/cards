f338882caed3dc7229830285fcbc1781684a1c8c
Michael Lescisin <michael.lescisin@uhn.ca>
2022-09-13 16:24:20 -0400

    CARDS-1892: Publish the Debian/Ubuntu APT package status files for the VM's OS, for each VM that we are using for a CARDS deployment
    Refactor github_publish_os_package_list.py by moving the GitHub API interaction code into GitHubRepoHandler.py as future scripts to be created under CARDS-1701 will need to use this functionality

--- Utilities/Administration/github_publish_os_package_list.py
+++ Utilities/Administration/github_publish_os_package_list.py
@@ -19,15 +19,12 @@
 # under the License.
 
 import os
-import jwt
 import sys
-import time
-import base64
 import distro
 import argparse
-import requests
 
 import os_file_management
+from GitHubRepoHandler import GitHubRepoHandler
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--private_key', help='File path the to private key used for JWT signing')
@@ -39,47 +36,13 @@ args = argparser.parse_args()
 
 THIS_DISTRO = distro.id()
 
-with open(args.private_key, 'r') as f:
-	PRIVATE_KEY = f.read()
-
-def updateFile(path, new_content_file, installation_token):
-	installation_headers = {}
-	installation_headers['Accept'] = 'application/vnd.github+json'
-	installation_headers['Authorization'] = 'token ' + installation_token
-
-	prev_version_resp = requests.get("https://api.github.com/repos/" + args.repository + "/contents/" + path, headers=installation_headers)
-	prev_version_sha = prev_version_resp.json()['sha']
-
-	commit = {}
-	commit['message'] = "Updating " + path
-	commit['committer'] = {'name': 'update_os_package_list.py', 'email': 'user@localhost'}
-	with open(new_content_file, 'rb') as f:
-		commit['content'] = base64.b64encode(f.read()).decode()
-	commit['sha'] = prev_version_sha
-
-	r = requests.put("https://api.github.com/repos/" + args.repository + "/contents/" + path, headers=installation_headers, json=commit)
-	if r.status_code not in range(200, 300):
-		raise Exception("Failed to update " + path)
-
-payload = {}
-payload['iat'] = int(time.time())
-payload['exp'] = payload['iat'] + (60 * 10)
-payload['iss'] = args.app_id
-
-github_jwt = jwt.encode(payload, PRIVATE_KEY, algorithm='RS256')
-
-application_headers = {}
-application_headers['Accept'] = 'application/vnd.github+json'
-application_headers['Authorization'] = 'Bearer ' + github_jwt.decode()
-
-installation_token_resp = requests.post("https://api.github.com/app/installations/" + args.installation_id + "/access_tokens", headers=application_headers)
-installation_token = installation_token_resp.json()['token']
+gh_client = GitHubRepoHandler(cli_args=args, bot_username="github_publish_package_list.py")
 
 try:
 	for system_file_path in os_file_management.OS_FILE_SET[THIS_DISTRO]:
 		system_file_name = os.path.basename(system_file_path)
 		github_path = os.path.join('hosts', args.deployment_hostname, 'vm', system_file_name)
-		updateFile(github_path, system_file_path, installation_token)
+		gh_client.updateGitHubFile(github_path, system_file_path)
 except KeyError:
 	print("Error: Unable to handle this Linux distribution.")
 	sys.exit(-1)
