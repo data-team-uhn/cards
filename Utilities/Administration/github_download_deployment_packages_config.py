46e51f37d73a601f5150c87e0b6cb0b1417e0775
Michael Lescisin <michael.lescisin@uhn.ca>
2022-09-13 16:49:08 -0400

    CARDS-1896: Write a script that takes a set of APT or APK package status files and determines if there are available fixes for any vulnerabilities within those packages
    Refactor github_download_deployment_packages_config.py so that all GitHub API interaction code is done through GitHubRepoHandler.py

--- Utilities/Administration/github_download_deployment_packages_config.py
+++ Utilities/Administration/github_download_deployment_packages_config.py
@@ -19,12 +19,10 @@
 # under the License.
 
 import os
-import sys
-import jwt
-import time
 import string
 import argparse
-import requests
+
+from GitHubRepoHandler import GitHubRepoHandler
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--private_key', help='File path the to private key used for JWT signing')
@@ -35,42 +33,7 @@ argparser.add_argument('--deployment_hostname', help='Directory name of the host
 argparser.add_argument('--download_dir', help='Directory to save the downloaded files to')
 args = argparser.parse_args()
 
-if args.private_key is None:
-	# Try to load it via environment variable
-	if 'GITHUB_API_PRIVATE_KEY' not in os.environ:
-		print("Error: --private_key was not specified and GITHUB_API_PRIVATE_KEY environment variable does not exist.")
-		sys.exit(-1)
-	PRIVATE_KEY = os.environ['GITHUB_API_PRIVATE_KEY']
-else:
-	with open(args.private_key, 'r') as f:
-		PRIVATE_KEY = f.read()
-
-if args.app_id is None:
-	# Try to set it via environment variable
-	if 'GITHUB_API_APP_ID' not in os.environ:
-		print("Error: --app_id was not specified and GITHUB_API_APP_ID environment variable does not exist.")
-		sys.exit(-1)
-	APP_ID = os.environ['GITHUB_API_APP_ID']
-else:
-	APP_ID = args.app_id
-
-if args.installation_id is None:
-	# Try to set it via environment variable
-	if 'GITHUB_API_INSTALLATION_ID' not in os.environ:
-		print("Error: --installation_id was not specified and GITHUB_API_INSTALLATION_ID environment variable does not exist.")
-		sys.exit(-1)
-	INSTALLATION_ID = os.environ['GITHUB_API_INSTALLATION_ID']
-else:
-	INSTALLATION_ID = args.installation_id
-
-if args.repository is None:
-	# Try to set it via environment variable
-	if 'GITHUB_REPOSITORY' not in os.environ:
-		print("Error: --repository was not specified and GITHUB_REPOSITORY environment variable does not exist.")
-		sys.exit(-1)
-	REPOSITORY = os.environ['GITHUB_REPOSITORY']
-else:
-	REPOSITORY = args.repository
+gh_client = GitHubRepoHandler(cli_args=args, bot_username="github_download_deployment_packages_config.py")
 
 def isValidFilename(filename):
 	validChars = string.ascii_letters + string.digits + '-' + '_' + ' '
@@ -79,48 +42,11 @@ def isValidFilename(filename):
 			return False
 	return True
 
-def downloadFile(path, save_path, installation_token):
-	installation_headers = {}
-	installation_headers['Accept'] = 'application/vnd.github.raw'
-	installation_headers['Authorization'] = 'token ' + installation_token
-
-	resp = requests.get("https://api.github.com/repos/" + REPOSITORY + "/contents/" + path, headers=installation_headers)
-	if resp.status_code != 200:
-		raise Exception("HTTP {} was returned when attempting to download {}.".format(resp.status_code, path))
-	with open(save_path, 'wb') as f_save:
-		f_save.write(resp.content)
-
-def listDirectory(path, installation_token):
-	installation_headers = {}
-	installation_headers['Accept'] = 'application/vnd.github+json'
-	installation_headers['Authorization'] = 'token ' + installation_token
-
-	resp = requests.get("https://api.github.com/repos/" + REPOSITORY + "/contents/" + path, headers=installation_headers)
-	if resp.status_code != 200:
-		raise Exception("HTTP {} was returned when attempting to list {}.".format(resp.status_code, path))
-
-	directory_contents = [ str(n['name']) for n in resp.json() ]
-	return directory_contents
-
-payload = {}
-payload['iat'] = int(time.time())
-payload['exp'] = payload['iat'] + (60 * 10)
-payload['iss'] = APP_ID
-
-github_jwt = jwt.encode(payload, PRIVATE_KEY, algorithm='RS256')
-
-application_headers = {}
-application_headers['Accept'] = 'application/vnd.github+json'
-application_headers['Authorization'] = 'Bearer ' + github_jwt.decode()
-
-installation_token_resp = requests.post("https://api.github.com/app/installations/" + INSTALLATION_ID + "/access_tokens", headers=application_headers)
-installation_token = installation_token_resp.json()['token']
-
 # Create the {{args.download_dir}}/{{args.deployment_hostname}}/vm directory structure
 os.mkdir(os.path.join(args.download_dir, args.deployment_hostname))
 os.mkdir(os.path.join(args.download_dir, args.deployment_hostname, 'vm'))
 
 # Use the installation_token to download everything in the repository under the args.deployment_hostname directory
-for filename in listDirectory('hosts/' + args.deployment_hostname + '/vm', installation_token):
+for filename in gh_client.listGitHubDirectory('hosts/' + args.deployment_hostname + '/vm'):
 	if isValidFilename(filename):
-		downloadFile('hosts/' + args.deployment_hostname + '/vm/' + filename, os.path.join(args.download_dir, args.deployment_hostname, 'vm', filename), installation_token)
+		gh_client.downloadGitHubFile('hosts/' + args.deployment_hostname + '/vm/' + filename, os.path.join(args.download_dir, args.deployment_hostname, 'vm', filename))
