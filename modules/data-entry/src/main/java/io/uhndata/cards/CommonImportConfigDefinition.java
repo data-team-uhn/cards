1e1f57f347696e0d3bc6cab2c576b60b8d1651bc
fnguyen <OrdinaryNeumann@gmail.com>
2022-11-15 16:28:59 -0500

    CARDS-1951: Add a configuration that allows to skip the survey scheduling and pre-creation of forms

---
+++ modules/data-entry/src/main/java/io/uhndata/cards/CommonImportConfigDefinition.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.uhndata.cards;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "Common import",
+    description = "Configuration for the data importers")
+public @interface CommonImportConfigDefinition
+{
+    /** Whether or not to handle scheduling surveys. */
+    boolean SURVEY_SCHEDULING_ENABLED = true;
+
+    @AttributeDefinition(name = "Import schedule",
+        description = "When enabled, survey scheduling is handled by CARDS.")
+    boolean survey_scheduling_enabled() default SURVEY_SCHEDULING_ENABLED;
+}
