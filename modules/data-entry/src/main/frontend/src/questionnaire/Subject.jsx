30ead64c138a3356f096d54f5fe2f1166faf8f79
veronikaslc <veronika.koltunov@gmail.com>
2022-11-22 09:36:18 -0500

    CARDS-1953: Loading a patient chart should display a loading indicator while the forms are being fetched Adressed codereview comments

--- modules/data-entry/src/main/frontend/src/questionnaire/Subject.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/Subject.jsx
@@ -328,7 +328,7 @@ function SubjectHeader(props) {
     fetchSubjectData();
   }, [id]);
 
-  if (!subject?.data) {
+  if (!subject) {
     return (
       <Grid item><CircularProgress/></Grid>
     );
@@ -398,7 +398,7 @@ function SubjectMemberInternal (props) {
   // Whether a subject is expanded and displaying its forms
   // The root subject is always expanded. Child subjects are collapsed by default.
   let [ expanded, setExpanded ] = useState(level == 0);
-  let [ subjectGroups, setSubjectGroups ] = useState();
+  let [ subjectGroups, setSubjectGroups ] = useState(null);
 
   let globalLoginDisplay = useContext(GlobalLoginContext);
 
@@ -436,6 +436,13 @@ function SubjectMemberInternal (props) {
   useEffect(() => {
     fetchTableData();
   }, [data['jcr:uuid']]);
+
+  // If the subjectGroups data has not yet been fetched, return an in-progress symbol
+  if (!subjectGroups) {
+    return (
+      <Grid container justifyContent="center" className={classes.circularProgressContainer}><Grid item><CircularProgress/></Grid></Grid>
+    );
+  }
 
   // If an error was returned, do not display a subject at all, but report the error
   if (error) {
@@ -497,8 +504,13 @@ function SubjectMemberInternal (props) {
           </Grid>
         </Grid>
       }
-      { expanded && (
-        Object.keys(subjectGroups || {}).length > 0 ? <>
+      { /* If we finished all fetching and have no data or child subjects to display for this subject, inform the user */ }
+      { expanded && childSubjects && childSubjects.length == 0 && subjectGroups && Object.keys(subjectGroups).length == 0 &&
+        <Grid item>
+          <Typography color="textSecondary" variant="caption">{`No data associated with this ${label.toLowerCase()} was found.`}</Typography>
+        </Grid>
+      }
+      { expanded && subjectGroups && <>
         {
           Object.keys(subjectGroups).map( (questionnaireTitle, j) => (
             <Grid item key={questionnaireTitle}>
@@ -587,13 +599,7 @@ function SubjectMemberInternal (props) {
           ))
         }
         </>
-        :
-        ( !childSubjects?.length && // If we have no data to display for this subject, inform the user
-          <Grid item>
-            <Typography color="textSecondary" variant="caption">{`No data associated with this ${label.toLowerCase()} was found.`}</Typography>
-          </Grid>
-        )
-      )}
+      }
       { /* Render child subjects at the bottom when the current subject is expanded */ }
       { expanded && childSubjects?.length ?
         (<Grid item xs={12} className={classes.subjectNestedContainer}>
