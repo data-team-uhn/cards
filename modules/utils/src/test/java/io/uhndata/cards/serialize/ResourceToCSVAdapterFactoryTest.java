68427cece2074bc4705fe5e3a2d3c127768bd82f
sofi2002sofi <sofia.didula@gmail.com>
2023-03-17 20:21:00 -0300

    CARDS-2154: Increase test coverage cards-utils
    fixes for ResourceToCSVAdapterFactory

--- modules/utils/src/test/java/io/uhndata/cards/serialize/ResourceToCSVAdapterFactoryTest.java
+++ modules/utils/src/test/java/io/uhndata/cards/serialize/ResourceToCSVAdapterFactoryTest.java
@@ -31,14 +31,18 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import io.uhndata.cards.serialize.spi.ResourceCSVProcessor;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for {@link ResourceToCSVAdapterFactory}.
  *
- * @version $Id $
+ * @version $Id$
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ResourceToCSVAdapterFactoryTest
@@ -78,7 +82,7 @@ public class ResourceToCSVAdapterFactoryTest
     }
 
     @Test
-    public void getAdapterForResourceAdaptableObjectReturnsResourcePath()
+    public void getAdapterForUnsupportedResourceReturnsResourcePath()
     {
         Resource adaptable = mock(Resource.class);
         ResourceCSVProcessor processor = mock(ResourceCSVProcessor.class);
@@ -89,6 +93,45 @@ public class ResourceToCSVAdapterFactoryTest
         CSVString adapter = this.factory.getAdapter(adaptable, CSVString.class);
         assertNotNull(adapter);
         assertEquals(TEST_SUBJECT_PATH, adapter.toString());
+    }
+
+    @Test
+    public void getAdapterWithNoProcessorsReturnsResourcePath()
+    {
+        Resource adaptable = mock(Resource.class);
+
+        when(adaptable.getPath()).thenReturn(TEST_SUBJECT_PATH);
+
+        Whitebox.setInternalState(this.factory, "allProcessors", List.of());
+        CSVString adapter = this.factory.getAdapter(adaptable, CSVString.class);
+        assertNotNull(adapter);
+        assertEquals(TEST_SUBJECT_PATH, adapter.toString());
+    }
+
+    @Test
+    public void getAdapterUsesFirstProcessorThatCanProcess()
+    {
+        Resource adaptable = mock(Resource.class);
+
+        ResourceCSVProcessor processor1 = mock(ResourceCSVProcessor.class);
+        when(processor1.canProcess(adaptable)).thenReturn(false);
+
+        ResourceCSVProcessor processor2 = mock(ResourceCSVProcessor.class);
+        when(processor2.canProcess(adaptable)).thenReturn(true);
+        String data = NODE_IDENTIFIER + "," + CREATED_BY_PROPERTY + "\n"
+                + UUID.randomUUID() + ",admin";
+        when(processor2.serialize(adaptable)).thenReturn(data);
+
+        ResourceCSVProcessor processor3 = mock(ResourceCSVProcessor.class);
+
+        Whitebox.setInternalState(this.factory, "allProcessors",
+                List.of(processor1, processor2, processor3));
+        CSVString adapter = this.factory.getAdapter(adaptable, CSVString.class);
+        verify(processor1, times(0)).serialize(adaptable);
+        verify(processor2, times(1)).serialize(adaptable);
+        verify(processor3, times(0)).serialize(adaptable);
+        assertNotNull(adapter);
+        assertEquals(data, adapter.toString());
     }
 
 }
