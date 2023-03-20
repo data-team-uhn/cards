a2988e9bca584b964d9991b8212e0a8193efe553
Marta Girdea <marta.girdea@gmail.com>
2023-03-20 10:03:38 -0400

    CARDS-1768: Refactoring/UX improvement: replace long 'Select's with Autocomplete Added documentation to QuestionnaireUtilities

--- modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireUtilities.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireUtilities.jsx
@@ -17,12 +17,34 @@
 //  under the License.
 //
 
+// Removes the "cards:" prefix from a jcr:primaryType string
+// Example: "cards:Question" -> "Question"
 export function stripCardsNamespace (str) { return str?.replaceAll(/^cards:/g, ""); }
 
+// Returns an array of all the questionnaire entries of specified types found in a json
+//
+// Parameters:
+// * json: a JSON object, usually the response of `/Questionaire/<QuestionnaireID>.deep.json
+// * entryTypes: an array of strings specifying the `jcr:primaryType`s of the entities that
+//   should be retrieved. By default, Questions and Sections are targeted.
+// * rootPath: a string that specifies the jcr path relatively to which the `relativePath` of
+//   the results should be computed. Defaults to the `@path` field in the `json`
+// * result: an array holding the retrieved entries, populated by the function's recursive
+//   calls and returned at the end. Can be prepopulated with some entries, is [] by default
+// Returns: an array of objects containing all entries found in the json whose jcr:primaryType
+//   match one of the provided entryTypes. The shape of the objects is:
+//   {
+//     id: the uuid (string),
+//     name: the @name (string),
+//     text: the text or label (string)
+//     path: the @path (string)
+//     relativePath: the path after removing rootPath form the start and @name from the end (string)
+//     type: the primary type, withoug the "cards:" prefix
+//   }
 export function findQuestionnaireEntries(
   json,
   entryTypes=["cards:Question", "cards:Section"],
-  rootPath = json['@path'],
+  rootPath = (json['@path'] || ''),
   result = []
 ) {
   if (typeof(json) == "object") {
