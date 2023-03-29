3e09b8cf85b9270b9e07943433589aac869295f3
Michael Lescisin <michael.lescisin@uhn.ca>
2023-03-29 13:05:40 -0400

    CARDS-2173: Upgrade the version of Alpine Linux used by CARDS Docker container images
    In Alpine Linux 3.17, /usr/bin/python already exists as a symlink that points to /usr/bin/python3

--- distribution/make_filled_m2.sh
+++ distribution/make_filled_m2.sh
@@ -19,5 +19,4 @@
 
 apk update || exit -1
 apk add bash openjdk11 maven python3 gcompat || exit -1
-ln -s /usr/bin/python3 /usr/bin/python || exit -1
 mvn clean install -Prelease -Dgpg.skip -Dmaven.javadoc.skip || exit -1
