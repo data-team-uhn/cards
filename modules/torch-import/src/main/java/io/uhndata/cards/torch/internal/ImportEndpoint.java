67cf32fd8feb0b24572e4ccdd9e554f0ed6d23d9
Sergiu Dumitriu <sergiu@phenotips.org>
2023-04-20 14:33:07 -0400

    CARDS-1886: Replace Torch import with Clarity import Move the Torch Import code into a separate module

--- modules/torch-import/src/main/java/io/uhndata/cards/torch/internal/ImportEndpoint.java
+++ modules/torch-import/src/main/java/io/uhndata/cards/torch/internal/ImportEndpoint.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package io.uhndata.cards.proms.internal.importer;
+package io.uhndata.cards.torch.internal;
 
 import java.io.IOException;
 import java.util.List;
