22c5b417baa7990a876fdd919e838498717a28c4
Sergiu Dumitriu <sergiu@phenotips.org>
2023-05-05 21:55:42 -0400

    CARDS-2200: Enable the Survey Tracker in proms Make the SurveyTracker class easy to customize

--- prems-resources/backend/src/main/java/io/uhndata/cards/prems/internal/surveytracker/SurveyTracker.java
+++ prems-resources/backend/src/main/java/io/uhndata/cards/prems/internal/surveytracker/SurveyTracker.java
@@ -32,11 +32,16 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.observation.ResourceChange;
 import org.apache.sling.api.resource.observation.ResourceChangeListener;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,12 +57,14 @@ import io.uhndata.cards.subjects.api.SubjectUtils;
  *
  * @version $Id$
  */
-@Component(immediate = true, service = { ResourceChangeListener.class, EventHandler.class }, property = {
-    ResourceChangeListener.PATHS + "=/Forms",
-    ResourceChangeListener.CHANGES + "=ADDED",
-    ResourceChangeListener.CHANGES + "=CHANGED",
-    EventConstants.EVENT_TOPIC + "=Notification/Patient/Appointment/*",
-})
+@Component(configurationPolicy = ConfigurationPolicy.REQUIRE,
+    service = { ResourceChangeListener.class, EventHandler.class }, property = {
+        ResourceChangeListener.PATHS + "=/Forms",
+        ResourceChangeListener.CHANGES + "=ADDED",
+        ResourceChangeListener.CHANGES + "=CHANGED",
+        EventConstants.EVENT_TOPIC + "=Notification/Patient/Appointment/*",
+    })
+@Designate(ocd = SurveyTracker.Config.class)
 public class SurveyTracker implements ResourceChangeListener, EventHandler
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(SurveyTracker.class);
@@ -83,15 +90,42 @@ public class SurveyTracker implements ResourceChangeListener, EventHandler
     @Reference
     private PatientAccessConfiguration accessConfiguration;
 
+    private final boolean trackSubmissions;
+
+    private final boolean trackEmails;
+
+    @ObjectClassDefinition(name = "Patient portal - Survey tracker")
+    public @interface Config
+    {
+        @AttributeDefinition(name = "Track survey submission")
+        boolean trackSubmissions() default true;
+
+        @AttributeDefinition(name = "Track invitation emails")
+        boolean trackEmails() default true;
+    }
+
+    @Activate
+    public SurveyTracker(Config config)
+    {
+        this.trackSubmissions = config.trackSubmissions();
+        this.trackEmails = config.trackEmails();
+    }
+
     @Override
     public void onChange(final List<ResourceChange> changes)
     {
+        if (!this.trackSubmissions) {
+            return;
+        }
         changes.forEach(this::handleResourceEvent);
     }
 
     @Override
     public void handleEvent(Event event)
     {
+        if (!this.trackEmails) {
+            return;
+        }
         boolean mustPopResolver = false;
         try (ResourceResolver localResolver = this.resolverFactory
             .getServiceResourceResolver(Map.of(ResourceResolverFactory.SUBSERVICE, "SurveyTracker"))) {
@@ -103,8 +137,12 @@ public class SurveyTracker implements ResourceChangeListener, EventHandler
             final Node surveyStatusQuestionnaire = session.getNode("/Questionnaires/Survey events");
             final Node surveyStatusForm =
                 ensureSurveyStatusFormExists(surveyStatusQuestionnaire, visitSubject, session);
-            final Node question = surveyStatusQuestionnaire
-                .getNode(StringUtils.toRootLowerCase(StringUtils.substringAfterLast(event.getTopic(), "/")) + "_sent");
+            final String questionName =
+                StringUtils.toRootLowerCase(StringUtils.substringAfterLast(event.getTopic(), "/")) + "_sent";
+            if (!surveyStatusQuestionnaire.hasNode(questionName)) {
+                return;
+            }
+            final Node question = surveyStatusQuestionnaire.getNode(questionName);
             final Node answer = this.formUtils.getAnswer(surveyStatusForm, question);
             if (answer != null) {
                 answer.setProperty("value", Calendar.getInstance());
@@ -167,6 +205,9 @@ public class SurveyTracker implements ResourceChangeListener, EventHandler
 
     private void updateSurveySubmittedDate(final Node submittedAnswer, final Session session) throws RepositoryException
     {
+        if (!session.nodeExists("/Questionnaires/Survey events/responses_received")) {
+            return;
+        }
         final Node surveyStatusQuestionnaire = session.getNode("/Questionnaires/Survey events");
         final Node surveyStatusForm = ensureSurveyStatusFormExists(surveyStatusQuestionnaire,
             this.formUtils.getSubject(this.formUtils.getForm(submittedAnswer)), session);
@@ -181,6 +222,9 @@ public class SurveyTracker implements ResourceChangeListener, EventHandler
     private void updateSurveyExpirationDate(final Node dischargedAnswer, final Session session)
         throws RepositoryException
     {
+        if (!session.nodeExists("/Questionnaires/Survey events/survey_expiry")) {
+            return;
+        }
         Calendar eventDate = (Calendar) this.formUtils.getValue(dischargedAnswer);
         if (eventDate != null) {
             final Node surveyStatusQuestionnaire = session.getNode("/Questionnaires/Survey events");
