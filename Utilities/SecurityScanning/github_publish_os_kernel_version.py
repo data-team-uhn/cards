825e521b665800018fe378793d48c81df0449896
Michael Lescisin <michael.lescisin@uhn.ca>
2023-06-09 15:27:05 -0400

    CARDS-2262: Publish the currently running kernel version for the VM so that vulnerabilities in non-running kernels can be safely ignored
    Add the github_publish_os_kernel_version.py utility

---
+++ Utilities/SecurityScanning/github_publish_os_kernel_version.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+import argparse
+import platform
+
+from GitHubRepoHandler import GitHubRepoHandler
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--private_key', help='File path to the private key used for JWT signing')
+argparser.add_argument('--app_id', help='GitHub App ID')
+argparser.add_argument('--installation_id', help='GitHub App installation ID')
+argparser.add_argument('--repository', help='OWNER/NAME of the GitHub repository (eg. data-team-uhn/cards)')
+argparser.add_argument('--deployment_hostname', help='Directory name of the host (eg. cardsdemo.uhndata.io)')
+args = argparser.parse_args()
+
+gh_client = GitHubRepoHandler(cli_args=args, bot_username="github_publish_os_kernel_version.py")
+
+github_path = os.path.join('hosts', args.deployment_hostname, 'vm', 'kernel_version')
+gh_client.writeGitHubTextFile(github_path, platform.release())
