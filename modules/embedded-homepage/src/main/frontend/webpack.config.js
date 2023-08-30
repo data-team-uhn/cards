9b3091d899f2610977f6ce43bbad5bfb875a9474
veronikaslc <veronika.koltunov@gmail.com>
2023-08-30 11:01:38 -0400

    CARDS-2323: Embedded view of subject charts and forms, without navigation elements

---
+++ modules/embedded-homepage/src/main/frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+
+module_name = require("./package.json").name + ".";
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    [module_name + 'themeindex']: './src/embeddedThemePage/index.jsx'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new WebpackAssetsManifest({
+      output: "assets.json"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist/SLING-INF/content/libs/cards/resources/',
+    publicPath: '/',
+    filename: '[name].[contenthash].js',
+  }
+};
