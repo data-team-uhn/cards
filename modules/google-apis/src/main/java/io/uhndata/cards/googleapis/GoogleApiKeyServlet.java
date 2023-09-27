90047e59b5a67ba209d702fb51848f1dfdddb04d
veronikaslc <veronika.koltunov@gmail.com>
2023-09-26 22:36:19 -0400

    CARDS-2348: New question type: postal address Better name for the bundle

--- modules/google-apis/src/main/java/io/uhndata/cards/googleapis/GoogleApiKeyServlet.java
+++ modules/google-apis/src/main/java/io/uhndata/cards/googleapis/GoogleApiKeyServlet.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package io.uhndata.cards.google;
+package io.uhndata.cards.googleapis;
 
 import java.io.IOException;
 import java.io.Writer;
