607ab8bf0c4c19c06cfd93cfac4b5acfe00f55c2
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2023-12-19 13:51:05 -0500

    CARDS-2450: Promote unsubmitted permissions to a core platform module

--- modules/permissions-unsubmitted/src/main/java/io/uhndata/cards/permissions/internal/UnsubmittedFormsRestrictionPattern.java
+++ modules/permissions-unsubmitted/src/main/java/io/uhndata/cards/permissions/internal/UnsubmittedFormsRestrictionPattern.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package io.uhndata.cards.proms.permissions;
+package io.uhndata.cards.permissions.internal;
 
 import java.util.List;
 
