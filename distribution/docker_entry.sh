8b2529800d649ccabd0fe012056318b142b83f31
Michael Lescisin <michael.lescisin@uhn.ca>
2024-01-11 11:38:42 -0500

    CARDS-2317: Split CARDS into multiple projects
    distribution/docker_entry.sh: The version of the CARDS-based project (eg. HERACLES, SPARC, etc...), as opposed to the version of the CARDS core platform, to be launched is now determined by the CARDS_PROJECT_VERSION environment variable and not the CARDS_VERSION environment variable

--- distribution/docker_entry.sh
+++ distribution/docker_entry.sh
@@ -45,9 +45,9 @@ VALID_CARDS_PROJECTS="||cards4kids|cards4lfs|cards4proms|cards4prems|cards4herac
 echo "${VALID_CARDS_PROJECTS}" | grep -q "|${CARDS_PROJECT}|" || { echo "Invalid project specified - defaulting to generic CARDS."; unset CARDS_PROJECT; }
 
 featureFlagString=""
-if [ ! -z $CARDS_PROJECT ]
+if [ ! -z $CARDS_PROJECT ] && [ ! -z $CARDS_PROJECT_VERSION ]
 then
-  featureFlagString="$featureFlagString -f mvn:io.uhndata.cards/${CARDS_PROJECT}/${CARDS_VERSION}/slingosgifeature"
+  featureFlagString="$featureFlagString -f mvn:io.uhndata.cards/${CARDS_PROJECT}/${CARDS_PROJECT_VERSION}/slingosgifeature"
 fi
 
 [[ "${CARDS_PROJECT}" == 'cards4proms' || "${CARDS_PROJECT}" == 'cards4prems' ]] && SMTPS_ENABLED="true"
