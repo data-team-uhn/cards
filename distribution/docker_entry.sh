5005e51151129336b1fc4b1da72150a74d9bffce
Andrew Crowther <acrowthe3388@gmail.com>
2024-04-23 11:32:04 -0400

    CARDS-2514: New "locking" module, with basic locking feature - Make locking opt-in

--- distribution/docker_entry.sh
+++ distribution/docker_entry.sh
@@ -164,6 +164,12 @@ then
   featureFlagString="$featureFlagString -f mvn:io.uhndata.cards/cards-slack-notifications/${CARDS_VERSION}/slingosgifeature"
 fi
 
+#Should the cards-locking module be loaded?
+if [[ "$LOCKING_ENABLED" == "true" ]]
+then
+  featureFlagString="$featureFlagString -f mvn:io.uhndata.cards/cards-locking/${CARDS_VERSION}/slingosgifeature"
+fi
+
 if [[ "$SMTPS_LOCALHOST_PROXY" == "true" ]]
 then
   keytool -import -trustcacerts -file /etc/cert/smtps_certificate.crt -keystore /etc/ssl/certs/java/cacerts -keypass changeit -storepass changeit -noprompt
