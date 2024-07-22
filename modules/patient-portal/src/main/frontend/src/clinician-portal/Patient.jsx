546d9eb8f4c01a3475ff63c57dbf190f9f4b396c
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2024-07-22 12:04:14 -0400

    CARDS-2558 / CARDS-2560: Patient page view for clinicians Fetch all visit information forms for a patient

--- modules/patient-portal/src/main/frontend/src/clinician-portal/Patient.jsx
+++ modules/patient-portal/src/main/frontend/src/clinician-portal/Patient.jsx
@@ -16,8 +16,8 @@
 //  specific language governing permissions and limitations
 //  under the License.
 //
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useContext } from "react";
+import { fetchWithReLogin, GlobalLoginContext } from "../login/loginDialogue.js";
 import QuestionnaireStyle from "../questionnaire/QuestionnaireStyle.jsx";
 
 import {
@@ -26,9 +26,55 @@ import {
 import withStyles from '@mui/styles/withStyles';
 
 function ClinicianPatient(props) {
+  const patientUuid = props.match.params.patientId.replace(/\..*/, '');
+  const [ patientData, setPatientData ] = useState();
+  const [ visitInformationQuestionnaire, setVisitInformationQuestionnaire ] = useState();
+  const [ visits, setVisits ] = useState();
+  const [ error, setError ] = useState();
+  const globalLoginDisplay = useContext(GlobalLoginContext);
+
+  const fetchPatientData = () => {
+    fetchWithReLogin(globalLoginDisplay, `/Subjects/${patientUuid}.data.deep.json`)
+      .then((response) => response.ok ? response.json() : Promise.reject(response))
+      .then((json) => {
+        setPatientData(json);
+      })
+      .catch(() => setError("The survey data could not be loaded for this visit. Please try again later or contact the administrator for further assistance."));
+  };
+  const fetchVisitInformationQuestionnaire = () => {
+    fetchWithReLogin(globalLoginDisplay, '/Questionnaires/Visit information.deep.json')
+      .then((response) => response.ok ? response.json() : Promise.reject(response))
+      .then((json) => {
+        setVisitInformationQuestionnaire(json);
+      })
+      .catch(() => setError("The survey data could not be loaded for this visit. Please try again later or contact the administrator for further assistance."));
+  };
+  const fetchVisitInformationForms = () => {
+    if (!visitInformationQuestionnaire || !patientData) {
+      return;
+    }
+    let url = new URL("/query", window.location.origin);
+    url.searchParams.set("query", `SELECT * FROM [cards:Form] as f where f.questionnaire = '${visitInformationQuestionnaire['jcr:uuid']}' and f.relatedSubjects = '${patientData['jcr:uuid']}' option (index tag cards)`);
+    url.searchParams.set("limit", 1000);
+    fetchWithReLogin(globalLoginDisplay, url)
+      .then(response => response.json())
+      .then(result => {
+        setVisits(Array.from(result.rows))
+      })
+      .catch(() => setError("The survey data could not be loaded for this visit. Please try again later or contact the administrator for further assistance."));
+  };
+
+  useEffect(fetchPatientData, []);
+  useEffect(fetchVisitInformationQuestionnaire, []);
+  useEffect(fetchVisitInformationForms, [patientData, visitInformationQuestionnaire]);
   return (
+    error ? <Typography>{error}</Typography>
+    :
     <Typography>
       Clinician patient view! Looking at {props.match.params.patientId}
+      {visits?.map((visit, index) => {
+        return <Typography key={index}>{visit['@name']}</Typography>
+      })}
     </Typography>
   );
 }
