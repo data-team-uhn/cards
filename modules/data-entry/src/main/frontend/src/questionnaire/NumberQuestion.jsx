89aca2563ca405b07080fae87b2a8b26ea53410b
Marta Girdea <marta-@users.noreply.github.com>
2024-12-11 10:45:20 -0500

    CARDS-2613: Units of measurement for numeric answers should support superscript (e.g. m^2)

--- modules/data-entry/src/main/frontend/src/questionnaire/NumberQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/NumberQuestion.jsx
@@ -37,6 +37,7 @@ import AnswerInstructions from "./AnswerInstructions";
 import Question from "./Question";
 import QuestionnaireStyle from "./QuestionnaireStyle";
 import MultipleChoice from "./MultipleChoice";
+import FormattedText from "../components/FormattedText";
 
 import AnswerComponentManager from "./AnswerComponentManager";
 
@@ -131,7 +132,7 @@ const useSliderStyles = makeStyles(theme => ({
 //    />
 function NumberQuestion(props) {
   const { existingAnswer, errorText, classes, pageActive, disableValueInstructions, ...rest} = props;
-  const { dataType,displayMode, minAnswers, minValue, maxValue, disableMinMaxValueEnforcement, messageForValuesOutsideMinMax, isRange,
+  const { dataType,displayMode, minAnswers, minValue, maxValue, disableMinMaxValueEnforcement, messageForValuesOutsideMinMax, isRange, unitOfMeasurement,
     sliderStep, sliderMarkStep, sliderOrientation, minValueLabel, maxValueLabel }
     = {sliderOrientation: "horizontal", ...props.questionDefinition, ...props};
   const answerNodeType = props.answerNodeType || DATA_TO_NODE_TYPE[dataType];
@@ -248,8 +249,8 @@ function NumberQuestion(props) {
     inputComponent: NumberFormatCustom, // Used to override a TextField's type
     className: classes.textField
   };
-  if (props.questionDefinition && props.questionDefinition.unitOfMeasurement) {
-    muiInputProps.endAdornment = <InputAdornment position="end">{props.questionDefinition.unitOfMeasurement}</InputAdornment>;
+  if (unitOfMeasurement) {
+    muiInputProps.endAdornment = <InputAdornment position="end"><FormattedText>{unitOfMeasurement}</FormattedText></InputAdornment>;
   }
 
   let hasAnswerOptions = !!(props.defaults || Object.values(props.questionDefinition).some(value => value['jcr:primaryType'] == 'cards:AnswerOption'));
@@ -291,6 +292,10 @@ function NumberQuestion(props) {
     return limits.join(' - ');
   }
 
+  let markdownFormatter = function(label, idx) {
+    return <FormattedText>{label}</FormattedText>;
+  }
+
   let setValue = function(fn, value) {
     let number = Number(value);
     if (dataType === "long" && !isNaN(number)) {
@@ -326,7 +331,7 @@ function NumberQuestion(props) {
 
   return (
     <Question
-      defaultDisplayFormatter={isRange? rangeDisplayFormatter : undefined}
+      defaultDisplayFormatter={isRange ? rangeDisplayFormatter : unitOfMeasurement ? markdownFormatter : undefined}
       compact={isRange}
       disableInstructions
       {...props}
