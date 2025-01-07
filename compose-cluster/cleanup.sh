e3f8d76c7808c1aceb8ecd734de284b971e772cb
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2025-01-07 13:36:17 -0500

    [misc] Upgrade the version of alpine linux used for our docker images

--- compose-cluster/cleanup.sh
+++ compose-cluster/cleanup.sh
@@ -43,7 +43,7 @@ rm -r secrets
 
 # Due permissions issues, we have to first remove all contents of SLING with Docker
 echo "Removing SLING"
-docker run --rm -v $(realpath ./SLING):/sling -it alpine:3.17 sh -c 'cd /sling; find . -delete'
+docker run --rm -v $(realpath ./SLING):/sling -it alpine:3.21 sh -c 'cd /sling; find . -delete'
 rm -rf SLING
 
 echo "Removing CARDS_LOGS"
