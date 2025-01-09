64e38e137e0345d6b6fb5b8e048eda8b1be9a167
veronikaslc <veronika.koltunov@gmail.com>
2025-01-08 22:36:14 -0500

    CARDS-2618: Replace Livetable with MaterialReactTable in administration screens Addressed codereview comments: Fixed typo

--- modules/patient-portal/src/main/frontend/src/patient-portal/Clinics.jsx
+++ modules/patient-portal/src/main/frontend/src/patient-portal/Clinics.jsx
@@ -43,7 +43,7 @@ function Clinics(props) {
       return {
         accessorKey: stat,
         header: camelCaseToWords(stat),
-        enableSorting: stat != "emergencyContac" || stat != "description",
+        enableSorting: stat != "emergencyContact" || stat != "description",
       };
     });
 
