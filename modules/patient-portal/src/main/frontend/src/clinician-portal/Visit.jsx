f9a5b8e1671ac67c8149b20d40daf000b4a8cd1b
Marta Girdea <marta-@users.noreply.github.com>
2025-02-18 09:42:21 -0500

    [ui-fix] Smaller avatar for items in the survey list

--- modules/patient-portal/src/main/frontend/src/clinician-portal/Visit.jsx
+++ modules/patient-portal/src/main/frontend/src/clinician-portal/Visit.jsx
@@ -58,6 +58,12 @@ import QuestionnaireStyle, { FORM_ENTRY_CONTAINER_PROPS } from "../questionnaire
 import { fetchWithReLogin, GlobalLoginContext } from "../login/loginDialogue.js";
 
 const useStyles = makeStyles(theme => ({
+  formItem: {
+    "& .MuiListItemAvatar-root" : {
+      marginTop: 6,
+      zoom: 1,
+    },
+  },
   stepIndicator : {
     border: "1px solid " + theme.palette.action.disabled,
     background: "transparent",
@@ -306,6 +312,7 @@ function Visit(props) {
       <List>
       { qIds.map((q, i) => (
         <ListItem
+          className={styles.formItem}
           key={q}
           disablePadding
           secondaryAction={withAction && !isFormLocked(q) && <EditButton entryPath={surveyData?.[q]?.["@path"]}/>}
