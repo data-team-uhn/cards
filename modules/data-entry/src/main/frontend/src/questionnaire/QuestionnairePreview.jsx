43b12fe7ebf006a5415962ec1c4267e939706bb2
veronikaslc <veronika.koltunov@gmail.com>
2025-02-22 00:43:24 -0500

    [dependency] Upgrade Material UI to v.7 Fixed grid and header alignments for Forms and Questionnaires

--- modules/data-entry/src/main/frontend/src/questionnaire/QuestionnairePreview.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/QuestionnairePreview.jsx
@@ -72,8 +72,7 @@ function QuestionnairePreview (props) {
     );
   }
 
-  return (<div className={data?.hideAnswerInstructions ? classes.hideAnswerInstructions : null}>
-    <Grid container {...FORM_ENTRY_CONTAINER_PROPS} >
+  return (<React.Fragment>
       { /* Added dummy save functionality for mocking file and pedigree questions functionality. */ }
       <FormProvider additionalFormData={{
           ['/Save']: () => { return new Promise((resolve, reject) => {return;})},
@@ -94,6 +93,7 @@ function QuestionnairePreview (props) {
                 existingAnswers={data}
                 keyProp={key}
                 classes={classes}
+                className={data?.hideAnswerInstructions ? classes.hideAnswerInstructions : null}
                 onChange={()=>{}}
                 visibleCallback={pageResult.callback}
                 pageActive={pageResult.page.visible}
@@ -115,7 +115,6 @@ function QuestionnairePreview (props) {
             onDone={close}
         />
       </Grid>
-    </Grid>
     {!paginationEnabled &&
       <MainActionButton
         icon={<CloseIcon />}
@@ -123,7 +122,7 @@ function QuestionnairePreview (props) {
         label="Close"
       />
     }
-  </div>);
+  </React.Fragment>);
 };
 
 export default withStyles(QuestionnairePreview, QuestionnaireStyle);
