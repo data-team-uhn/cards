eccc26aa3c56d4f038101f113348c2b8513f9e26
veronikaslc <veronika.koltunov@gmail.com>
2025-02-25 20:19:21 -0500

    [dependency] Update react-router from v. 5 to v. 7 Fixed Form rendering on save & view: force form checkout and re-ferch as useEffect(() => {}, []) is not triggered anymore if navigate() routs to the same component render

--- modules/data-entry/src/main/frontend/src/questionnaire/Form.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/Form.jsx
@@ -204,7 +204,7 @@ function Form (props) {
     requireCompletion && paginationEnabled && setDisableProgress(true);
   }, [requireCompletion, paginationEnabled]);
 
-  let checkoutIfNeededAndFetchData = () => {
+  let checkoutIfNeededAndFetchData = (callback) => {
     // Check if it was already checked out
     fetchWithReLogin(globalLoginDisplay, formURL + "/jcr:isCheckedOut")
       .then(response => response.text())
@@ -221,7 +221,8 @@ function Form (props) {
         } else {
           fetchData();
         }
-      });
+      })
+      .finally(callback);
   };
 
   // Fetch the form's data as JSON from the server.
@@ -412,7 +413,9 @@ function Form (props) {
     // ...but only after the Form has been saved and checked-in
     saveDataWithCheckin(undefined, () => {
         removeWindowHandlers && removeWindowHandlers();
-        navigate(urlBase + formURL);
+        checkoutIfNeededAndFetchData(() => {
+            navigate(urlBase + formURL);
+        });
     });
   }
 
