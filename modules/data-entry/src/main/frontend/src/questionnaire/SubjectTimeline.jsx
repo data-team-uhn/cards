75d65091c42e081e44226d6a67783375c2bb4ab9
veronikaslc <veronika.koltunov@gmail.com>
2025-02-26 11:55:54 -0500

    CARDS-2632: Errors fetching subject data leaves the subject timeline view hanging Stop displaying the loading animation if there is an error in response to subject fetch request

--- modules/data-entry/src/main/frontend/src/questionnaire/SubjectTimeline.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/SubjectTimeline.jsx
@@ -160,7 +160,7 @@ function TimelineEntry(classes, dateEntry, index, length, nextEntry) {
  */
 function SubjectTimeline(props) {
   let { classes, subject } = props;
-  let [dateEntries, setDateEntries] = useState(null);
+  let [ dateEntries, setDateEntries ] = useState(null);
   // Error message set when fetching the data from the server fails
   let [ error, setError ] = useState();
   let globalLoginDisplay = useContext(GlobalLoginContext);
@@ -357,6 +357,7 @@ function SubjectTimeline(props) {
 
   // Callback method for the `fetchData` method, invoked when the request failed.
   let handleError = (response) => {
+    setDateEntries([]);
     setError(response);
     console.log(response.statusText || response.message);
   };
