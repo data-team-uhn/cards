06cc25b8df800dcff6678a26ed23f5e59c9c7549
veronikaslc <veronika.koltunov@gmail.com>
2025-03-07 15:20:08 -0500

    [misc] Better class name

--- modules/patient-portal/src/main/frontend/src/patient-portal/SurveyInstructionsConfiguration.jsx
+++ modules/patient-portal/src/main/frontend/src/patient-portal/SurveyInstructionsConfiguration.jsx
@@ -78,7 +78,7 @@ function SurveyInstructionsConfiguration() {
         onConfigSaved={() => setHasChanges(false)}
         >
           <List className={classes.formEntries}>
-            { Object.keys(labels).map(category => { return (<span key={category + category}>
+            { Object.keys(labels).map(category => { return (<span key={category + "Wrapper"}>
               <ListItem key={category}>
                 <Typography variant="h6">{camelCaseToWords(category)}</Typography>
               </ListItem>
