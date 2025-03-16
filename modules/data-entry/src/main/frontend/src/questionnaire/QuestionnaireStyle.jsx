258368237799f1fb936c24bc7e80bbcd7e0f94d8
veronikaslc <veronika.koltunov@gmail.com>
2025-03-16 15:21:08 -0400

    [misc] Added circular progress during form saving process Removed leftover styling

--- modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
@@ -268,14 +268,6 @@ const questionnaireStyle = theme => ({
     circularProgressContainer: {
         marginTop: theme.spacing(5),
     },
-    circularProgressBackdrop: {
-	    backgroundColor: theme.palette.background.paper,
-        zIndex: theme.zIndex.drawer + 1,
-        opacity: "0.5!important",
-        [theme.breakpoints.up('md')]: {
-          marginLeft: "260px",
-        },
-    },
     compactLayout : {
       "& .MuiList-root" : {
         [theme.breakpoints.up('sm')]: {
