817cfd14fbb769926b14df6d993a306f2e195f6d
veronikaslc <veronika.koltunov@gmail.com>
2025-03-17 12:32:50 -0400

    [misc] Wrapping block in React Fragment element

--- modules/patient-portal/src/main/frontend/src/patient-portal/SurveyInstructionsConfiguration.jsx
+++ modules/patient-portal/src/main/frontend/src/patient-portal/SurveyInstructionsConfiguration.jsx
@@ -78,7 +78,7 @@ function SurveyInstructionsConfiguration() {
         onConfigSaved={() => setHasChanges(false)}
         >
           <List className={classes.formEntries}>
-            { Object.keys(labels).map(category => { return (<span key={category + "Wrapper"}>
+            { Object.keys(labels).map(category => { return (<React.Fragment key={category + "Wrapper"}>
               <ListItem key={category}>
                 <Typography variant="h6">{camelCaseToWords(category)}</Typography>
               </ListItem>
@@ -129,7 +129,7 @@ function SurveyInstructionsConfiguration() {
                   }
                 </ListItem>)
               })}
-            </span>)
+            </React.Fragment>)
           })}
         </List>
       </AdminConfigScreen>
