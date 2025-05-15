0de644346abadf187d4f683230362376c54bab29
veronikaslc <veronika.koltunov@gmail.com>
2025-05-15 15:15:07 -0400

    [misc] Avoid input switch from uncontrollable to controllable behaviour

--- modules/data-entry/src/main/frontend/src/questionnaireEditor/DroppableAnswerOption.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaireEditor/DroppableAnswerOption.jsx
@@ -167,7 +167,7 @@ function DroppableAnswerOption(props) {
             <Tooltip title="Selected by default">
               <Checkbox
                 color="secondary"
-                checked={value.isDefault}
+                checked={value.isDefault || false}
                 onChange={(event) => {
                   setOptions(old => {
                     var _new = old.slice();
