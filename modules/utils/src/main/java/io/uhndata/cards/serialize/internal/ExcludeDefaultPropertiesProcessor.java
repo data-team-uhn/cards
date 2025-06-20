data-team-uhn/cards

commit edbca39726791b5641ab50c9e3aeb803ca34e4cb

CARDS-2672: Json processor that excludes properties if their value is the default value
Minor Javadoc rewording.

modules/utils/src/main/java/io/uhndata/cards/serialize/internal/ExcludeDefaultPropertiesProcessor.java
@@ -40,14 +40,14 @@ import io.uhndata.cards.forms.api.FormUtils;
 import io.uhndata.cards.serialize.spi.ResourceJsonProcessor;
 
 /**
- * An optional {@link ResourceJsonProcessor} that processes resources of all resource types.
- * This processor targets nodes and excludes properties if their value is the default value or obvious non-values.
+ * An optional {@link ResourceJsonProcessor} applicable to any type of resource. This processor targets
+ * nodes and excludes their properties if their value is the default value or an obvious non-value.
  * The name of this processor is {@code excludeDefaultProperties}.
  *
- * Property exclusion logic for a node properties:
+ * Property exclusion logic:
  * <p>
  * <ul>
- *   <li>If a property's value matches its JCR-defined default value, the property is excluded from JSON output.</li>
+ *   <li>If a property's value matches its JCR-defined default value, the property is excluded from the JSON output.</li>
  *   <li>If a property does not have a JCR-defined default value:
  *     <ul>
  *       <li>It is excluded if it's a boolean type property with a value of {@code false}.</li>

commit 572e61fecc28720d4fa1119829e092e902c1254b

[misc] Fix check style error
Introduced by commit edbca39726791b5641ab50c9e3aeb803ca34e4cb for CARDS-2672 .

modules/utils/src/main/java/io/uhndata/cards/serialize/internal/ExcludeDefaultPropertiesProcessor.java
@@ -47,7 +47,8 @@ import io.uhndata.cards.serialize.spi.ResourceJsonProcessor;
  * Property exclusion logic:
  * <p>
  * <ul>
- *   <li>If a property's value matches its JCR-defined default value, the property is excluded from the JSON output.</li>
+ *   <li>If a property's value matches its JCR-defined default value, the property is excluded from the
+ *       JSON output.</li>
  *   <li>If a property does not have a JCR-defined default value:
  *     <ul>
  *       <li>It is excluded if it's a boolean type property with a value of {@code false}.</li>