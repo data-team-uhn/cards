08ec18a9fa9e136fd72d2bf3ed14d1cdaa66795e
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2025-07-08 09:54:22 -0400

    [misc] More descriptive error when the Google ApiKey is missing

--- start_cards.sh
+++ start_cards.sh
@@ -136,11 +136,12 @@ function message_bioportal_apikey_missing() {
 }
 
 function message_google_apikey_missing() {
-  echo -e "${TERMINAL_YELLOW}*******************************${TERMINAL_NOCOLOR}"
-  echo -e "${TERMINAL_YELLOW}*                             *${TERMINAL_NOCOLOR}"
-  echo -e "${TERMINAL_YELLOW}* GOOGLE_APIKEY not specified *${TERMINAL_NOCOLOR}"
-  echo -e "${TERMINAL_YELLOW}*                             *${TERMINAL_NOCOLOR}"
-  echo -e "${TERMINAL_YELLOW}*******************************${TERMINAL_NOCOLOR}"
+  echo -e "${TERMINAL_YELLOW}*********************************************************${TERMINAL_NOCOLOR}"
+  echo -e "${TERMINAL_YELLOW}*                                                       *${TERMINAL_NOCOLOR}"
+  echo -e "${TERMINAL_YELLOW}* GOOGLE_APIKEY not specified                           *${TERMINAL_NOCOLOR}"
+  echo -e "${TERMINAL_YELLOW}* Certain question types may have limited functionality *${TERMINAL_NOCOLOR}"
+  echo -e "${TERMINAL_YELLOW}*                                                       *${TERMINAL_NOCOLOR}"
+  echo -e "${TERMINAL_YELLOW}*********************************************************${TERMINAL_NOCOLOR}"
 }
 
 function message_hancestro_install_ok() {
