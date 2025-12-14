aa6863b37f0dc113679118c0c9c3260af99798b3
veronikaslc <veronika.koltunov@gmail.com>
2025-12-14 04:20:34 -0500

    CARDS-2797: Enforce various useful ESLint rules Added react rule to report unused prop types

--- modules/data-entry/src/main/frontend/src/questionnaire/SubjectTimeline.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/SubjectTimeline.jsx
@@ -383,7 +383,8 @@ function SubjectTimeline(props) {
 }
 
 SubjectTimeline.propTypes = {
-  id: PropTypes.string
+  classes: PropTypes.object.isRequired,
+  subject: PropTypes.object.isRequired
 }
 
 export default withStyles(SubjectTimeline, QuestionnaireStyle);
