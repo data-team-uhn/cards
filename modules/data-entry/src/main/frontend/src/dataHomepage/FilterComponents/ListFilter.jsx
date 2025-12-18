b2765e98d9f12a76dc7879e83cb138c2e15ae32d
veronikaslc <veronika.koltunov@gmail.com>
2025-12-18 16:31:05 -0500

    [misc] Removed deprecated forwardRef() Returned removed ref by accident

--- modules/data-entry/src/main/frontend/src/dataHomepage/FilterComponents/ListFilter.jsx
+++ modules/data-entry/src/main/frontend/src/dataHomepage/FilterComponents/ListFilter.jsx
@@ -39,7 +39,7 @@ const COMPARATORS = DEFAULT_COMPARATORS.slice().concat(UNARY_COMPARATORS);
  * @param {object} questionDefinition Object containing the definition of the question. Should include nodes whose jcr:primaryType is cards:AnswerOption
  *
  */
-const ListFilter = (props) => {
+const ListFilter = (props, ref) => {
   checkPropTypes(ListFilter, props);
   const { classes, initial, onChangeInput, questionDefinition } = props;
   // Manage our own state inside here as well
