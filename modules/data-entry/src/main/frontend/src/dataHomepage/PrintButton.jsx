56be09054c174e1797855a81e29807955a263736
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2026-01-09 14:56:07 -0500

    CARDS-2797: Enforce various useful ESLint rules Remove leftover mention of a bogus prop from jsdoc

--- modules/data-entry/src/main/frontend/src/dataHomepage/PrintButton.jsx
+++ modules/data-entry/src/main/frontend/src/dataHomepage/PrintButton.jsx
@@ -39,7 +39,6 @@ import PrintPreview from "../questionnaire/PrintPreview.jsx";
  * variant: String defining the render component and view of the print action button, default "icon"
  * size: String regulating the size of an icon button, default "medium"
  * className: String of class name that applies to the button element if the IconButton when variant == "icon"
- * buttonText: String specifying the text to be displayed on the button or the tooltip, default "Print preview"
  * fullScreen: Boolean specifying if the preview is full screen or displayed as a modal, default true
  * disableShortcut: Boolean specifying if Ctrl+P should activate this button or not. Default is false, meaning the shortcut is active.
  * onOpen: Callback for opening the preview dialog
