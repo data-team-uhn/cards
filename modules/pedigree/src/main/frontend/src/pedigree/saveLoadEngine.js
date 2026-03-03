4a159eb0e5ad34fe5ff5580bc04a38ae5808fd82
veronikaslc <veronika.koltunov@gmail.com>
2026-03-02 19:42:19 -0500

    [code-cleanup] Removed dead code

--- modules/pedigree/src/main/frontend/src/pedigree/saveLoadEngine.js
+++ modules/pedigree/src/main/frontend/src/pedigree/saveLoadEngine.js
@@ -26,46 +26,6 @@ import TemplateSelector from './view/templateSelector';
  * @constructor
  */
 
-function unescapeRestData (data) {
-  // http://stackoverflow.com/questions/4480757/how-do-i-unescape-html-entities-in-js-change-lt-to
-  var tempNode = document.createElement('div');
-  tempNode.innerHTML = data.replace(/&amp;/, '&');
-  return tempNode.innerText || tempNode.text || tempNode.textContent;
-}
-
-function getSelectorFromXML(responseXML, selectorName, attributeName, attributeValue) {
-  if (responseXML.querySelector) {
-    // modern browsers
-    return responseXML.querySelector(selectorName + '[' + attributeName + '=\'' + attributeValue + '\']');
-  } else {
-    // IE7 && IE8 && some other older browsers
-    // http://www.w3schools.com/XPath/xpath_syntax.asp
-    // http://msdn.microsoft.com/en-us/library/ms757846%28v=vs.85%29.aspx
-    var query = '//' + selectorName + '[@' + attributeName + '=\'' + attributeValue + '\']';
-    try {
-      return responseXML.selectSingleNode(query);
-    } catch (e) {
-      // Firefox v3.0-
-      alert('your browser is unsupported');
-      window.stop && window.stop();
-      throw 'Unsupported browser';
-    }
-  }
-}
-
-function getSubSelectorTextFromXML(responseXML, selectorName, attributeName, attributeValue, subselectorName) {
-  var selector = getSelectorFromXML(responseXML, selectorName, attributeName, attributeValue);
-
-  var value = selector.innerText || selector.text || selector.textContent;
-
-  if (!value)     // fix IE behavior where (undefined || "" || undefined) == undefined
-  {
-    value = '';
-  }
-
-  return value;
-}
-
 var SaveLoadEngine = Class.create( {
 
   initialize: function() {
@@ -150,8 +110,6 @@ var SaveLoadEngine = Class.create( {
     }   // nothing to do
 
     editor.getView().unmarkAll();
-
-    var me = this;
 
     var jsonData = this.serialize();
 
