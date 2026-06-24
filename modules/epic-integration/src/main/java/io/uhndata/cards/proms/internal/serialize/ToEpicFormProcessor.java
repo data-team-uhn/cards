96e0e30bacf4f879604f841386a4c003c213c434
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2026-06-24 16:27:51 -0400

    [rebase] Make old code work with the new CARDS codebase

--- modules/epic-integration/src/main/java/io/uhndata/cards/proms/internal/serialize/ToEpicFormProcessor.java
+++ modules/epic-integration/src/main/java/io/uhndata/cards/proms/internal/serialize/ToEpicFormProcessor.java
@@ -28,13 +28,14 @@ import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.query.Query;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonString;
-import javax.json.JsonValue;
-import javax.json.JsonValue.ValueType;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
+import jakarta.json.JsonValue.ValueType;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.LoginException;
@@ -84,6 +85,14 @@ public class ToEpicFormProcessor implements ResourceJsonProcessor
     public String getName()
     {
         return "toEpic";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Only runs on `Forms`.\n"
+            + "Serialize forms in the format expected by Epic. "
+            + "Designed for use with `deep`.";
     }
 
     @Override
